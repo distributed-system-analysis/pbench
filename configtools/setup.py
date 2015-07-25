a316d365648840c65717d865394f937588a10018
Peter Portante <peter.portante@redhat.com>
2015-07-25 08:30:36 -0400

    Drop the RPM and configtools subtrees
    The RPM sausage making will be moved to Fedora, and the configtools
    will be moved to is a sister repo.

--- configtools/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='configtools',
-      version='0.2',
-      description='Simple tools for using config files',
-      url='http://foo',
-      packages = ['configtools'],
-     )
