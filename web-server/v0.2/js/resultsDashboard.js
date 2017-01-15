3a7c79b9aaac166fd9fde1556442c3cf43acdc7c
Gurbir Kalsi <kalsigurbir@gmail.com>
2017-01-15 19:35:20 +0000

    Rename CSS/JS dependencies

--- web-server/v0.2/js/resultsDashboard.js
+++ web-server/v0.2/js/resultsDashboard.js
@@ -1,153 +1,103 @@
 function retrieveData() {
 	var listvalues = localStorage.getItem('resultData');
 	var finalvalue = JSON.parse(listvalues);
-	var tableHeader = document.getElementById("result_name");
-	tableHeader.innerHTML = finalvalue.resultName;
 	retrieveJSON(finalvalue.resultName);
-
 }
 
 function retrieveJSON(resultName) {
-	//Get result metadata
+	// Get result metadata
+    $.ajax({ dataType: "json", url: 'http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?source={ "query": { "match": { "run.name": "' + resultName + '" } }, "sort": "_index" }&_source_exclude=table-of-contents', success: function (data) {
+        if (data.hits.hits.length < 1) {
+            var dbHTML = document.getElementById("dbstart");
+            dbHTML.innerHTML = "Dashboard load for result " + resultName + " failed. :(";
+            return;
+        }
+        var result = data.hits.hits[data.hits.hits.length - 1]._source;
 
-	//Get turbostat
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.turbostat" } } } } } } } }', function(data) {
-		var turbostat = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var turbostatHTML = document.getElementById("turbostat");
-		turbostatHTML.innerHTML = turbostat;
-
-
-	});
-	//Get mpstat
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.mpstat" } } } } } } } }', function(data) {
-		var mpstat = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var mpstatHTML = document.getElementById("mpstat");
-		mpstatHTML.innerHTML = mpstat;
-
-
-	});
-
-	//Get sar
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.sar" } } } } } } } }', function(data) {
-		var sar = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var sarHTML = document.getElementById("sar");
-		sarHTML.innerHTML = sar;
-
-
-	});
-
-	//Get proc-interrupts
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.proc-interrupts" } } } } } } } }', function(data) {
-		var procInterrupts = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var procInterruptsHTML = document.getElementById("proc-interrupts");
-		procInterruptsHTML.innerHTML = procInterrupts;
-
-
-	});
-
-	//Get host
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.host" } } } } } } } }', function(data) {
-		var host = data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var hostHTML = document.getElementById("host");
-		hostHTML.innerHTML = host;
-
-
-	});
-
-	//Get perf
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.perf" } } } } } } } }', function(data) {
-		var perf = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[4].key + "-" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[3].key + "=\"" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[4].key + " -" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + " --" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[2].key + "=" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key + "\"";
-		var perfHTML = document.getElementById("perf");
-		perfHTML.innerHTML = perf; 
-
-
-	});
-
-	//Get pidstat
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.pidstat" } } } } } } } }', function(data) {
-		var pidstat = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var pidstatHTML = document.getElementById("pidstat");
-		pidstatHTML.innerHTML = pidstat;
-
-
-	});
-
-	//Get iostat
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.iostat" } } } } } } } }', function(data) {
-		var iostat = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var iostatHTML = document.getElementById("iostat");
-		iostatHTML.innerHTML = iostat;
-
-
-	});
-
-	//Get proc-vmstat
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "aggs": { "my_agg": { "filter": { "term": { "run.name": "'+ resultName + '" } }, "aggs": { "my_inner_agg": { "nested": { "path": "tools" }, "aggs": { "perhost": { "terms": { "field": "tools.proc-vmstat" } } } } } } } }', function(data) {
-		var procVmstat = "--" + data.aggregations.my_agg.my_inner_agg.perhost.buckets[1].key + "="  + data.aggregations.my_agg.my_inner_agg.perhost.buckets[0].key;
-		var procVmstatHTML = document.getElementById("proc-vmstat");
-		procVmstatHTML.innerHTML = procVmstat;
-
-
-	});
-
-	//Get config
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/_search?search_type=count&source={ "query": { "match": { "name": "' + resultName + '" } }, "aggs": { "run": { "terms": { "field": "config", "size": 0 } } } }', function(data) {
-		var config = data.aggregations.run.buckets[0].key;
-		var configHTML = document.getElementById("config");
-		configHTML.innerHTML = config;
-
-
-	});
-
-
-	//Get script
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "query": { "match": { "name": "' + resultName + '" } }, "aggs": { "run": { "terms": { "field": "script", "size": 0 } } } }', function(data) {
-		var script = data.aggregations.run.buckets[0].key;
+        // Update header
+        $("#dbstart").hide();
+        $("#dbfinal").show();
+	    var result_name = document.getElementById("result_name");
+	    result_name.innerHTML = resultName;
+	    // run.script
 		var scriptHTML = document.getElementById("script");
-		scriptHTML.innerHTML = script;
+		scriptHTML.innerHTML = result.run.script;
 
-
-	});
-
-	//Get controller
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "query": { "match": { "name": "' + resultName + '" } }, "aggs": { "run": { "terms": { "field": "controller", "size": 0 } } } }', function(data) {
-		var controller = data.aggregations.run.buckets[0].key;
+        // Update metadata list on right sidebar
+		var configHTML = document.getElementById("config");
+		configHTML.innerHTML = result.run.config;
 		var controllerHTML = document.getElementById("controller");
-		controllerHTML.innerHTML = controller;
-
-	});
-
-	//Get generated-by 
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "query": { "match": { "name": "' + resultName + '" } }, "aggs": { "run": { "terms": { "field": "generated-by", "size": 0 } } } }', function(data) {
-		var generatedBy = data.aggregations.run.buckets[0].key;
+		controllerHTML.innerHTML = result.run.controller;
 		var generatedByHTML = document.getElementById("generated_by");
-		generatedByHTML.innerHTML = generatedBy;
-
-
-	});
-
-	//Get generated-by-version
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "query": { "match": { "name": "' + resultName + '" } }, "aggs": { "run": { "terms": { "field": "generated-by-version", "size": 0 } } } }', function(data) {
-		var generatedByVersion = data.aggregations.run.buckets[0].key;
+		generatedByHTML.innerHTML = result._metadata["generated-by"];
 		var generatedByVersionHTML = document.getElementById('generated_by_version');
-		generatedByVersionHTML.innerHTML = generatedByVersion;
-
-
-	});
-
-	//Get file-name
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/pbench.pbench-0.*/_search?search_type=count&source={ "query": { "match": { "name": "' + resultName + '" } }, "aggs": { "run": { "terms": { "field": "file-name", "size": 0 } } } }', function(data) {
-		var fileName = data.aggregations.run.buckets[0].key;
+		generatedByVersionHTML.innerHTML = result._metadata["generated-by-version"];
 		var fileNameHTML = document.getElementById("file_name");
-		fileNameHTML.innerHTML = fileName;
+		fileNameHTML.innerHTML = result._metadata["file-name"];
+        var pbenchVerHTML = document.getElementById("pbench_version");
+        pbenchVerHTML.innerHTML = result.pbench["pbench-rpm-version"];
 
+        // Update tools information on right sidebar
+        if (result.tools.length > 0) {
+            // FIX-ME: create a datatable for all hosts and tool options as a pop-up
+            var tool = result.tools[0];
+	        // tools.host
+		    var hostHTML = document.getElementById("host");
+		    hostHTML.innerHTML = tool.host;
+            // tools.label
+            if (tool.label !== undefined) {
+		        var labelHTML = document.getElementById("label");
+		        labelHTML.innerHTML = tool.label;
+            }
+            // tools.turbostat
+            if (tool.turbostat !== undefined) {
+		        var turbostatHTML = document.getElementById("turbostat");
+		        turbostatHTML.innerHTML = tool.turbostat;
+            }
+	        // tools.mpstat
+            if (tool.mpstat !== undefined) {
+		        var mpstatHTML = document.getElementById("mpstat");
+		        mpstatHTML.innerHTML = tool.mpstat;
+            }
+	        // tools.sar
+            if (tool.sar !== undefined) {
+		        var sarHTML = document.getElementById("sar");
+		        sarHTML.innerHTML = tool.sar;
 
-	});
-
-
-
+                // If we have sar tool data, use that to draw summary graphs
+                //constructChart("lineChart", 1, "uperf_CPU_usage");
+                //constructChart("lineChart", 2, "disk_Queue_Size");
+                //constructChart("lineChart", 3, "network_l2_network_Mbits_sec");
+                //constructChart("lineChart", 4, "memory_memory_activity");
+            }
+	        // tools.proc-interrupts
+            if (tool["proc-interrupts"] !== undefined) {
+		        var procInterruptsHTML = document.getElementById("proc-interrupts");
+		        procInterruptsHTML.innerHTML = tool["proc-interrupts"];
+            }
+	        // tools.perf
+            if (tool.perf !== undefined) {
+		        var perfHTML = document.getElementById("perf");
+		        perfHTML.innerHTML = tool.perf;
+            }
+	        // tools.pidstat
+            if (tool.pidstat !== undefined) {
+		        var pidstatHTML = document.getElementById("pidstat");
+		        pidstatHTML.innerHTML = tool.pidstat;
+            }
+	        // tools.iostat
+            if (tool.iostat !== undefined) {
+		        var iostatHTML = document.getElementById("iostat");
+		        iostatHTML.innerHTML = tool.iostat;
+            }
+	        // tools.proc-vmstat
+            if (tool["proc-vmstat"] !== undefined) {
+		        var procVmstatHTML = document.getElementById("proc-vmstat");
+		        procVmstatHTML.innerHTML = tool["proc-vmstat"];
+            }
+        }
+	}});
 }
-
 
 function onStart() {
 	retrieveData();
