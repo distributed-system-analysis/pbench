8ad2050ce816c85efb1927828381770f2aded02f
Gurbir Kalsi <kalsigurbir@gmail.com>
2017-01-22 03:17:06 +0000

    Remove single page graph work from dashboard work

--- web-server/v0.2/js/mainDashboard.js
+++
@@ -1,3 +0,0 @@
-function retrieveJSON() {
-	$.getJSON('http://es-perf44.perf.lab.eng.bos.redhat.com:9280/_search?search_type=count&source={ "aggs": { "run": { "terms": { "field": "controller", "size": 0 } } } }')
-}
