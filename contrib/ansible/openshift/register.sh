6dc690287ff4bac2f401b954e06833c4f0d9faed
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2017-07-18 17:13:57 -0400

    Register perf tool on all the nodes

--- contrib/ansible/openshift/register.sh
+++ contrib/ansible/openshift/register.sh
@@ -67,6 +67,7 @@ while read -u 11 line;do
   pbench-register-tool --label=$label --name=iostat --remote $remote
   pbench-register-tool --label=$label --name=pidstat --remote $remote
   pbench-register-tool --label=$label --name=disk --remote $remote
+  pbench-register-tool --label=$label --name=perf
   if [ "$group"  == "master" ]; then
     pbench-register-tool --label=$label --name=oc --remote $remote
     pbench-register-tool --label=$label --name=pprof --remote $remote -- --osecomponent=master --interval=$ose_master_interval
