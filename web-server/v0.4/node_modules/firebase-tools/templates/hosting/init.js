215eef905d16d5c7c84343f73d5d69ad22a1c432
gurbirkalsi <kalsigurbir@gmail.com>
2017-10-14 14:21:50 -0400

    Initial commit

--- web-server/v0.4/node_modules/firebase-tools/templates/hosting/init.js
+++
@@ -1,2 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({/*--CONFIG--*/});
