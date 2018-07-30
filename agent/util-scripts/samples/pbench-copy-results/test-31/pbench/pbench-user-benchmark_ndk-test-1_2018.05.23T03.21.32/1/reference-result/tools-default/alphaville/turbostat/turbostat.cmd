7a7356abf4297a693b1da0420d8598c84af0bd6e
Nick Dokos <ndokos@redhat.com>
2018-07-30 14:37:01 -0400

    Unit test for pbench-copy-results.
    Add unit test for pbench-copy-results.

---
+++ agent/util-scripts/samples/pbench-copy-results/test-31/pbench/pbench-user-benchmark_ndk-test-1_2018.05.23T03.21.32/1/reference-result/tools-default/alphaville/turbostat/turbostat.cmd
@@ -0,0 +1,1 @@
+/opt/pbench-agent/tool-scripts/datalog/turbostat-datalog 3 /var/lib/pbench-agent/pbench-user-benchmark_ndk-test-1_2018.05.23T03.21.32/1/reference-result/tools-default/turbostat/turbostat-stdout.txt
