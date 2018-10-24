2b25482707e49940639479a8bb0c615e48400ca1
Andrew Theurer <atheurer@redhat.com>
2018-10-23 20:03:19 -0400

    create-links.sh: overlay new files on existing pbench-agent Running this script from a pbench git repo in ./agent/ will create any symlinks to new files in the repo but not in the installed package under /opt/pbench-agent.

---
+++ contrib/create-links.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mypwd=`/bin/pwd`
+subdir=`basename $mypwd`
+if [ "$subdir" != "agent" ]; then
+	echo "you need to run this from ./agent dir in a pbench git repo"
+	exit 1
+fi
+for dir in lib util-scripts bench-scripts bench-scripts/postprocess tool-scripts tool-scripts/postprocess; do
+		for i in `find $dir -maxdepth 1 | grep -v gold | grep -v unittests | grep -v samples | grep -v test-bin`; do
+		if [ ! -e /opt/pbench-agent/$i ]; then
+			echo creating symlink "$mypwd/$i <- /opt/pbench-agent/$i"
+			ln -sf $mypwd/$i /opt/pbench-agent/$i
+		fi
+	done
+done
