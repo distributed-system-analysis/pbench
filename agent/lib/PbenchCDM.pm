9efff1eeaade71cbfcad0ae13bcf1b843db7d9c9
Andrew Theurer <atheurer@redhat.com>
2018-10-25 14:28:46 -0400

    chnages for CDMv2

--- agent/lib/PbenchCDM.pm
+++ agent/lib/PbenchCDM.pm
@@ -38,36 +38,36 @@ sub get_user_email { # looks for USER_NAME in %ENV
 sub populate_base_fields { # create the fields every doc must have
 	my $doc_ref = shift;
 	#$$doc_ref{'doc_id'} = get_uuid;
-	$$doc_ref{'ver'} = 1;
+	$$doc_ref{'cdm'}{'ver'} = 2;
 }
 sub copy_doc_fields {
 	my $copy_from_ref = shift; # what document we need to copy from
 	my $copy_to_ref = shift; # what new document we're copying to
-	#for my $field_name (grep(!/doc_id|doc_ver|doc_create_time/, keys %$copy_from_ref)) {
-	for my $field_name (grep(!/^doc_/, keys %$copy_from_ref)) {
-		$$copy_to_ref{$field_name} = $$copy_from_ref{$field_name};
-	}
+	%$copy_to_ref = %$copy_from_ref;
+	undef $$copy_to_ref{'cdm'};
+	#for my $field_name (grep(!/^doc_/, keys %$copy_from_ref)) {
+		#$$copy_to_ref{$field_name} = $$copy_from_ref{$field_name};
+	#}
 	# convert doc_id to run_id, iter_id, sample_id, or period_id
 	#$$copy_to_ref{$$copy_from_ref{'doc_type'} . "_id"} = $$copy_from_ref{'doc_id'};
 }
 sub create_run_doc {
 	my %doc;
 	populate_base_fields(\%doc);
-	$doc{'run_bench_name'} = shift; # the name of the actual benchmark used, like fio ir uperf
-	$doc{'run_bench_params'} = shift; # the full list of parameters when calling the benchmark
-	$doc{'run_bench_clients'} = shift; # client hosts involved in the benchmark
-	$doc{'run_bench_servers'} = shift; # server hosts involved in the benchmark
-	$doc{'run_user_desc'} = shift; # user provided shortlist of var:val with "," separator (no spaces)
-	$doc{'run_user_name'} = shift; # user's real name
-	$doc{'run_user_email'} = shift; #user's email address
-	$doc{'run_harness_name'} = shift; #harness name like pbench, browbeat, cbt
-	$doc{'run_tool_names'} = shift; # list tool names like sar, mpstat
-	$doc{'run_host'} = get_hostname; # hostname of this controller system
-	$doc{'run_ignore'} = JSON::false; # set to true later if run should be ingnored in queries
-	$doc{'run_archive'} = JSON::false; # set to true later if you wish to archive (and remove) run from ES
-	$doc{'run_datetime'} = get_pbench_datetime;
-	$doc{'doc_type'} = 'run';
-	$doc{'run_id'} = get_uuid; #$doc{'doc_id'};
+	$doc{'run'}{'bench'}{'name'} = shift; # the name of the actual benchmark used, like fio ir uperf
+	$doc{'run'}{'bench'}{'params'} = shift; # the full list of parameters when calling the benchmark
+	$doc{'run'}{'bench'}{'clients'} = shift; # client hosts involved in the benchmark
+	$doc{'run'}{'bench'}{'servers'} = shift; # server hosts involved in the benchmark
+	$doc{'run'}{'user'}{'desc'} = shift; # user provided shortlist of var:val with "," separator (no spaces)
+	$doc{'run'}{'user'}{'name'} = shift; # user's real name
+	$doc{'run'}{'user'}{'email'} = shift; #user's email address
+	$doc{'run'}{'harness_name'} = shift; #harness name like pbench, browbeat, cbt
+	$doc{'run'}{'tool_names'} = shift; # list tool names like sar, mpstat
+	$doc{'run'}{'host'} = get_hostname; # hostname of this controller system
+	$doc{'run'}{'ignore'} = JSON::false; # set to true later if run should be ingnored in queries
+	$doc{'run'}{'start'} = time * 1000;
+	$doc{'run'}{'id'} = get_uuid; #$doc{'doc_id'};
+	$doc{'cdm'}{'doctype'} = 'run';
 	# other fields not required at time of doc creation:
 	# 'run_notes' : <text> 
 	return %doc;
@@ -75,59 +75,61 @@ sub create_run_doc {
 sub create_config_doc { # document describing a configuration source
 	my $copy_from_ref = shift; # first arg is a reference to a doc (like the run doc) we copy info from
 	my $config_ref = shift; # second arg is hash reference to any other keys/values to include in this doc
-	my $doc_type = "config_" . $$config_ref{'module'};
 	my %doc;
-	populate_base_fields(\%doc);
 	copy_doc_fields($copy_from_ref, \%doc); # get some essential fields from another doc (like run)
-	$doc{'doc_type'} = $doc_type;
-	$doc{$doc_type . '_id'} = get_uuid;
+	populate_base_fields(\%doc);
+	$doc{'cdm'}{'doctype'} = "config_" . $$config_ref{'module'};
+	$doc{'config'}{'id'} = get_uuid;
 	for my $key (keys %$config_ref) {
-		#$doc{$doc_type . "_" . $key} = $$config_ref{$key};
-		$doc{$key} = $$config_ref{$key};
+		if ($key =~ /^host$|^module$|^source_type$|^scribe_uuid$/) {
+			$doc{'config'}{$key} = $$config_ref{$key};
+		} else {
+			$doc{'config'}{$$config_ref{'module'}}{$key} = $$config_ref{$key};
+		}
 	}
 	return %doc;
 }
 sub create_bench_iter_doc { # document describing the benchmark iteraton sample
 	my %doc;
-	populate_base_fields(\%doc);
 	copy_doc_fields(shift, \%doc); # get some essential fields from iter-sample, our first arg
-	$doc{'iteration_params'} = shift; # second arg is benchmark parameters for this iter
-	$doc{'iteration_id'} = get_uuid;
-	$doc{'doc_type'} = 'iter';
+	populate_base_fields(\%doc);
+	$doc{'iteration'}{'params'} = shift; # second arg is benchmark parameters for this iter
+	$doc{'iteration'}{'id'} = get_uuid;
+	$doc{'cdm'}{'doctype'} = 'iteration';
 	return %doc;
 }
 sub create_bench_iter_sample_doc { # document describing the benchmark iteraton sample
 	my %doc;
-	populate_base_fields(\%doc);
 	copy_doc_fields(shift, \%doc); # get some essential fields from iter doc, our first arg
-	$doc{'sample_num'} = shift; # second arg is sample number (just used to make it obvious which order these occur in)
-	$doc{'sample_id'} = get_uuid;
-	$doc{'doc_type'} = 'sample';
+	populate_base_fields(\%doc);
+	$doc{'sample'}{'num'} = shift; # second arg is sample number (just used to make it obvious which order these occur in)
+	$doc{'sample'}{'id'} = get_uuid;
+	$doc{'cdm'}{'doctype'} = 'sample';
 	return %doc;
 }
 sub create_bench_iter_sample_period_doc { # document describing the benchmark iteraton sample
 	my %doc;
-	populate_base_fields(\%doc);
 	copy_doc_fields(shift, \%doc); # get some essential fields from iter-sample doc, our first arg
-	$doc{'period_name'} = shift; # second arg is period name
-	$doc{'period_prev_id'} = shift; # third arg is link to prev period in this sample, if any
-	$doc{'period_id'} = get_uuid;
-	$doc{'doc_type'} = 'period';
+	populate_base_fields(\%doc);
+	$doc{'period'}{'name'} = shift; # second arg is period name
+	$doc{'period'}{'prev_id'} = shift; # third arg is link to prev period in this sample, if any
+	$doc{'period'}{'id'} = get_uuid;
+	$doc{'cdm'}{'doctype'} = 'period';
 	return %doc;
 }
 sub create_metric_sample_doc { # document describing the benchmark iteraton sample
 	my %doc;
-	populate_base_fields(\%doc);
 	copy_doc_fields(shift, \%doc); # get some essential fields from a prev doc, our first arg
-	$doc{'doc_type'} = 'metric';
+	populate_base_fields(\%doc);
+	$doc{'cdm'}{'doctype'} = 'metric';
 	# These are the required fields for any metric-instance-sample.  There are potentially
 	# more fields, but not all metrics use all the same options fields.  However, the ones
 	# below must all be used, and so creating a new doc requires that these fields be
 	# defined.
-	$doc{'metric_class'} = shift; # "throughput" (work over time, like Gbps or interrupts/sec) or "count" (quantity, percent, sum, elapsed time, value, etc)
-	$doc{'metric_type'} = shift; # A generic name for this metric, like "gigabits-per-second", does not include specifics like "/dev/sda" or "cpu1"
-	$doc{'metric_hostname'} = shift; # the hostname where this metric comes from
-	$doc{'metric_source'} = shift; # a benchmark or tool where this metric comes from, like "iostat" or "fio"
+	$doc{'metric'}{'class'} = shift; # "throughput" (work over time, like Gbps or interrupts/sec) or "count" (quantity, percent, sum, elapsed time, value, etc)
+	$doc{'metric'}{'type'} = shift; # A generic name for this metric, like "gigabits-per-second", does not include specifics like "/dev/sda" or "cpu1"
+	$doc{'metric'}{'hostname'} = shift; # the hostname where this metric comes from
+	$doc{'metric'}{'source'} = shift; # a benchmark or tool where this metric comes from, like "iostat" or "fio"
 	# The instance_name_format tells us how the name for this metric-instance is assembled.
 	# The instance name is assembled from other fields in this document
 	# the format is described by joining strings and field names (which are identified by % before and after), like:
@@ -148,10 +150,10 @@ sub create_metric_sample_doc { # document describing the benchmark iteraton samp
 	# more fields required, and this code can't possibly know all situations
 	# so it is up to the caller of this function to understand that scenario
 	# and provide an adequtate instance name format.
-	$doc{'metric_name_format'} = shift;
-	$doc{'metric_value'} = shift; # the value of the metric
-	$doc{'metric_timestamp'} = shift; # the epochtime
-	$doc{'metric_id'} = get_uuid;
+	$doc{'metric'}{'name_format'} = shift;
+	$doc{'metric'}{'value'} = shift; # the value of the metric
+	$doc{'metric'}{'end'} = shift; # the end epochtime for this value
+	$doc{'metric'}{'id'} = get_uuid;
 	# Optional fields will be validated with a different function, likely at the
 	# time the document is written to a file.  A list of optional fields needs
 	# to be maintained.  ES docs typically cannot have more than 1000 fields,
