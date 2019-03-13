29e358ba6b1779d7ae8dda52e3ed3bb2ac6dd4c4
Peter Portante <peter.portante@redhat.com>
2019-03-13 19:11:34 +0000

    Remove unneeded sample data from fio-0 post-processing

--- agent/tool-scripts/postprocess/samples/fio-0/result/reference-result/tools-default/localhost/proc-interrupts/proc-interrupts.cmd
+++
@@ -1,1 +0,0 @@
-/opt/pbench-agent/tool-scripts/datalog/proc-interrupts-datalog 3 /var/lib/pbench-agent/fio_rw_2017.06.14T17.51.10/1-rw-4KiB/sample1/tools-default/proc-interrupts/proc-interrupts-stdout.txt
