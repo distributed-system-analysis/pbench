6bdb7c70cb475911eea164a8141de0bfea4674f3
gurbirkalsi <kalsigurbir@gmail.com>
2019-05-23 15:27:41 -0400

    Wrap jschart with React component for handling reloading/resetting
    `TimeseriesGraph` defined jschart as a React component and handles data
    visualization according to the React lifecycle of the component.
    
    Initial rendering occurs when the component mounts and reloading occurs
    when new data, series names, or axis series data is passed to the
    component.

---
+++ web-server/v0.4/src/components/TimeseriesGraph/index.js
@@ -0,0 +1,70 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import jschart from 'jschart';
+
+export default class TimeseriesGraph extends PureComponent {
+  static propTypes = {
+    dataSeriesNames: PropTypes.array.isRequired,
+    data: PropTypes.array.isRequired,
+    graphId: PropTypes.string.isRequired,
+    graphName: PropTypes.string,
+    graphOptions: PropTypes.object,
+    xAxisSeries: PropTypes.string.isRequired,
+    xAxisTitle: PropTypes.string,
+    yAxisTitle: PropTypes.string,
+  };
+
+  static defaultProps = {
+    graphName: null,
+    graphOptions: {},
+    xAxisTitle: null,
+    yAxisTitle: null,
+  };
+
+  componentDidMount = () => {
+    const {
+      xAxisSeries,
+      dataSeriesNames,
+      data,
+      graphId,
+      graphName,
+      xAxisTitle,
+      yAxisTitle,
+      graphOptions,
+    } = this.props;
+
+    jschart.create_jschart(0, 'timeseries', graphId, graphName, xAxisTitle, yAxisTitle, {
+      dynamic_chart: true,
+      json_object: {
+        x_axis_series: xAxisSeries,
+        data_series_names: dataSeriesNames,
+        data,
+      },
+      ...graphOptions,
+    });
+  };
+
+  componentDidUpdate = prevProps => {
+    const { data, dataSeriesNames, xAxisSeries, graphId } = this.props;
+
+    if (
+      JSON.stringify(prevProps.data) !== JSON.stringify(data) ||
+      JSON.stringify(prevProps.dataSeriesNames) !== JSON.stringify(dataSeriesNames) ||
+      prevProps.xAxisSeries !== xAxisSeries
+    ) {
+      jschart.chart_reload(graphId, {
+        json_object: {
+          x_axis_series: xAxisSeries,
+          data_series_names: dataSeriesNames,
+          data,
+        },
+      });
+    }
+  };
+
+  render() {
+    const { graphId } = this.props;
+
+    return <div id={graphId} />;
+  }
+}
