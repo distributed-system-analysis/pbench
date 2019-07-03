305734d00ec868a0bbf47488d6664d1c2d13d6ad
Peter Portante <peter.portante@redhat.com>
2019-07-03 04:30:24 +0000

    Move pbench-agent to python3

--- agent/bench-scripts/postprocess/fio-postprocess-viz.py
+++ agent/bench-scripts/postprocess/fio-postprocess-viz.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python2
-from __future__ import print_function
+#!/usr/bin/env python3
 
 import sys
 from os.path import join, basename
-try:
-    from configparser import SafeConfigParser
-except ImportError:
-    from ConfigParser import SafeConfigParser
+from configparser import ConfigParser
 
 _prog = basename(sys.argv[0])
 
@@ -22,10 +18,10 @@ html = """<html>
     <script src="/static/js/v0.3/saveSvgAsPng.js" charset="utf-8"></script>
     <div id='jschart_latency'>
       <script>
-        create_jschart(0, "%s", "jschart_latency", "Percentiles", "Time (msec)", "Latency (usec)",
-            { plotfiles: [ %s ],
+        create_jschart(0, "{}", "jschart_latency", "Percentiles", "Time (msec)", "Latency (usec)",
+            {{ plotfiles: [ {} ],
               sort_datasets: false, x_log_scale: false
-            });
+            }});
       </script>
     </div>
     <script>finish_page()</script>
@@ -44,9 +40,9 @@ def main(ctx):
     elif pct == 100.0:
       col = "max"
     else:
-      col = "p%1.0f" % pct
+      col = "p{:1.0f}".format(pct)
     columns.append(col)
-    plot_files.append("%s.log" % (col,))
+    plot_files.append("{}.log".format(col))
   columns_l = len(columns)
   with open(join(ctx.DIR, 'hist.csv'), 'r') as csv:
     line = csv.readline()
@@ -59,15 +55,15 @@ def main(ctx):
       return 1
     out_files = [open(join(ctx.DIR, pf), 'w') for pf in plot_files]
     for i in range(columns_l):
-      out_files[i].write("#LABEL:%s\n" % (columns[i],))
+      out_files[i].write("#LABEL:{}\n".format(columns[i]))
     for line in csv:
       vs = line.split(', ')
       for i in range(columns_l):
-        out_files[i].write("%d %s\n" % (int(vs[0]), vs[i+1].rstrip()))
+        out_files[i].write("{:d} {}\n".format(int(vs[0]), vs[i+1].rstrip()))
     for i in range(columns_l):
       out_files[i].close()
   chart_type = "xy"
-  cp = SafeConfigParser(allow_no_value=True)
+  cp = ConfigParser(allow_no_value=True)
   cp.read(ctx.job_file)
   for s in cp.sections():
     try:
@@ -76,12 +72,13 @@ def main(ctx):
     except Exception:
       pass
   result_file_name = join(ctx.DIR, 'results.html')
-  print("[%s] Chart Type: %s (%s)" % (_prog, chart_type, result_file_name))
+  print("[{}] Chart Type: {} ({})".format(
+      _prog, chart_type, result_file_name))
   with open(result_file_name, 'w') as fp:
-    list_of_plot_file_quoted_strings = [ "\"%s\"" % (pf,) for pf in plot_files ]
+    list_of_plot_file_quoted_strings = [ "\"{}\"".format(pf) for pf in plot_files ]
     # Note we don't plot samples.log
     concatenated_plot_file_quoted_strings = ", ".join(list_of_plot_file_quoted_strings[1:])
-    fp.write(html % (chart_type, concatenated_plot_file_quoted_strings))
+    fp.write(html.format(chart_type, concatenated_plot_file_quoted_strings))
   return 0
 
 if __name__ == '__main__':
