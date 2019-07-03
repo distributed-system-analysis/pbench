9598052f77d726956e2dfb3259e183b66f883c04
Peter Portante <peter.portante@redhat.com>
2019-07-03 04:30:24 +0000

    Move pbench-agent to python3

--- agent/bench-scripts/templates/make-fio-jobfile.py
+++ agent/bench-scripts/templates/make-fio-jobfile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 """ Constructs an fio job file with the following precedence
     from high to low:
 
@@ -15,56 +15,16 @@
 """
 import sys
 from collections import OrderedDict
-try:
-    from configparser import SafeConfigParser
-except ImportError:
-    from ConfigParser import SafeConfigParser
+from configparser import ConfigParser
 
-try:
-    SafeConfigParser.read_dict
-except AttributeError:
-
-    # Taken from configparser 3.5.0-py2.7 with minor modifications:
-    def _read_dict(self, dictionary, source='<dict>'):
-        """Read configuration from a dictionary.
-
-        Keys are section names, values are dictionaries with keys and values
-        that should be present in the section. If the used dictionary type
-        preserves order, sections and their keys will be added in order.
-
-        All types held in the dictionary are converted to strings during
-        reading, including section names, option names and keys.
-
-        Optional second argument is the `source' specifying the name of the
-        dictionary being read.
-        """
-        elements_added = set()
-        for section, keys in dictionary.items():
-            section = str(section)
-            try:
-                self.add_section(section)
-            except (DuplicateSectionError, ValueError):
-                if section in elements_added:
-                    raise
-            elements_added.add(section)
-            for key, value in keys.items():
-                key = self.optionxform(str(key))
-                if value is not None:
-                    value = str(value)
-                if (section, key) in elements_added:
-                    raise DuplicateOptionError(section, key, source)
-                elements_added.add((section, key))
-                self.set(section, key, value)
-
-    SafeConfigParser.read_dict = _read_dict
 
 def parse_config(filename):
-    cp = SafeConfigParser(allow_no_value=True)
+    cp = ConfigParser(allow_no_value=True)
     cp.read(filename)
     return cp._sections
 
 def write_config(dictionary, out=sys.stdout):
-    cp = SafeConfigParser(allow_no_value=True)
+    cp = ConfigParser(allow_no_value=True)
     cp.read_dict(dictionary)
     cp.write(out)
 
