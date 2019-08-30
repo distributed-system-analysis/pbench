712b689c68c0bb2e639b82d30957f0deea9d0bb6
Anisha1234 <aswain@redhat.com>
2019-08-30 17:12:37 +0530

    adds unit test for checking fio data metric

--- web-server/v0.4/src/pages/RunComparison/index.test.js
+++ web-server/v0.4/src/pages/RunComparison/index.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import 'jest-canvas-mock';
 
 import RunComparison from './index';
+import { parseClusteredIterations } from '../../utils/parse';
 
 const mockProps = {
   selectedControllers: ['controller1', 'controller2'],
@@ -15,6 +16,19 @@ const mockLocation = {
     clusteredGraphData: [],
   },
 };
+
+const clusteredIterations = {};
+clusteredIterations.sample_metric = [
+  {
+    benchmark_name: 'fio',
+    'throughput-sample_metric-client_hostname:all-closestsample': 0,
+    'throughput-sample_metric-client_hostname:all-mean': 0,
+    'throughput-sample_metric-client_hostname:all-stddevpct': 0,
+  },
+];
+const clusterLabels = [];
+clusterLabels.sample_metric = ['sample-1'];
+const selectedConfig = ['benchmark-sample-1'];
 
 const mockDispatch = jest.fn();
 const wrapper = shallow(
@@ -29,5 +43,10 @@ describe('test RunComparison page component', () => {
 
   it('render multiple user selected controllers', () => {
     expect(wrapper.instance().props.selectedControllers).toEqual(['controller1', 'controller2']);
+  });
+
+  it('displays correct metric data', () => {
+    const result = parseClusteredIterations(clusteredIterations, clusterLabels, selectedConfig);
+    expect(result.tableData.sample_metric[0].primaryMetric).toEqual('sample_metric');
   });
 });
