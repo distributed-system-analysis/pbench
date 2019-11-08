d485f9c4023913f74923fd4c4d32f904d1e85c6a
Peter Portante <peter.portante@redhat.com>
2019-11-08 16:50:47 -0500

    Refactor code base on operational review
    First we change all uses of `[ ]` to `[[ ]]` so that we are using Bash
    Shell syntax everywhere instead of Bourne shell, and we use the `${}`
    form of variable name references to avoid problems in context when
    expanding.
    
    Second we remove redundant code, and fix up the permissions before we
    move the tree into place.  We also do a better job of using the variables
    we have defined, and cleaning up when results and users links encounter
    errors.

--- server/bin/pbench-unpack-tarballs.sh
+++ server/bin/pbench-unpack-tarballs.sh
@@ -38,53 +38,48 @@
 
 
 # load common things
-. $dir/pbench-base.sh
+. ${dir}/pbench-base.sh
 
 # check that all the directories exist
-test -d $ARCHIVE || doexit "Bad ARCHIVE=$ARCHIVE"
-test -d $INCOMING || doexit "Bad INCOMING=$INCOMING"
-test -d $RESULTS || doexit "Bad RESULTS=$RESULTS"
-test -d $USERS || doexit "Bad USERS=$USERS"
+test -d ${ARCHIVE} || doexit "Bad ARCHIVE=${ARCHIVE}"
+test -d ${INCOMING} || doexit "Bad INCOMING=${INCOMING}"
+test -d ${RESULTS} || doexit "Bad RESULTS=${RESULTS}"
+test -d ${USERS} || doexit "Bad USERS=${USERS}"
 
-# make sure only one copy is running.
-# Use 'flock -n $LOCKFILE /home/pbench/bin/pbench-unpack-tarballs' in the
-# crontab to ensure that only one copy is running. The script itself
-# does not use any locking.
-
-# the link source and destination(s) for this script
+# The link source and destination(s) for this script.
 linksrc=TO-UNPACK
 linkdest=UNPACKED
 linkdestlist=$(getconf.py -l unpacked-states pbench-server)
 
-tmp=$TMP/$PROG.$$
-trap 'rm -rf $tmp' EXIT INT QUIT
+tmp=${TMP}/${PROG}.${$}
+trap 'rm -rf ${tmp}' EXIT INT QUIT
 
-mkdir -p $tmp || doexit "Failed to create $tmp"
+mkdir -p ${tmp} || doexit "Failed to create ${tmp}"
 
-log_init $PROG
+log_init ${PROG}
 
-echo $TS
+echo ${TS}
 
 # Accumulate errors and logs in files for reporting at the end.
-mail_content=$tmp/mail_content.log
-index_content=$tmp/index_mail_contents
+mail_content=${tmp}/mail_content.log
+index_content=${tmp}/index_mail_contents
 
 # get the list of files we'll be operating on - sort them by size
-list=$tmp/$PROG.list
+list=${tmp}/${PROG}.list
 
-# Find all the links in all the $ARCHIVE/<controller>/$linksrc
+# Find all the links in all the ${ARCHIVE}/<controller>/${linksrc}
 # directories, emitting a list of their full paths with the size
 # in bytes of the file the link points to, and then sort them so
 # that we process the smallest tar balls first.
 > ${list}.unsorted
-# First we find all the $linksrc directories
-for linksrc_dir in $(find $ARCHIVE/ -maxdepth 2 -type d -name $linksrc); do
-    # Find all the links in a given $linksrc directory that are
+# First we find all the ${linksrc} directories
+for linksrc_dir in $(find ${ARCHIVE}/ -maxdepth 2 -type d -name ${linksrc}); do
+    # Find all the links in a given ${linksrc} directory that are
     # links to actual files (bad links are not emitted!).
-    find -L $linksrc_dir -type f -name '*.tar.xz' -printf "%s %p\n" 2>/dev/null >> ${list}.unsorted
-    # Find all the links in the same $linksrc directory that don't
+    find -L ${linksrc_dir} -type f -name '*.tar.xz' -printf "%s %p\n" 2>/dev/null >> ${list}.unsorted
+    # Find all the links in the same ${linksrc} directory that don't
     # link to anything so that we can count them as errors below.
-    find -L $linksrc_dir -type l -name '*.tar.xz' -printf "%s %p\n" 2>/dev/null >> ${list}.unsorted
+    find -L ${linksrc_dir} -type l -name '*.tar.xz' -printf "%s %p\n" 2>/dev/null >> ${list}.unsorted
 done
 sort -n ${list}.unsorted > ${list}
 rm -f ${list}.unsorted
@@ -96,196 +91,178 @@ typeset -i ndups=0
 typeset -i nwarn=0
 
 # Initialize report content
-> $mail_content
-> $index_content
+> ${mail_content}
+> ${index_content}
 
 while read size result ;do
-    ntotal=$ntotal+1
+    ntotal=${ntotal}+1
 
-    link=$(readlink -e $result)
-    if [ -z "$link" ] ;then
-        echo "$TS: symlink target for $result does not exist" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
+    link=$(readlink -e ${result})
+    if [[ -z "${link}" ]]; then
+        echo "${TS}: symlink target for ${result} does not exist" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
         # FIXME: quarantine $result
         continue
     fi
-    resultname=$(basename $result)
+    resultname=$(basename ${result})
     resultname=${resultname%.tar.xz}
 
     # XXXX - for now, if it's a duplicate name, just punt and avoid
     # producing the error
-    if [ ${resultname%%.*} == "DUPLICATE__NAME" ] ;then
-        ndups=$ndups+1
+    if [[ ${resultname%%.*} == "DUPLICATE__NAME" ]]; then
+        ndups=${ndups}+1
         # FIXME: quarantine $result
         continue
     fi
 
-    basedir=$(dirname $link)
-    hostname=$(basename $basedir)
+    basedir=$(dirname ${link})
+    hostname=$(basename ${basedir})
 
-    # make sure that all the relevant state directories exist
-    mk_dirs $hostname
-    # ... and a couple of other necessities.
-    if [[ $? -ne 0 ]] ;then
-        echo "$TS: Creation of $hostname processing directories failed for $result: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
+    # Make sure that all the relevant state directories exist
+    mk_dirs ${hostname}
+    status=${?}
+    if [[ ${?} -ne 0 ]]; then
+        echo "${TS}: Creation of ${hostname} processing directories failed for ${result}: code ${status}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
         continue
     fi
-    mkdir -p $INCOMING/$hostname
-    if [[ $? -ne 0 ]] ;then
-        echo "$TS: Creation of $INCOMING/$hostname failed for $result: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
-        continue
-    fi
-    incoming=$INCOMING/$hostname/$resultname
-    if [ -e $incoming ]; then
-        echo "$TS: Incoming result, $INCOMING/$hostname/$resultname/, already exists, skipping $result" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
+
+    incoming=${INCOMING}/${hostname}/${resultname}
+    if [[ -e ${incoming} ]]; then
+        echo "${TS}: Incoming result, ${incoming}, already exists, skipping ${result}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
         # FIXME: quarantine $result
         continue
     fi
 
-    mkdir -p $INCOMING/$hostname
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: mkdir -p $INCOMING/$hostname failed for $result: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
-        continue
-    fi
-    if [ -e $INCOMING/$hostname/${resultname} ]; then
-        echo "$TS: $INCOMING/$hostname/${resultname} already exists for $result" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
-        continue
-    fi
-
-    mkdir $INCOMING/$hostname/${resultname}.unpack
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: 'mkdir ${resultname}.unpack' failed for $result: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
+    mkdir -p ${incoming}.unpack
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: 'mkdir ${incoming}.unpack' failed for ${result}: code ${status}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
         popd > /dev/null 2>&1
         continue
     fi
     let start_time=$(timestamp-seconds-since-epoch)
-    tar --extract --no-same-owner --touch --delay-directory-restore --file="$result" --force-local --directory="$INCOMING/$hostname/${resultname}.unpack"
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: 'tar -xf $result' failed: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
+    tar --extract --no-same-owner --touch --delay-directory-restore --file="${result}" --force-local --directory="${incoming}.unpack"
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: 'tar -xf ${result}' failed: code ${status}" | tee -a ${mail_content} >&4
+        rm -rf ${incoming}.unpack
+        nerrs=${nerrs}+1
         continue
-    fi
-    mv $INCOMING/$hostname/${resultname}.unpack/${resultname} $INCOMING/$hostname/${resultname}
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: '$result' does not contain ${resultname} directory at the top level; skipping" | tee -a $mail_content >&4
-        rm -rf $INCOMING/$hostname/${resultname}.unpack
-        nerrs=$nerrs+1
-        # FIXME: quarantine $result
-        continue
-    fi
-    rmdir $INCOMING/$hostname/${resultname}.unpack
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: WARNING - '$result' should only contain the ${resultname} directory at the top level, ignoring other content" | tee -a $mail_content >&4
-        rm -rf $INCOMING/$hostname/${resultname}.unpack
-        nwarn=$nwarn+1
     fi
 
     # chmod directories to at least 555
-    find $INCOMING/$hostname/${resultname} -type d -print0 | xargs -0 chmod ugo+rx
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: 'chmod ugo+rx' of subdirs $resultname for $result failed: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        rm -rf $INCOMING/$hostname/$resultname
-        # FIXME: quarantine $result
+    find ${incoming}.unpack/${resultname} -type d -print0 | xargs -0 chmod ugo+rx
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: 'chmod ugo+rx' of subdirs ${resultname} for ${result} failed: code ${status}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
+        rm -rf ${incoming}.unpack
         continue
     fi
 
     # chmod files to at least 444
-    chmod -R ugo+r $INCOMING/$hostname/$resultname
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: 'chmod -R ugo+r $resultname' for $result failed: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        rm -rf $INCOMING/$hostname/$resultname
+    chmod -R ugo+r ${incoming}.unpack/${resultname}
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: 'chmod -R ugo+r ${resultname}' for ${result} failed: code ${status}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
+        rm -rf ${incoming}.unpack
+        continue
+    fi
+
+    # Move the final unpacked tar ball into place
+    mv ${incoming}.unpack/${resultname} ${INCOMING}/${hostname}/
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: '${result}' does not contain ${resultname} directory at the top level; skipping" | tee -a ${mail_content} >&4
+        rm -rf ${incoming}.unpack
+        nerrs=${nerrs}+1
         # FIXME: quarantine $result
         continue
+    fi
+    rmdir ${incoming}.unpack
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: WARNING - '${result}' should only contain the ${resultname} directory at the top level, ignoring other content" | tee -a ${mail_content} >&4
+        rm -rf ${incoming}.unpack
+        nwarn=${nwarn}+1
     fi
 
     # Version 002 agents use the metadata log to store a prefix.
     # They may also store a user option in the metadata log.
     # We check for both of these here (n.b. if nothing is found
     # they are going to be empty strings):
-    prefix=$(getconf.py -C $INCOMING/$hostname/$resultname/metadata.log prefix run)
-    user=$(getconf.py -C $INCOMING/$hostname/$resultname/metadata.log user run)
+    prefix=$(getconf.py -C ${INCOMING}/${hostname}/${resultname}/metadata.log prefix run)
+    user=$(getconf.py -C ${INCOMING}/${hostname}/${resultname}/metadata.log user run)
 
     # Version 001 agents use a prefix file.  If there is a prefix file,
     # create a link as specified in the prefix file.  pbench-dispatch
     # has already moved it to the .prefix subdir
-    prefixfile=$basedir/.prefix/$resultname.prefix
-    if [ -f $prefixfile ] ;then
-        # add the slash to make both cases uniform in what follows
-        prefix=$(cat $prefixfile)
+    prefixfile=${basedir}/.prefix/${resultname}.prefix
+    if [[ -f ${prefixfile} ]]; then
+        prefix=$(cat ${prefixfile})
     fi
 
     # if non-empty and does not contain a trailing slash, add one
-    if [ ! -z $prefix -a ${prefix%/} = ${prefix} ] ;then
+    if [[ ! -z "${prefix}" && "${prefix%/}" = "${prefix}" ]]; then
         prefix=${prefix}/
     fi
 
-    mkdir -p $RESULTS/$hostname/$prefix
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: mkdir -p $RESULTS/$hostname/$prefix for $result failed: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
-        rm -rf $INCOMING/$hostname/$resultname
+    mkdir -p ${RESULTS}/${hostname}/${prefix}
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: mkdir -p ${RESULTS}/${hostname}/${prefix} for ${result} failed: code ${status}" | tee -a ${mail_content} >&4
+        rm -rf ${incoming}
+        nerrs=${nerrs}+1
         continue
-    else
-        # make a link in results/
-        echo "ln -s $incoming $RESULTS/$hostname/$prefix$resultname"
-        ln -s $incoming $RESULTS/$hostname/$prefix$resultname
-        status=$?
-        if [[ $status -ne 0 ]] ;then
-            echo "$TS: ln -s $incoming $RESULTS/$hostname/$prefix$resultname for $result failed: code $status" | tee -a $mail_content >&4
-            nerrs=$nerrs+1
-            rm -rf $incoming
-            rm -rf $INCOMING/$hostname/$resultname
+    fi
+    # make a link in results/
+    echo "ln -s ${incoming} ${RESULTS}/${hostname}/${prefix}${resultname}"
+    ln -s ${incoming} ${RESULTS}/${hostname}/${prefix}${resultname}
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: ln -s ${incoming} ${RESULTS}/${hostname}/${prefix}${resultname} for ${result} failed: code ${status}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
+        rm -rf ${incoming}
+        continue
+    fi
+
+    if [[ ! -z ${user} ]]; then
+        # make a link in users/ but first make sure the directory exists
+        mkdir -p ${USERS}/${user}/${hostname}/${prefix}
+        status=${?}
+        if [[ ${status} -ne 0 ]]; then
+            echo "${TS}: mkdir -p ${USERS}/${user}/${hostname}/${prefix} for ${result} failed: code ${status}" | tee -a ${mail_content} >&4
+            rm -rf ${incoming}
+            rm -f ${RESULTS}/${hostname}/${prefix}${resultname}
+            nerrs=${nerrs}+1
             continue
         fi
 
-        if [ ! -z ${user} ] ;then
-            # make a link in users/ but first make sure the directory exists
-            mkdir -p ${USERS}/${user}/${hostname}/${prefix}
-            echo "ln -s ${incoming} $USERS/${user}/${hostname}/${prefix}${resultname}"
-            ln -s ${incoming} $USERS/${user}/${hostname}/${prefix}${resultname}
-            status=$?
-            if [[ $status -ne 0 ]] ;then
-                echo "$TS: code $status: ln -s ${incoming} $USERS/${user}/${hostname}/${prefix}${resultname}" | tee -a $mail_content >&4
-                nerrs=$nerrs+1
-                rm -rf ${incoming}
-                rm -rf ${INCOMING}/${hostname}/${resultname}
-                rm -rf ${RESULTS}/${hostname}/${prefix}${resultname}
-                continue
-            fi
+        echo "ln -s ${incoming} ${USERS}/${user}/${hostname}/${prefix}${resultname}"
+        ln -s ${incoming} ${USERS}/${user}/${hostname}/${prefix}${resultname}
+        status=${?}
+        if [[ ${status} -ne 0 ]]; then
+            echo "${TS}: code ${status}: ln -s ${incoming} ${USERS}/${user}/${hostname}/${prefix}${resultname}" | tee -a ${mail_content} >&4
+            nerrs=${nerrs}+1
+            rm -rf ${incoming}
+            rm -f ${RESULTS}/${hostname}/${prefix}${resultname}
+            continue
         fi
     fi
 
-    mv $ARCHIVE/$hostname/$linksrc/$resultname.tar.xz $ARCHIVE/$hostname/$linkdest/$resultname.tar.xz
-    status=$?
-    if [[ $status -ne 0 ]] ;then
-        echo "$TS: Cannot move symlink to $ARCHIVE/$hostname/$resultname.tar.xz from $linksrc to $linkdest: code $status" | tee -a $mail_content >&4
-        nerrs=$nerrs+1
+    mv ${ARCHIVE}/${hostname}/${linksrc}/${resultname}.tar.xz ${ARCHIVE}/${hostname}/${linkdest}/${resultname}.tar.xz
+    status=${?}
+    if [[ ${status} -ne 0 ]]; then
+        echo "${TS}: Cannot move symlink ${ARCHIVE}/${hostname}/${resultname}.tar.xz from ${linksrc} to ${linkdest}: code ${status}" | tee -a ${mail_content} >&4
+        nerrs=${nerrs}+1
         # Cleanup needed here but trap takes care of it.
         rm -rf ${incoming}
-        rm -rf ${INCOMING}/${hostname}/${resultname}
-        rm $RESULTS/$hostname/$prefix$resultname
+        rm -f ${RESULTS}/${hostname}/${prefix}${resultname}
+        rm -f ${USERS}/${user}/${hostname}/${prefix}${resultname}
         continue
     fi
 
@@ -295,15 +272,15 @@ while read size result ;do
     let totsuc=0
     for state in ${linkdestlist}; do
         ln -sf ${ARCHIVE}/${hostname}/${resultname}.tar.xz ${ARCHIVE}/${hostname}/${state}/${resultname}.tar.xz
-        status=$?
-        if [ ${status} -eq 0 ] ;then
+        status=${?}
+        if [[ ${status} -eq 0 ]]; then
             let totsuc+=1
         else
-            echo "$TS: Cannot create ${ARCHIVE}/${hostname}/${resultname}.tar.xz link to $state: code $status" | tee -a $mail_content >&4
+            echo "${TS}: Cannot create ${ARCHIVE}/${hostname}/${resultname}.tar.xz link in state ${state}: code ${status}" | tee -a ${mail_content} >&4
             let toterr+=1
         fi
     done
-    if [ $toterr -gt 0 ]; then
+    if [[ ${toterr} -gt 0 ]]; then
         # Count N link creations as one error since it is for handling of a
         # single tarball.
         let nerrs+=1
@@ -312,21 +289,21 @@ while read size result ;do
     let end_time=$(timestamp-seconds-since-epoch)
     let duration=end_time-start_time
     # log the success
-    echo "$TS: $hostname/$resultname: success - elapsed time (secs): $duration - size (bytes): $size"
-    ntb=$ntb+1
-done < $list
+    echo "${TS}: ${hostname}/${resultname}: success - elapsed time (secs): ${duration} - size (bytes): ${size}"
+    ntb=${ntb}+1
+done < ${list}
 
-echo "$TS: Processed $ntb tarballs"
+echo "${TS}: Processed ${ntb} tarballs"
 
 log_finish
 
-subj="$PROG.$TS($PBENCH_ENV) - w/ $nerrs errors"
-cat << EOF > $index_content
-$subj
-Processed $ntotal result tar balls, $ntb successfully, $nwarn warnings, $nerrs errors, and $ndups duplicates
+subj="${PROG}.${TS}(${PBENCH_ENV}) - w/ ${nerrs} errors"
+cat << EOF > ${index_content}
+${subj}
+Processed ${ntotal} result tar balls, ${ntb} successfully, ${nwarn} warnings, ${nerrs} errors, and ${ndups} duplicates
 
 EOF
-cat $mail_content >> $index_content
+cat ${mail_content} >> ${index_content}
 pbench-report-status --name ${PROG} --pid ${$} --timestamp $(timestamp) --type status ${index_content}
 
 exit 0
