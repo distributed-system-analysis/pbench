6aa57b7c5103122958eeb6fa12fdce8d963f18a8
Anisha1234 <aswain@redhat.com>
2020-01-06 16:51:25 +0530

    Implements feature to edit session description

--- web-server/v0.4/src/services/explore.js
+++ web-server/v0.4/src/services/explore.js
@@ -1,6 +1,6 @@
 import request from '../utils/request';
 
-export default async function querySharedSessions(params) {
+export async function querySharedSessions(params) {
   const { datastoreConfig } = params;
 
   const endpoint = `${datastoreConfig.graphql}`;
@@ -16,6 +16,32 @@ export default async function querySharedSessions(params) {
               createdAt
           }   
         }`,
+    },
+  });
+}
+
+export async function queryEditDescription(params) {
+  const { datastoreConfig, id, value } = params;
+
+  const endpoint = `${datastoreConfig.graphql}`;
+
+  return request.post(endpoint, {
+    data: {
+      query: `
+        mutation($description: String!,$id: ID!) {
+        updateUrl(
+          data: {description: $description}
+          where: {id: $id})
+        {
+          id
+          config
+          description
+        }
+      }`,
+      variables: {
+        id,
+        description: value,
+      },
     },
   });
 }
