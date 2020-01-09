3fb4270cc891e0c673502a4f8224f53a41ea43c2
Anisha1234 <aswain@redhat.com>
2020-01-09 12:54:05 +0530

    small fixes

--- web-server/v0.4/src/pages/Explore/index.test.js
+++ web-server/v0.4/src/pages/Explore/index.test.js
@@ -23,11 +23,11 @@ describe('test delete session feature', () => {
     expect(wrapper.find(Table).length).toEqual(1);
   });
   it('render of the edit session description columns', () => {
-    expect(wrapper.find(Table).props().columns.length).toEqual(5);
+    expect(wrapper.find(Table).props().columns.length).toEqual(6);
     expect(wrapper.find(Table).props().columns[3].title).toEqual('Edit');
   });
   it('render of the delete session columns', () => {
-    expect(wrapper.find(Table).props().columns.length).toEqual(5);
-    expect(wrapper.find(Table).props().columns[4].title).toEqual('Delete');
+    expect(wrapper.find(Table).props().columns.length).toEqual(6);
+    expect(wrapper.find(Table).props().columns[5].title).toEqual('Delete');
   });
 });
