358288cfbc0f379a5c478524334e5c45997341e7
Gonzalo Rafuls <grafuls@redhat.com>
2020-02-07 03:15:14 -0500

    feat: pbench-server REST API
    Added pbench-server to be executed by systemctl.
    Additional changes required on RPM build spec.
    
    feat: agent side of transfer REST
    
    feat: moved util-scripts to own utils dir
    
    final prototype for move_results
    
    feat: moved read in chunks and post to copy_results_tb
    
    reverted util-scripts changes
    
    fixed revert
    
    feat: refactored pbench-make-result-tb to python
    
    feat: converted copy-results-tb
    
    added get on pbench-server for check sum
    
    fix: formatting on logger
    
    feat: added first test for pbench-server
    
    added test for post
    
    added tests plus mod gitignore
    
    feat: fixed and moved tests around for pbench-server REST
    
    feat: fixes to tests plus metadata.log fixure cleanup
    
    general refactoring
    
    feat: reafactored and added tests
    
    feat: gral tests refactoring
    
    removed config invocation for agent
    
    added host_info entrypoint test
    
    feat: tests refactoring
    
    feat: added pytests to travis
    
    fix: travisyml conflicts
    
    fix: removed new line on init
    
    fix: travis yml python path
    
    fix: travis yml python script
    
    fix: added missing libraries for travis
    
    fix: added werkzeug library to travis install
    
    fix: moved python rpm package to be systems one
    
    fix: added system site packages for travis virtualenv
    
    feat: added tests and pytest.ini
    
    fix: log formatting plus added tests
    
    fix: fixes to TCRT tests
    
    Addressing review comments
    
    Moved agent utils to lib/pbench and config ammendments
    
    fix to travis.yml and tests references
    
    general refactoring
    
    Added libs to pythonpath for travis
    
    removed unused import
    
    fix to travis configtools import
    
    Added _PBENCH_*_CONFIG env variables to travis
    
    Fix for fixtures paths
    
    fix on test fixture variables
    
    fix for travis env yaml
    
    fix for server tests
    
    Refactored config on server side to accomodate to pytest
    
    moved logger to init
    
    fixed configtools refactor
    
    fix for config fixture install_dir
    
    Fix for test copy results
    
    fix to server tests
    
    Fixes to server tests
    
    fixed flake8 issues
    
    black lint fixes
    
    made config methods
    
    blacked
    
    Fixes to config
    
    Added argparse for move_results plus removed check entrypoint for server
    
    Addressing comments from PR
    
    Fixes to agent tests
    
    fix for test copy result
    
    Fixes to test move results fixtures
    
    fix to tox pythonpath
    
    fix for pytest execution
    
    Fixed requirements/removed pytest-flask
    
    running pytest via python
    
    Fixes to pythonpath for tox
    
    fixes to tox pythonpath
    
    moved pythonpath to envdir in tox
    
    Added build-system to pytproject toml
    
    added requires to build-system
    
    Fix configtools import
    
    fix configtools import
    
    moved rpm package to pipy
    
    removed sitepackages from tox
    
    Fixes to fixtures
    
    removed rpy-py-installer
    
    Removed rpm for host info
    
    modified env from VIRTUAL_ENV to PYTHONPATH
    
    Fix for server test setup
    
    removed fixtures scope plus redundant packages on test-requirements
    
    Added fixture decorator
    
    Added pytest-flask package
    
    fixes to server conftest
    
    blacked
    
    fixes to server tests
    
    Final fixes to tests
    
    blacked
    
    fixes to add-metalog-option
    
    additional fixes

---
+++ server/lib/pbench_server/pbench_server.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+import hashlib
+import os
+
+from flask import request, jsonify, Flask
+from flask_restful import Resource, abort, Api
+from werkzeug.utils import secure_filename
+
+from pbench import get_pbench_logger
+from pbench_server.config import ServerConfig
+
+ALLOWED_EXTENSIONS = {"xz"}
+
+app = None
+
+
+def allowed_file(filename):
+    try:
+        fn = filename.rsplit(".", 1)[1].lower()
+    except IndexError:
+        return False
+    allowed = "." in filename and fn in ALLOWED_EXTENSIONS
+    return allowed
+
+
+def register_endpoints(api, app):
+    api.add_resource(Upload, f"{app.config['REST_URI']}/upload")
+    api.add_resource(HostInfo, f"{app.config['REST_URI']}/host_info")
+
+
+def create_app():
+    global app
+
+    config = ServerConfig()
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    app.config_pbench = config.get_pbench_config()
+    app.config_server = config.get_server_config()
+
+    app.config["PORT"] = app.config_server.get("rest_port")
+    app.config["VERSION"] = app.config_server.get("rest_version")
+    app.config["MAX_CONTENT_LENGTH"] = app.config_server.get("rest_max_content_length")
+    app.config["REST_URI"] = app.config_server.get("rest_uri")
+    app.config["LOG"] = app.config_server.get("rest_log")
+
+    app.logger = get_pbench_logger(__name__, app.config_pbench)
+
+    register_endpoints(api, app)
+
+    return app
+
+
+class HostInfo(Resource):
+    def get(self):
+        global app
+        try:
+            response = jsonify(
+                dict(
+                    message=f"{app.config_server.get('user')}@{app.config_server.get('host')}"
+                    f":{app.config_server.get('pbench-receive-dir-prefix')}-002"
+                )
+            )
+        except Exception:
+            app.logger.debug("There was something wrong constructing the host info.")
+            abort(400, message="There was something wrong with your request")
+        response.status_code = 200
+        return response
+
+
+class Upload(Resource):
+    def post(self):
+        global app
+        if not request.headers.get("filename"):
+            app.logger.debug("Missed filename in header")
+            abort(400, message="Missing filename header in request")
+        filename = secure_filename(request.headers.get("filename"))
+
+        if not request.headers.get("md5sum"):
+            app.logger.debug("Missed md5sum in header")
+            abort(400, message="Missing md5sum header in request")
+        md5sum = request.headers.get("md5sum")
+
+        app.logger.debug(f"Receiving file: {filename}")
+        if not allowed_file(filename):
+            app.logger.debug("Bad file extension received")
+            abort(400, message="File extension not supported. Only .xz")
+
+        upload_directory = app.config_server.get("pbench-receive-dir-prefix")
+        if not upload_directory:
+            app.logger.debug("Missing config variable for pbench-receive-dir-prefix")
+            abort(400, message="Missing config variable for pbench-receive-dir-prefix")
+        if not os.path.exists(upload_directory):
+            os.mkdir(upload_directory)
+        full_path = os.path.join(upload_directory, filename)
+
+        try:
+            with open(full_path, "wb") as f:
+                chunk_size = 4096
+                app.logger.debug("Writing chunks")
+                hash_md5 = hashlib.md5()
+
+                while True:
+                    chunk = request.stream.read(chunk_size)
+                    if len(chunk) == 0:
+                        break
+
+                    f.write(chunk)
+                    hash_md5.update(chunk)
+        except Exception:
+            abort(400, message=f"There was something wrong uploading {filename}")
+
+        if hash_md5.hexdigest() != md5sum:
+            abort(400, message=f"md5sum check failed for {filename}")
+
+        response = jsonify(dict(message="File successfully uploaded"))
+        response.status_code = 201
+        return response
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, port=app.config["PORT"])
