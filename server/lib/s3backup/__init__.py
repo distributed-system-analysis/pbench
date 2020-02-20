cf8b46be2c4d51afabfcb3c3ba9ed2e99cbfe489
Nick Dokos <ndokos@redhat.com>
2020-02-19 22:44:13 -0500

    S3 backup: Large object fixes
    Various bug fixes in backup, verify and the library.
    A substantial amount of debug logging has been added to
    the code.
    
    The mock implementation of S3 was modified substantially to fix
    a bunch of problems. Basically, it was not behaving like the real
    thing and the differences were causing problems. The details can
    be found in the README file which will be in the last commit of
    this series.
    
    The most serious problem was that the mock get_object() was wrongly
    assuming that the object exists already. It is now modified to raise a
    NoSuchKey exception. The generic get_tarball_header() routine is
    modified to do impedance matching for the exceptions: the real S3
    client raises ClientError and then we have to look in the response to
    see what kind of error it is, but if it is a NoSuchKey error, we raise
    our own NoSuchKey exception in that case as well.  backup_to_s3() then
    catches that exception and acts accordingly.
    
    Mock S3 implementation README file
    ----------------------------------
    The mock implementation is described in some detail. Examples of the
    real S3 implementation, together with some descriptions of its quirks,
    are provided as motivation for the mock implementation.
    
    The file should be helpful in understanding the unit tests (old and
    new).
    
    Unit test changes
    -----------------
    
    Gold files required changes because of additional debug logging in
    the backup scripts.
    
    The test-6.* gold files required additional changes because of fixes
    to the mock S3 connector code in the library (see the s3backup/README
    file for details).
    
    The test-9.* gold files (verify backup) required additional changes
    caused by bugs in the verify script, as well as the fixes to the mock
    S3 connector code in the library (see the s3backup/README file for
    details).
    
    New tests for large object functionality
    ----------------------------------------
    
    6.15 is derived from 6.12: the backups are already in place, so we
    check that redoing the backup is idempotent (as long as the checksums
    match).
    
    6.16 and 6.17 test error conditions from the S3 server.
    
    9.8 adds a large object test for testing the verify program.

--- server/lib/s3backup/__init__.py
+++ server/lib/s3backup/__init__.py
@@ -17,6 +17,14 @@ class Status(Enum):
     SUCCESS = 0
     FAIL = 1
 
+class NoSuchKey(Exception):
+    pass
+
+class MockGetObjectException(Exception):
+    """A generic exception for testing - only raised by the mocked get_object()
+    so that get_tarball_header() can be tested.
+    """
+    pass
 
 class Entry(object):
     """An Entry object consists of a name (of an S3 object) and the MD5
@@ -33,6 +41,8 @@ class Entry(object):
     def __eq__(self, other):
         return self.name == other.name and self.md5 == other.md5
 
+    def __str__(self):
+        return "{} :: {}".format(self.name, self.md5)
 
 class S3Config(object):
 
@@ -54,9 +64,9 @@ class S3Config(object):
             multipart_chunksize=self.chunk_size)
         self.logger = logger
         if debug_unittest:
-            self.connector = MockS3Connector(config)
+            self.connector = MockS3Connector(config, logger)
         else:
-            self.connector = S3Connector(config)
+            self.connector = S3Connector(config, logger)
 
         # Normally, the connector defines a bucket_name attribute as
         # specified in the config file.  The application layer does
@@ -83,38 +93,25 @@ class S3Config(object):
 
     # pass through to the corresponding connector method
     def get_tarball_header(self,  Bucket=None, Key=None):
-        return self.connector.get_object(Bucket=Bucket, Key=Key)
-
-    @staticmethod
-    def calculate_multipart_etag(tb, chunk_size):
-        md5s = []
-
-        with open(tb, 'rb') as fp:
-            while True:
-                data = fp.read(chunk_size)
-                if not data:
-                    break
-                md5s.append(hashlib.md5(data))
-
-        if len(md5s) > 1:
-            digests = b"".join(m.digest() for m in md5s)
-            new_md5 = hashlib.md5(digests)
-            new_etag = '{}-{}'.format(new_md5.hexdigest(), len(md5s))
-        elif len(md5s) == 1:
-            # file smaller than chunk size
-            new_etag = '{}'.format(md5s[0].hexdigest())
+        try:
+            resp = self.connector.get_object(Bucket=Bucket, Key=Key)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                raise NoSuchKey
+            else:
+                raise
+        except Exception:
+            raise
         else:
-            new_etag = ''
-
-        return new_etag
+            return resp
 
     def put_tarball(self, Name=None, Body=None, Size=0, ContentMD5=None, Bucket=None, Key=None):
-        md5_base64_value = (base64.b64encode(
-            bytes.fromhex(ContentMD5))).decode()
+
         if Size < (5 * self.GB):
             try:
-                self.connector.put_object(
-                    Bucket=self.bucket_name, Key=Key, Body=Body, ContentMD5=md5_base64_value)
+                # The S3 put_object() expects ContentMD5 to be base64-encoded.
+                self.connector.put_object(Bucket=self.bucket_name, Key=Key, Body=Body,
+                                          ContentMD5=s3_contentMD5(ContentMD5))
             except ConnectionClosedError:
                 # This is a transient failure and will be retried at
                 # the next invocation of the backups.
@@ -134,7 +131,9 @@ class S3Config(object):
                 return Status.SUCCESS
         else:
             # calculate multi etag value
-            etag = self.calculate_multipart_etag(Name, self.chunk_size)
+            etag = self.connector.calculate_multipart_etag(Name, self.chunk_size)
+            if not etag:
+                return Status.FAIL
             try:
                 self.connector.upload_fileobj(
                     Body=Body,
@@ -143,8 +142,10 @@ class S3Config(object):
                     Config=self.transfer_config,
                     ExtraArgs={
                         'Metadata': {
-                            'ETag': etag,
-                            'MD5': ContentMD5
+                            # S3 insists on lower-casing these field names, so
+                            # we succumb in order to avoid confusion.
+                            'etag': etag,
+                            'md5': ContentMD5
                         }
                     })
             except ClientError as e:
@@ -168,8 +169,9 @@ class S3Config(object):
                         self.logger.info("Multi-upload to s3 succeeded: {}", Key)
                         return Status.SUCCESS
                     else:
-                        # delete object from s3 and move to specific
-                        # state directory for retry
+                        # delete object from s3
+                        # TBD: this should be flagged for retry, but
+                        # currently we just fail.
                         self.connector.delete_object(
                             Bucket=self.bucket_name, Key=Key)
                         self.logger.error("Multi-upload to s3 failed: {}, etag doesn't match", Key)
@@ -183,6 +185,43 @@ class S3Config(object):
 
     def list_objects(self, **kwargs):
         return self.connector.list_objects(**kwargs)
+
+    # response decoding - tbh is the header returned
+    # by get_tarball_header().
+    @staticmethod
+    def s3_md5(tbh):
+        # We need to decode the response to figure out where the MD5
+        # is stored: for a large object, it is in the Metadata field;
+        # for a small object, it is in the ETag field, but it's
+        # wrapped in double quotes which we strip.
+        # tbh is the header returned by get_tarball_header().
+        if 'Metadata' in tbh and tbh['Metadata']:
+            if 'md5' in tbh['Metadata']:
+                return tbh['Metadata']['md5']
+            else:
+                return None
+        else:
+            return tbh['ETag'].strip('"')
+
+    def header_md5(self, objh):
+        # the object header comes from a call to list_objects(): this
+        # call returns a response whose 'Contents' field is a list of
+        # object headers.  Each header contains (at least) a 'Key'
+        # field, an 'ETag' field and a 'Size field. We use the 'Size'
+        # field to distinguish whether the 'ETag' field contains an
+        # md5 sum or not: for small objects (< 5 Gib) it does; but for
+        # large objects (>= 5 Gib), we need to make an additional
+        # get_tarball_header() call to get the additional metadata
+        # that we store with the ExtraArgs parameter to upload_fileobj()
+        # in put_tarball() above.
+        if objh['Size'] < 5 * self.GB:
+            return objh['ETag'].strip('"')
+        else:
+            try:
+                obj = self.get_tarball_header(Bucket=self.bucket_name, Key=objh['Key'])
+            except Exception:
+                return None
+            return self.s3_md5(obj)
 
 ###########################################################################
 # Connectors start here.
@@ -218,10 +257,8 @@ class Connector(object):
         return os.path.getsize(tb)
 
 # Connector to the S3 service.
-
-
 class S3Connector(Connector):
-    def __init__(self, config):
+    def __init__(self, config, logger):
         # S3 backup can be turned off by commenting out the
         # [pbench-server-backup]endpoint_url option in the config
         # file.
@@ -241,6 +278,27 @@ class S3Connector(Connector):
                                          aws_secret_access_key='{}'.format(
                                              self.secret_access_key),
                                          endpoint_url='{}'.format(self.endpoint_url))
+        self.logger = logger
+
+    @staticmethod
+    def calculate_multipart_etag(tb, chunk_size):
+        md5s = []
+
+        with open(tb, 'rb') as fp:
+            for data in fp.read(chunk_size):
+                md5s.append(hashlib.md5(data))
+
+        if len(md5s) > 1:
+            digests = b"".join(m.digest() for m in md5s)
+            new_md5 = hashlib.md5(digests)
+            new_etag = '{}-{}'.format(new_md5.hexdigest(), len(md5s))
+        elif len(md5s) == 1:
+            # file smaller than chunk size
+            new_etag = md5s[0].hexdigest()
+        else:
+            new_etag = ''
+
+        return new_etag
 
     def list_objects(self, **kwargs):
         return self.s3client.list_objects_v2(**kwargs)
@@ -251,7 +309,7 @@ class S3Connector(Connector):
     def get_object(self, Bucket=None, Key=None):
         return self.s3client.get_object(Bucket=Bucket, Key=Key)
 
-    def put_object(self, Body=None, ContentMD5=None, Bucket=None, Key=None):
+    def put_object(self,  Bucket=None, Key=None, Body=None, ContentMD5=None):
         return self.s3client.put_object(Bucket=Bucket, Key=Key,
                                         Body=Body,
                                         ContentMD5=ContentMD5)
@@ -267,40 +325,61 @@ class S3Connector(Connector):
 
 # Connector to the mock "S3" service for unit testing.
 class MockS3Connector(Connector):
-    """
-    The mock object is used for unit testing. It provides a "connector"
+    """The mock object is used for unit testing. It provides a "connector"
     to the backend service that is implemented using the local
     filesystem, rather than dispatching to the real S3 backend
     service.
+
+    We store the "object" as a file together with an ETag file. For
+    "small" objects, we store the MD5sum of the file (with double
+    quotes wrapping it, just as the real S3 service returns it) in the
+    ETag file.  For "large" objects, we store a deliberately modified
+    md5sum (we bump up the first digit to the next hex digit, wrapping
+    around to 0 if necessary).
+
     """
 
     # class "constant"
     GB = 1024 ** 3
 
-    def __init__(self, config):
+    def __init__(self, config, logger):
         try:
             self.path = config.get('pbench-server-backup', 'endpoint_url')
         except (NoSectionError, NoOptionError):
             self.path = None
         else:
             self.bucket_name = config.get('pbench-server-backup', 'bucket_name')
+        self.logger = logger
+
+    @staticmethod
+    def calculate_multipart_etag(tb, chunk_size):
+        # just return a dummy string
+        return "1234567890abcde-2"
 
     def list_objects(self, **kwargs):
         ob_dict = {}
         bucketpath = os.path.join(self.path, kwargs['Bucket'])
         result_list = glob.glob(os.path.join(bucketpath, "*/*.tar.xz"))
         result_list.sort()
-        # we pretend that objects in the SPECIAL_BUCKET are large objects.
+        # We pretend that SPECIAL_BUCKET contains too many objects to
+        # be returned in one call: we'll need a continuation call to get
+        # them all.
         if kwargs['Bucket'] == "SPECIAL_BUCKET":
             if 'ContinuationToken' in kwargs.keys():
+                # return all but the first object
                 resp = self.create_ob_dict_for_list_objects(ob_dict,
                                                             bucketpath,
                                                             result_list[2:])
+                # ... and indicate that there are no more objects.
+                resp['IsTruncated'] = False
                 return resp
             else:
+                # return just the first object
                 resp = self.create_ob_dict_for_list_objects(ob_dict,
                                                             bucketpath,
                                                             result_list[:2])
+                # ... and indicate that there are more objects.
+                resp['IsTruncated'] = True
                 resp['NextContinuationToken'] = 'yes'
                 return resp
         else:
@@ -313,22 +392,30 @@ class MockS3Connector(Connector):
     def create_ob_dict_for_list_objects(ob_dict, bucketpath, result_list):
         result_name_list = []
         for i in result_list:
-            with open(i, 'rb') as f:
-                data = f.read()
-                md5 = hashlib.md5(data).hexdigest()
-            result_name_list.append({'ETag': '"{}"'.format(md5),
+            etag_file = "{}.ETag".format(i)
+            with open(etag_file) as f:
+                etag = f.read()[:-1]
+
+            # prepare to lie about the size: if there is a <foo>.tar.xz.MD5
+            # file, this is a Large Object, so we set the size to 6Gib.
+            md5_file = "{}.MD5".format(i)
+            if os.path.exists(md5_file):
+                size = 6442450944  # equivalent to 6 GB
+            else:
+                size = os.path.getsize(i)
+            result_name_list.append({'ETag': etag,
+                                     'Size': size,
                                      'Key': os.path.relpath(i, start=bucketpath)})
         ob_dict['Contents'] = result_name_list
-        ob_dict['ResponseMetadata'] = {'HTTPStatusCode': 400}
+        ob_dict['ResponseMetadata'] = {'HTTPStatusCode': 200}
         return ob_dict
 
     def put_object(self, Bucket=None, Key=None, Body=None, ContentMD5=None):
         if not self.path:
             return Status.FAIL
-        md5_hex_value = hashlib.md5(Body.read()).hexdigest()
-        md5_base64_value = (base64.b64encode(
-            bytes.fromhex(md5_hex_value))).decode()
-        if md5_base64_value == ContentMD5:
+        barray = Body.read()
+        md5 = hashlib.md5(barray).hexdigest()
+        if s3_contentMD5(md5) == ContentMD5:
             test_controller = Key.split("/")[0]
             try:
                 os.mkdir("{}/{}/{}".format(self.path,
@@ -336,8 +423,15 @@ class MockS3Connector(Connector):
             except FileExistsError:
                 # directory already exists, ignore
                 pass
-            with open('{}/{}/{}'.format(self.path, self.bucket_name, Key), 'wb') as f:
-                f.write(Body.read())
+
+            fout = '{}/{}/{}'.format(self.path, self.bucket_name, Key)
+            with open(fout, 'wb') as f:
+                f.write(barray)
+
+            etagfile = '{}/{}/{}.ETag'.format(self.path, self.bucket_name, Key)
+            with open(etagfile, 'w') as f:
+                f.write(f'"{md5}"\n')
+
             return Status.SUCCESS
         return Status.FAIL
 
@@ -352,15 +446,16 @@ class MockS3Connector(Connector):
         except FileExistsError:
             # directory already exists, ignore
             pass
+        etag = ExtraArgs['Metadata']['etag']
         with open('{}/{}/{}'.format(self.path, self.bucket_name, Key), 'wb') as f:
             f.write(Body.read())
-        with open('{}/{}/{}.multiEtag'.format(self.path, self.bucket_name, Key), 'w') as f:
-            if "6.13" in Key:
-                pass
-            else:
-                # N.B. the double quotes are intentional: they simulate
-                # what the real S3 service does with the ETag field.
-                f.write('"{}"\n'.format(ExtraArgs['Metadata']['ETag']))
+        with open('{}/{}/{}.ETag'.format(self.path, self.bucket_name, Key), 'w') as f:
+            # N.B. the double quotes are intentional: they simulate
+            # what the real S3 service does with the ETag field.
+            f.write(f'"{etag}"\n')
+        md5 = ExtraArgs['Metadata']['md5']
+        with open('{}/{}/{}.MD5'.format(self.path, self.bucket_name, Key), 'w') as f:
+            f.write(f'{md5}\n')
 
     def head_bucket(self, Bucket=None):
         if os.path.exists(os.path.join(self.path, Bucket)):
@@ -376,24 +471,74 @@ class MockS3Connector(Connector):
             return None
         ob_dict = {}
         result_path = os.path.join(self.path, Bucket, Key)
+
+
+        # Testing error cases
+        # ClientError with code "other"
+        resp_other_client_error = {'Error': {'Code': 'Other', 'Message': 'Other error from mocked S3 get_object()'}}
+        if Key == "foo/client_error_other.tar.xz":
+            raise ClientError(resp_other_client_error, 'get_object')
+
+        # some other exception
+        if (Key == "foo/get_object_exception.tar.xz"):
+            raise MockGetObjectException('Other exception from mocked S3 get_object()')
+
+        # "real" testing begins here
+        resp_no_such_key = {'Error': {'Code': 'NoSuchKey', 'Message': 'No such key from mocked S3 get_object()'}}
+        if not os.path.exists(result_path):
+            raise ClientError(resp_no_such_key, 'get_object')
+
         with open(result_path, 'rb') as f:
             data = f.read()
             md5 = hashlib.md5(data).hexdigest()
+
         ob_dict['ResponseMetadata'] = {'HTTPStatusCode': 200}
-        multi_etag_file = '{}/{}/{}.multiEtag'.format(
-                                                self.path,
-                                                self.bucket_name,
-                                                Key)
-        if os.path.exists(multi_etag_file):
-            with open(multi_etag_file) as f:
-                multi_etag_value = f.read()[:-1]
-            ob_dict['ETag'] = multi_etag_value
+        etag_file = '{}/{}/{}.ETag'.format(
+            self.path,
+            self.bucket_name,
+            Key)
+        md5_file = '{}/{}/{}.MD5'.format(
+            self.path,
+            self.bucket_name,
+            Key)
+
+        if os.path.exists(md5_file):
+            with open(etag_file) as f:
+                etag = f.read()[:-1]
+            with open(md5_file) as f:
+                md5 = f.read()[:-1]
+
+            ob_dict['ETag'] = etag
+            # lower-case field names here to agree with S3 convention: it seems
+            # to lower-case fields that are passed in ExtraArgs (see above), so
+            # we follow that convention here.
+            ob_dict['Metadata'] = {
+                'etag': etag.strip('"'),
+                'md5': md5
+            }
         else:
             ob_dict['ETag'] = '"{}"'.format(md5)
+            ob_dict['Metadata'] = {}
         return ob_dict
 
     def getsize(self, tar):
-        if '6.12' in tar:
-            return 6442450944  # equivalent to 6 GB
-        else:
-            return os.path.getsize(tar)
+        # The tar  argument is a full pathname, so it contains the test number.
+
+        # These are "large" object tests: when one is added to unittests, it also
+        # needs to be added here (unless the real size of the tarball is > 5GB which
+        # is NOT recommended :-) ).
+        lo_tests = ['6.12', '6.15', '9.8']
+        for tst in lo_tests:
+            if tst in tar:
+                return 6442450944  # equivalent to 6 GB
+        return os.path.getsize(tar)
+
+
+# utility functions
+
+# s3 put_object() wants a base64-encoded string.
+# Argument md5 is what md5sum() returns: the hexdigest() value of the md5.
+def s3_contentMD5(md5):
+    # get bytes from hex, base64-encode the bytes and then
+    # decode to a string - ugh...
+    return (base64.b64encode(bytes.fromhex(md5))).decode()
