3b1cda644a4ba51ed9586be3d6341aa08af2d57e
Chuck Short <chucks@redhat.com>
2020-03-10 09:54:11 -0400

    Fix flake8 errors in server/lib/s3backup/__init__.py
    ./server/lib/s3backup/__init__.py:6:1: F401 'sys' imported but unused
    ./server/lib/s3backup/__init__.py:10:1: F401 'shutil' imported but unused
    ./server/lib/s3backup/__init__.py:11:1: F401 'time' imported but unused
    ./server/lib/s3backup/__init__.py:12:1: F401 'configtools' imported but unused
    ./server/lib/s3backup/__init__.py:13:1: F401 'datetime.datetime' imported but unused
    ./server/lib/s3backup/__init__.py:14:1: F401 'configparser.ConfigParser' imported but unused
    
    Signed-off-by: Chuck Short <chucks@redhat.com>

--- server/lib/s3backup/__init__.py
+++ server/lib/s3backup/__init__.py
@@ -3,15 +3,10 @@ This module provides convenience functions that interface to lower-level service
 """
 import boto3
 import os
-import sys
 import glob
 import base64
 import hashlib
-import shutil
-import time
-import configtools
-from datetime import datetime
-from configparser import ConfigParser, NoSectionError, NoOptionError
+from configparser import NoSectionError, NoOptionError
 from boto3.s3.transfer import TransferConfig
 from botocore.exceptions import ConnectionClosedError, ClientError
 
