85f41ba4fcdd3bf2c2eb524f11fee5798eee9f90
Peter Portante <peter.a.portante@gmail.com>
2020-03-10 22:37:51 -0400

    Revert "Refactor tool-scripts to use one script"
    This reverts commit 2bb049e736e325920464f4454b6f5e15b56fcca5.

--- agent/tool-scripts/tests/perf/gold/tools-group/perf/perf.cmd
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# base-tool generated command file
-
-LANG=C PYTHONUNBUFFERED=True /var/tmp/pbench-test-tool-scripts/opt/pbench-agent/tool-scripts/datalog/perf-datalog /var/tmp/pbench-test-tool-scripts/perf/tools-group/perf '--a42' -g
--- agent/tool-scripts/tests/prometheus-metrics/gold/tools-group/prometheus-metrics/prometheus-metrics.cmd
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# base-tool generated command file
-
-LANG=C PYTHONUNBUFFERED=True GOPATH=/var/tmp/pbench-test-tool-scripts/go PATH=${PATH}:${GOPATH}/bin /var/tmp/pbench-test-tool-scripts/opt/pbench-agent/tool-scripts/datalog/prometheus-metrics-datalog /var/tmp/pbench-test-tool-scripts/prometheus-metrics/tools-group/prometheus-metrics 42 tests/prometheus-metrics/inventory-hosts.lis
