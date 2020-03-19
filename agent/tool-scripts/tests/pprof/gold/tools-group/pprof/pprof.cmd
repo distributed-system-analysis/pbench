c31331545ca6d27cfc4c32c444dedde760ef95c7
Peter Portante <peter.portante@redhat.com>
2020-03-19 17:29:59 -0400

    Use exec instead of fork for tools were possible
    This allows us to handle special cases for stopping tools correctly, while
    avoiding extra code to do so.

--- agent/tool-scripts/tests/pprof/gold/tools-group/pprof/pprof.cmd
+++ agent/tool-scripts/tests/pprof/gold/tools-group/pprof/pprof.cmd
@@ -1,4 +1,4 @@
 #!/bin/bash
 # base-tool generated command file
 
-LANG=C PYTHONUNBUFFERED=True GOPATH=/var/tmp/pbench-test-tool-scripts/go PATH=${PATH}:${GOPATH}/bin /var/tmp/pbench-test-tool-scripts/opt/pbench-agent/tool-scripts/datalog/pprof-datalog /var/tmp/pbench-test-tool-scripts/pprof/tools-group/pprof 42 tests/pprof/inventory-hosts.lis
+LANG=C PYTHONUNBUFFERED=True GOPATH=/var/tmp/pbench-test-tool-scripts/go PATH=${PATH}:${GOPATH}/bin exec /var/tmp/pbench-test-tool-scripts/opt/pbench-agent/tool-scripts/datalog/pprof-datalog /var/tmp/pbench-test-tool-scripts/pprof/tools-group/pprof 42 tests/pprof/inventory-hosts.lis
