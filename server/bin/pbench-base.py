df5e98d4d7d20c06abbe23aa96c296d2ff463b5a
riya <riyad17031998@gmail.com>
2020-04-07 15:46:36 +0530

    Formatted scripts using f-strings
    Fixes #1585
    
    This PR deals with using f-string's wherever possible in the
    following scripts:
    
    * pbench-report-status.py
    * pbench-base.py
    * pbench-index.py
    * pbench-satellite-state-change.py
    * pbench-verify-backup-tarballs.py
    * pbench-backup-tarballs.py

--- server/bin/pbench-base.py
+++ server/bin/pbench-base.py
@@ -41,8 +41,8 @@ if not parsed.cfg_name:
     )
     if not os.path.exists(config_name):
         print(
-            "{}: No config file specified: set _PBENCH_SERVER_CONFIG env variable or use"
-            " --config <file> on the command line".format(_prog),
+            f"{_prog}: No config file specified: set _PBENCH_SERVER_CONFIG env variable or use"
+            f" --config <file> on the command line",
             file=sys.stderr,
         )
         sys.exit(1)
@@ -53,7 +53,7 @@ else:
 try:
     config = PbenchConfig(config_name)
 except BadConfig as e:
-    print("{}: {} (config file {})".format(_prog, e, config_name), file=sys.stderr)
+    print(f"{_prog}: {e} (config file {config_name})", file=sys.stderr)
     sys.exit(1)
 
 # Exclude the "files" and "conf" attributes from being exported
@@ -69,8 +69,7 @@ for att in vars:
         os.environ[att] = getattr(config, att)
     except AttributeError:
         print(
-            '{}: Missing internal pbench attribute, "{}", in'
-            " configuration".format(_prog, att),
+            f'{_prog}: Missing internal pbench attribute, "{att}", in configuration',
             file=sys.stderr,
         )
         sys.exit(1)
@@ -78,6 +77,6 @@ for att in vars:
 if config._unittests:
     os.environ["_PBENCH_SERVER_TEST"] = "1"
 
-cmd = "{}.sh".format(sys.argv[1])
+cmd = f"{sys.argv[1]}.sh"
 args = [cmd] + sys.argv[2:]
 os.execv(cmd, args)
