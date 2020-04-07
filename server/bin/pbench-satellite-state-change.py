a5e7bf4bef95c7dd664c0d0714be47130286e6e4
riya <riyad17031998@gmail.com>
2020-04-07 15:46:36 +0530

    Formatted scripts using f-strings
    Fixes #1585
    
    This PR deals with using f-string's wherever possible in the
    following scripts:
    
    * pbench-report-status.py
    * pbench-base.py
    * pbench-index.py
    * pbench-satellite-state-change.py
    * pbench-verify-backup-tarballs.py
    * pbench-backup-tarballs.py

--- server/bin/pbench-satellite-state-change.py
+++ server/bin/pbench-satellite-state-change.py
@@ -7,17 +7,17 @@ import shutil
 _prog = os.path.basename(sys.argv[0])
 
 if len(sys.argv) < 2:
-    print("{}: Missing working directory argument".format(_prog), file=sys.stderr)
+    print(f"{_prog}: Missing working directory argument", file=sys.stderr)
     sys.exit(1)
 
 if len(sys.argv) > 2:
-    print("{}: Too many arguments ({!r})".format(_prog, sys.argv), file=sys.stderr)
+    print(f"{_prog}: Too many arguments ({sys.argv!r})", file=sys.stderr)
     sys.exit(1)
 
 try:
     os.chdir(sys.argv[1])
 except Exception as e:
-    print("{}: {}".format(_prog, e), file=sys.stderr)
+    print(f"{_prog}: {e}", file=sys.stderr)
     sys.exit(1)
 
 errors = 0
@@ -28,7 +28,7 @@ for tar in sys.stdin:
         shutil.move(src, des)
     except Exception as e:
         errors += 1
-        print("{}: {}".format(_prog, e), file=sys.stderr)
+        print(f"{_prog}: {e}", file=sys.stderr)
         continue
 
 sys.exit(1 if errors > 0 else 0)
