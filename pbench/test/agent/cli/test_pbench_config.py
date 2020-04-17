83fb1a8793ffd455c30a5b0f2722c7e33d4fe0e9
Charles Short <chucks@redhat.com>
2020-04-17 15:25:26 -0400

    Add pbench-config cli
    Unify the server/agent getconf.py so that we arent re-using the same
    code with minimal differene. Also add unittests to test output.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

---
+++ pbench/test/agent/cli/test_pbench_config.py
@@ -0,0 +1,34 @@
+import subprocess
+
+
+def capture(command):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
+    out, err = proc.communicate()
+    return out, err, proc.returncode
+
+
+def test_pbench_config():
+    command = ["pbench-config"]
+    out, err, exitcode = capture(command)
+    assert exitcode == 1
+
+
+def test_pbench_config_help():
+    command = ["pbench-config", "--help"]
+    out, err, exitcode = capture(command)
+    assert b"--help" in out
+    assert exitcode == 0
+
+
+def test_pbench_agent_config(monkeypatch, tmpdir):
+    cfg = tmpdir / "pbench-agent.cfg"
+    pbench_config = """
+    [pbench-agent]
+    pbench_run = /tmp
+    """
+    cfg.write(pbench_config)
+    monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(cfg))
+    command = ["pbench-config", "pbench_run", "pbench-agent"]
+    out, err, exitcode = capture(command)
+    assert b"/tmp" in out
+    assert exitcode == 0
