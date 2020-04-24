7db3625d716b08294252b47539364ad066ee8ffb
Charles Short <chucks@redhat.com>
2020-04-24 09:18:42 -0400

    Search for agent configuration
    Search for the agent configuration if no configuraiton is specified.
    If the configuration is not specified we check for _PBENCH_AGENT_CONFIG
    environment variable, check for the configuration in
    agent/config otherwise error out. Once we loaded a configuration file
    make sure that is valid.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- pbench/agent/config.py
+++ pbench/agent/config.py
@@ -5,14 +5,44 @@ import sys
 
 from pbench.common import configtools
 from pbench.common import exceptions
+from pbench.common.constants import AGENT_PATH
 from pbench.agent.utils import error_out
 
+def lookup_agent_configuration(filename=None):
+    """Return config file PATH"""
+    if filename is None:
+        pbench_config = os.environ.get("_PBENCH_AGENT_CONFIG", None)
+        if pbench_config is None:
+            # pbench is not always invoked with -C or --config or the
+            # _PBENCH_AGENT_CONFIG environment variable set. Since we
+            # really need access to the config file to operate, and we
+            # know the location of that config file
+            # we check to see if that exists before declaring a problem.
+            filename = os.path.join(AGENT_PATH, 
+                "config/pbench-agent-default.cfg")
+        else:
+            filename = pbench_config            
+    
+    path = pathlib.Path(filename)
+    if not path.exists():
+        error_out(f"Unable to find configuration: {filename}")
+    
+    config_files = configtools.file_list(filename)
+    config_files.reverse()
 
+    try:
+        config = configparser.ConfigParser()
+        config.read(config_files)
+    except configparser.Error as e:
+        print(e)
+        sys.exit(2)
+
+    return config
+    
 class AgentConfig:
     def __init__(self, cfg_name=None):
         self.cfg_name = cfg_name
-        self.pbench_config = self._get_agent_config()
-
+        self.pbench_config = lookup_agent_configuration(self.cfg_name)
         try:
             self.agent = self.pbench_config["pbench-agent"]
             self.results = self.pbench_config["results"]
@@ -24,26 +54,3 @@ class AgentConfig:
 
     def get_results(self):
         return self.results
-
-    def _get_agent_config(self):
-        """Agent configuration validation"""
-        if self.cfg_name is None:
-            self.cfg_name = os.environ.get("_PBENCH_AGENT_CONFIG", None)
-            if not self.cfg_name:
-                error_out(f"_PBENCH_AGENT_CONFIG is not set")
-
-        path = pathlib.Path(self.cfg_name)
-        if not path.exists():
-            error_out(f"{self.cfg_name} does not exist")
-
-        config_files = configtools.file_list(self.cfg_name)
-        config_files.reverse()
-
-        try:
-            config = configparser.ConfigParser()
-            config.read(config_files)
-        except configparser.Error as e:
-            print(e)
-            sys.exit(2)
-
-        return config
