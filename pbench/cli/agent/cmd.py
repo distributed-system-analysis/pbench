633613dfe583b9cc81cc90cba5a4c75139dc5139
Charles Short <chucks@redhat.com>
2020-04-24 11:49:48 -0400

    Add config options and debug options for agent
    Detect the configuration file that the user is going to be using for an
    individual run. If no conifiguration file one will be chosen for the
    user.
    
    Also allow to turn debugging on and off. Remove some cruft as well.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- pbench/cli/agent/cmd.py
+++ pbench/cli/agent/cmd.py
@@ -1,6 +1,7 @@
 import click
 
-from pbench.cli import options
+from pbench.agent import utils
+from pbench.cli.agent import options
 from pbench.cli.agent.commmands.log import add_metalog_option
 from pbench.cli.agent.commands.results import move_results
 
@@ -8,17 +9,21 @@ from pbench.cli.agent.commands.results import move_results
 # pbench-add-metalog
 #
 @click.command()
+@click.pass_context
+@options.pbench_agent_config
+@options.pbench_agent_debug
 @click.argument("logfile")
 @click.argument("section")
 @click.argument("option")
 @click.argument("value")
-def metalog_add(logfile, section, option, value):
+def metalog_add(ctx, config, debug, logfile, section, option, value):
     """Add an option to a section of the metadata.log file.
 
     E.g. using an 'iterations' arg for the option
     iterations: 1-iter, 2-iter, 3-iter
     where the iterations are in the <iterations.file>, one iteration per line
     """
+    ctx = utils.init_context(ctx, config, debug)
     add_metalog_option(logfile, section, option, value)
 
 
@@ -29,8 +34,12 @@ def metalog_add(logfile, section, option, value):
 # pbench backwards compat
 #
 @click.command()
+@click.pass_context
+@options.pbench_agent_config
+@options.pbench_agent_debug
 @options.pbench_upload_user
 @options.pbench_server_prefix
 @options.pbench_show_server
-def _move_results(user, prefix, show_server):
+def _move_results(ctx, config, debug, user, prefix, show_server):
+    ctx = utils.init_context(ctx, config, debug)
     move_results(user, prefix, show_server)
