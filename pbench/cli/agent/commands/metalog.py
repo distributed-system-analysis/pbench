edb726c9558ab6d6eb1a6eb0cd0d131bd1a2684c
Charles Short <chucks@redhat.com>
2020-04-24 11:14:06 -0400

    Refactor pbench-cli
    Consolidate pbench-cli to make it easier to backport commands
    so that the workload scripts can still keep on working when
    we convert more bash scripts to python.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- pbench/cli/agent/commands/metalog.py
+++ pbench/cli/agent/commands/metalog.py
@@ -25,21 +25,3 @@ def metalog_add_option(logfile, section, option, value):
     where the iterations are in the <iterations.file>, one iteration per line
     """
     add_metalog_option(logfile, section, option, value)
-
-
-#
-# pbench backwards compatible
-#
-@click.command()
-@click.argument("logfile")
-@click.argument("section")
-@click.argument("option")
-@click.argument("value")
-def metalog_add(logfile, section, option, value):
-    """Add an option to a section of the metadata.log file.
-
-    E.g. using an 'iterations' arg for the option
-    iterations: 1-iter, 2-iter, 3-iter
-    where the iterations are in the <iterations.file>, one iteration per line
-    """
-    add_metalog_option(logfile, section, option, value)
