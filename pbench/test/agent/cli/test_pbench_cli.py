24a6c5bb457946f25e638e8febfe4103a248b8dd
Charles Short <chucks@redhat.com>
2020-04-24 09:36:58 -0400

    Centralize click options
    Centralize click options so they can be shared with the backwards
    compatible commands.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- pbench/test/agent/cli/test_pbench_cli.py
+++ pbench/test/agent/cli/test_pbench_cli.py
@@ -4,12 +4,11 @@ from pbench.test.agent.cli.conftest import capture
 def test_pbench_cli():
     command = ["pbench"]
     out, err, exitcode = capture(command)
-    assert exitcode == 0
+    assert exitcode == 0, f"out={out!r} err={err!r}"
 
 
 def test_pbench_config_help():
     command = ["pbench", "--help"]
     out, err, exitcode = capture(command)
-    print(err)
-    assert b"--help" in out
-    assert exitcode == 0
+    assert b"--help" in out, f"out={out!r} err={err!r}"
+    assert exitcode == 0, f"out={out!r} err={err!r}"
