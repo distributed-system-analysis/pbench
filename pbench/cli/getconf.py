4f97cbd69ca9248f512b8bfb6b057596f9b27787
Charles Short <chucks@redhat.com>
2020-05-11 11:41:08 -0400

    Add pbench-config cli
    Unify the server/agent getconf.py so that we arent re-using the same
    code with minimal differene. Also add unittests to test output.
    
    Aso update the unit testing framework to add a "functional"
    envrionment that we can check to see if the scripts that are
    installed via setuptools are working correctly.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

---
+++ pbench/cli/getconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from pbench import configtools
+
+
+def get_pbench_config():
+    """Determine if we are using agent or server."""
+    agent_cfg = os.environ.get("_PBENCH_AGENT_CONFIG")
+    server_cfg = os.environ.get("_PBENCH_SERVER_CONFIG")
+    if agent_cfg:
+        return "_PBENCH_AGENT_CONFIG"
+    elif server_cfg:
+        return "_PBENCH_SERVER_CONFIG"
+    else:
+        return sys.exit(1)
+
+
+def main():
+    opts, args = configtools.parse_args(
+        configtools.options,
+        usage="Usage: getconf.py [options] <item>|-a <section> [<section> ...]",
+    )
+    conf, files = configtools.init(opts, get_pbench_config())
+    status = configtools.main(conf, args, opts, files)
+    sys.exit(status)
+
+
+if __name__ == "__main__":
+    main()
