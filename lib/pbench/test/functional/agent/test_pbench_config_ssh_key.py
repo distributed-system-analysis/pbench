8a38d2746332efd1202c2472120fc54e275497ea
Charles Short <chucks@redhat.com>
2020-05-25 16:00:10 +0000

    Add pbench-agent-config-ssh-key
    - Replace shell script with python script
    - Made python script installable via setuptools
    - Added unittests
    
    Signed-off-by: Charles Short <chucks@redhat.com>

---
+++ lib/pbench/test/functional/agent/test_pbench_config_ssh_key.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+def test_pbench_config_ssh_key_help():
+    command = ["pbench-agent-config-ssh-key", "--help"]
+    out, err, exitcode = pytest.helpers.capture(command)
+    assert exitcode == 0
+    assert b"--help" in out
+
+
+def test_pbench_config_ssh_key_help_failed():
+    command = ["pbench-agent-config-ssh-key", "--foo"]
+    out, err, exitcode = pytest.helpers.capture(command)
+    assert exitcode == 2
+
+
+def test_pbench_config_ssh_key(
+    tmpdir, create_agent_environment, pbench_installdir, pbench_conf
+):
+    fake_key = tmpdir / "id_rsa"
+    fake_key.write("")
+
+    command = ["pbench-agent-config-ssh-key", pbench_conf, fake_key]
+    out, err, exitcode = pytest.helpers.capture(command)
+    assert exitcode == 0
+    fake_key = pbench_installdir / "id_rsa"
+    assert fake_key.exists() is True
