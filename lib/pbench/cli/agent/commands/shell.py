a5851f0e96693d984f6dbacd7d6f2a5b709286e5
Charles Short <chucks@redhat.com>
2020-06-14 02:01:05 +0000

    Add version option
    Add '--version' option so that we can debug which version
    of pbench the user is using.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/cli/agent/commands/shell.py
+++ lib/pbench/cli/agent/commands/shell.py
@@ -1,10 +1,12 @@
 import click
+import pbr.version
 
 
 @click.group()
 @click.option(
     "--debug/--no-debug", help="Enable or disable debug mode, Default is disabled"
 )
+@click.version_option(version=pbr.version.VersionInfo("pbench"))
 @click.pass_context
 def main(ctxt, debug):
     ctxt.obj = {}
