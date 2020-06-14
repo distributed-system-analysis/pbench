22141b6de923356632f9351f478e5e0da8cf5334
Charles Short <chucks@redhat.com>
2020-06-14 02:18:58 +0000

    Add autocomplete support
    Add support to use bash autocomplete.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/cli/agent/commands/shell.py
+++ lib/pbench/cli/agent/commands/shell.py
@@ -1,5 +1,8 @@
 import click
+import click_completion
 import pbr.version
+
+click_completion.init()
 
 
 @click.group()
@@ -9,6 +12,13 @@ import pbr.version
 @click.version_option(version=pbr.version.VersionInfo("pbench"))
 @click.pass_context
 def main(ctxt, debug):
+    """
+    A benchmarking and performance analysis framework.
+
+    Enable autocomplete issue:
+
+     eval "$(_PBENCH_COMPLETE=source molecule)"
+    """
     ctxt.obj = {}
     ctxt.obj["args"] = {}
     ctxt.obj["args"]["debug"] = debug
