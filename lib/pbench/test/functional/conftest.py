bffeb70ea7abd52037bbe7e31c2f285bfa0f5c56
Charles Short <chucks@redhat.com>
2020-06-14 02:27:46 +0000

    Add simple functional tests
    Add simple functional tests to make sure that pbench binary
    gets installed correctly and works as expected.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/test/functional/conftest.py
+++ lib/pbench/test/functional/conftest.py
@@ -1,6 +1,9 @@
+import pytest
+
 import subprocess
 
 
+@pytest.helpers.register
 def capture(command):
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
     out, err = proc.communicate()
