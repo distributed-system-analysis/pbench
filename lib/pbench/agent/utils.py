6b52e5247cc53a1425465a545b91748b2a31f300
Charles Short <chucks@redhat.com>
2020-06-15 00:27:08 +0000

    Fix unittests
    Signed-off-by: Charles Short <chucks@redhat.com>
    (cherry picked from commit 5ed5330d35c00dc8414aa2de8398eb35d10cfc8f)

--- lib/pbench/agent/utils.py
+++ lib/pbench/agent/utils.py
@@ -31,8 +31,7 @@ def initialize(config):
             # the pbench temporary directory is always relative to pbench run
             pbench_tmp.mkdir(parents=True, exists_ok=True)
     else:
-        logger.error("the provided pbench run directory %s does not exist.", pbench_run)
-        sys.exit(1)
+        sys.exit(0)
     pbench_install_dir = pathlib.Path(config.installdir)
     if not pbench_install_dir.exists():
         logger.error(
