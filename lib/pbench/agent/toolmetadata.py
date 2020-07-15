cb3434e8c63f868a3caec44e37bd658a2e0b3785
maxusmusti <meyceoz@redhat.com>
2020-07-15 17:17:36 -0400

    Prometheus/Node_Exporter Full V1 Integration Commit
    This work adds the notion of a "collector" to the Tool Data Sink, and
    "persistent tools" which run continuously without cycling through the
    "start", "stop", and "send" phases.  The collector is responsible for
    continuously pulling data from those tools which are now started during
    the new "init" phase, and stopped during the new "end" phase.
    
    The first actual implementation of this kind of collector is for the
    Prometheus data collection environment, where a `node-exporter`
    "persistent tool" is run providing an end-point for a prometheus server
    "collector" to pull data from it and store it locally off the run
    directory (`${benchmark_run_dir}`,
    e.g. `${benchmark_run_dir}/collector/prometheus`).
    
    This work adds the `tool-scripts/meta.json` file which is used to
    describe which tools are persistent and which are transient (default).

---
+++ lib/pbench/agent/toolmetadata.py
@@ -0,0 +1,115 @@
+from pathlib import Path
+import json
+import os
+
+
+class ToolMetadataExc(Exception):
+    pass
+
+
+class ToolMetadata:
+    def __init__(self, mode, context, logger):
+        self.logger = logger
+        assert mode in (
+            "redis",
+            "json",
+        ), f"Logic bomb! Unexpected mode, {mode}, encountered constructing tool meta data"
+        assert (
+            context
+        ), "Logic bomb! No context given on ToolMetadata object construction"
+        self.mode = mode
+        if mode == "redis":
+            self.redis_server = context
+            self.json_file = None
+        else:
+            self.redis_server = None
+            json_path = Path(context, "tool-scripts", "meta.json")
+            try:
+                self.json = json_path.resolve(strict=True)
+            except FileNotFoundError:
+                raise ToolMetadataExc(f"missing {json_path}")
+            except Exception:
+                raise
+        self.data = self.__getInitialData()
+
+    def __getInitialData(self):
+        if self.mode == "json":
+            if not os.path.isfile(self.json):
+                self.logger.error(
+                    "There is no tool-scripts/meta.json in given install dir"
+                )
+                return None
+            with self.json.open("r") as json_file:
+                metadata = json.load(json_file)
+        elif self.mode == "redis":
+            try:
+                meta_raw = self.redis_server.get("tool-metadata")
+            except Exception:
+                self.logger.exception(
+                    "Failure to fetch tool metadata from the Redis server"
+                )
+                raise
+            else:
+                if meta_raw is None:
+                    self.logger.error("Metadata has not been loaded into redis yet")
+                    return None
+            try:
+                metadata = json.loads(meta_raw.decode("utf-8"))
+            except Exception as exc:
+                self.logger.error(
+                    "Bad metadata loaded into Redis server, '%s', json=%r",
+                    exc,
+                    meta_raw,
+                )
+                return None
+        return metadata
+
+    def __dataCheck(self):
+        """Check for existing/loadable data, return True if retreival possible, False otherwise"""
+        if not self.data:
+            self.data == self.__getInitialData()
+            if not self.data:
+                self.logger.error(f"Unable to access data through {self.mode}")
+                return False
+        return True
+
+    def getFullData(self):
+        if self.__dataCheck():
+            return self.data
+        return None
+
+    def getPersistentTools(self):
+        if self.__dataCheck():
+            return list(self.data["persistent"].keys())
+        return None
+
+    def getTransientTools(self):
+        if self.__dataCheck():
+            return list(self.data["transient"].keys())
+        return None
+
+    def getProperties(self, tool):
+        if tool in self.data["persistent"].keys():
+            return self.data["persistent"][tool]
+        elif tool in self.data["transient"].keys():
+            return self.data["transient"][tool]
+
+    def loadIntoRedis(self, info):
+        if self.mode == "redis":
+            try:
+                self.json = Path(info).resolve(strict=True)
+            except FileNotFoundError:
+                raise ToolMetadataExc(f"missing {info}")
+            except Exception:
+                raise
+        elif self.mode == "json":
+            self.redis_server = info
+
+        try:
+            with self.json.open("r") as json_file:
+                metadata = json.load(json_file)
+                self.redis_server.set("tool-metadata", json.dumps(metadata))
+        except Exception:
+            self.logger.error("Failed to load the data into redis")
+            raise
+        return None
