c00ecb87200d1cde0d71d5b9f5e6005d8bc12e10
fuwang <fuwang@redhat.com>
2020-07-21 10:59:48 -0400

    Fix conflicts

--- lib/pbench/server/api/__init__.py
+++ lib/pbench/server/api/__init__.py
@@ -1,13 +1,15 @@
-import hashlib
 import os
+import sys
+import hashlib
 import requests
 
+from pathlib import Path
 from flask import request, jsonify, Flask, make_response
 from flask_restful import Resource, abort, Api
 from werkzeug.utils import secure_filename
 
-from pbench.server.logger import get_pbench_logger
-from pbench.server.api.config import ServerConfig
+from pbench.common.logger import get_pbench_logger
+from pbench.server import PbenchServerConfig
 
 ALLOWED_EXTENSIONS = {"xz"}
 
@@ -32,13 +34,32 @@ def register_endpoints(api, app):
 def create_app():
     global app
 
-    config = ServerConfig()
+    cfg_name = os.environ.get("_PBENCH_SERVER_CONFIG")
+    if not cfg_name:
+        print(
+            f"{__name__}: ERROR: No config file specified; set"
+            " _PBENCH_SERVER_CONFIG",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    config = PbenchServerConfig(cfg_name)
 
     app = Flask(__name__)
     api = Api(app)
 
-    app.config_pbench = config.get_pbench_config()
-    app.config_server = config.get_server_config()
+    app.logger = get_pbench_logger(__name__, config)
+
+    app.config_server = config.conf["pbench-server"]
+
+    prdp = app.config_server.get("pbench-receive-dir-prefix")
+    try:
+        upload_directory = Path(f"{prdp}-002").resolve(strict=True)
+    except Exception:
+        app.logger.error("Missing config variable for pbench-receive-dir-prefix")
+        sys.exit(1)
+    else:
+        app.upload_directory = upload_directory
 
     app.config["PORT"] = app.config_server.get("rest_port")
     app.config["VERSION"] = app.config_server.get("rest_version")
@@ -112,8 +133,10 @@ class HostInfo(Resource):
                 )
             )
         except Exception:
-            app.logger.debug("There was something wrong constructing the host info.")
-            abort(400, message="There was something wrong with your request")
+            app.logger.exception(
+                "There was something wrong constructing the host info."
+            )
+            abort(500, message="There was something wrong with your request")
         response.status_code = 200
         return response
 
@@ -131,18 +154,12 @@ class Upload(Resource):
             abort(400, message="Missing md5sum header in request")
         md5sum = request.headers.get("md5sum")
 
-        app.logger.debug(f"Receiving file: {filename}")
+        app.logger.debug("Receiving file: {}", filename)
         if not allowed_file(filename):
             app.logger.debug("Bad file extension received")
             abort(400, message="File extension not supported. Only .xz")
 
-        upload_directory = app.config_server.get("pbench-receive-dir-prefix")
-        if not upload_directory:
-            app.logger.debug("Missing config variable for pbench-receive-dir-prefix")
-            abort(400, message="Missing config variable for pbench-receive-dir-prefix")
-        if not os.path.exists(upload_directory):
-            os.mkdir(upload_directory)
-        full_path = os.path.join(upload_directory, filename)
+        full_path = app.upload_directory / filename
 
         try:
             with open(full_path, "wb") as f:
@@ -158,7 +175,8 @@ class Upload(Resource):
                     f.write(chunk)
                     hash_md5.update(chunk)
         except Exception:
-            abort(400, message=f"There was something wrong uploading {filename}")
+            app.logger.exception("There was something wrong uploading {}", filename)
+            abort(500, message=f"There was something wrong uploading {filename}")
 
         if hash_md5.hexdigest() != md5sum:
             abort(400, message=f"md5sum check failed for {filename}")
