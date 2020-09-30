8661135f4ddb57efffa1fd1f801c49ecd8a0c4ea
Charles Short <chucks@redhat.com>
2020-09-30 10:32:45 -0400

    Pythonize pbench-avg-stddev
    Replace pbench-avg-stddev with python script and make it installable.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

---
+++ lib/pbench/cli/agent/commands/stddev.py
@@ -0,0 +1,25 @@
+"""
+pbench-avg-stddev
+
+
+input: a bunch of numbers on cmd line
+output: an average (mean), sample standard deviation, sample standard deviation percent, and which input was closest to the avgerage
+"""
+
+import click
+
+import numpy as np
+
+
+@click.command()
+@click.argument("values", nargs=-1, type=click.FLOAT)
+def main(values):
+    values = list(values)
+    avg = np.average(values)
+    stddev = np.std(values)
+    stddevpct = 100 * stddev / avg
+
+    values = np.array(values)
+    closest_index = np.abs(values - avg).argmin() + 1
+
+    print("%.4f %.4f %.4f %d" % (avg, stddev, stddevpct, closest_index))
