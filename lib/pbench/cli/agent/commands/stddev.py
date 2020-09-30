5f9ccf969ada432e0427e49dcf703b2172854207
Charles Short <chucks@redhat.com>
2020-09-30 13:39:10 -0400

    Drop numpy
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/cli/agent/commands/stddev.py
+++ lib/pbench/cli/agent/commands/stddev.py
@@ -11,18 +11,17 @@ nargs = -1, accepts an unlimited number of arguments
 
 import click
 
-import numpy as np
+import statistics
 
 
 @click.command()
 @click.argument("values", nargs=-1, type=click.FLOAT)
 def main(values):
     values = list(values)
-    avg = np.average(values)
-    stddev = np.std(values)
+    avg = statistics.mean(values)
+    stddev = statistics.stdev(values)
     stddevpct = 100 * stddev / avg
 
-    values = np.array(values)
-    closest_index = np.abs(values - avg).argmin() + 1
+    closest_index = min(range(len(value)), key=lambda i: abs(value[i] - avg))
 
     print("%.4f %.4f %.4f %d" % (avg, stddev, stddevpct, closest_index))
