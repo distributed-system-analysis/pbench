bd1ed05ef0eb3148a3cb434496503aef136eac55
npalaska <npalaska@redhat.com>
2020-09-29 20:39:02 -0400

    Add gunicorn wsgi server configuration
    - Run gunicorn through pbench-server script
    - save gunicorn pid to a file in /run/pbench-server dir for clean stop

--- lib/pbench/cli/server/shell.py
+++ lib/pbench/cli/server/shell.py
@@ -1,8 +1,23 @@
 #!/bin/env python3
 
 from pbench.server.api import create_app
+import subprocess
 
 
 def main():
     app = create_app()
-    app.run(debug=True, port=app.config["PORT"])
+    port = app.config["PORT"]
+    host = app.config["BIND_HOST"]
+    workers = app.config["WORKERS"]
+    subprocess.run(
+        [
+            "gunicorn",
+            "--workers",
+            str(workers),
+            "--pid",
+            "/run/pbench-server/gunicorn.pid",
+            "--bind",
+            f"{str(host)}:{str(port)}",
+            "pbench.server.api:create_app()",
+        ]
+    )
