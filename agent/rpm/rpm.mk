c2b64ac1a5d640698ef8aa5b26189b48f5790f24
Peter Portante <peter.portante@redhat.com>
2020-10-05 15:14:26 -0400

    Refactor agent RPM infrastructure to DRY
    We take a page from the `b0.69` branch, and refactor the Makefile to avoid
    having to build the source tree twice, once for the source tar ball, once
    when running the RPM build.  Now we build the install tree for the source
    tar ball and just use that directly.
    
    We also refactor the agent source tree `Makefile` so that we only add what
    we need to the source tar ball, allowing us to drop the use of the
    `exclude.pats` file.
    
    Further, we switch to using a Jinja2 template for the `pbench-agent.spec`
    file, allowing us to drop the need for the `set-version-release` script.

--- agent/rpm/rpm.mk
+++ agent/rpm/rpm.mk
@@ -4,21 +4,15 @@ check:
 	if [ "${version}" == "" ] ;then echo "version undefined" > /dev/stderr; exit 1 ;fi
 	if [ "${prog}" == "" ] ;then echo "prog undefined" > /dev/stderr ; exit 2 ;fi
 
+RPMDIRS = BUILD BUILDROOT SPECS SOURCES SRPMS RPMS
+
 .PHONY: rpm-dirs
 rpm-dirs:
-	for i in BUILD BUILDROOT SPECS SOURCES SRPMS RPMS; do mkdir -p ${HOME}/rpmbuild/$$i ; done
+	for i in ${RPMDIRS}; do mkdir -p ${HOME}/rpmbuild/$$i ; done
 
 .PHONY: rpm-clean
-rpm-clean: rpm-dirs
-	for i in BUILD BUILDROOT SPECS SOURCES SRPMS RPMS; do rm -rf ${HOME}/rpmbuild/$$i/* ; done
-
-dot-clean:
-	rm -f .rpm-copy .spec-copy .build
-
-.PHONY: spec-copy
-spec-copy: .spec-copy
-.spec-copy:
-	set-version-release ${version} $(prog).spec ${USE_GIT_SHA1} > ${HOME}/rpmbuild/SPECS/${prog}.spec && touch .spec-copy
+rpm-clean:
+	for i in ${RPMDIRS}; do rm -rf ${HOME}/rpmbuild/$$i/* ; done
 
 ###########################################################################
 # these are used in the Makefiles that include this file
@@ -48,8 +42,5 @@ copr-index \
 copr-inotify \
 copr-dashboard:	$(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm
 	copr-cli build $(_copr_user)/$(subst copr,pbench,$@) $(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
-
-# The default clean target
-clean:: dot-clean
 
 veryclean:: clean rpm-clean
