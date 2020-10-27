0a35891a4ccde008b7dc38f0f9a9dc231013cea1
Anisha1234 <aswain@redhat.com>
2020-10-27 12:49:02 +0530

    Adds ability to reference tabs on 'Getting Started' and 'Learn More' page

--- js/tab.js
+++ js/tab.js
@@ -1,5 +1,21 @@
-function openTab(evt, cityName){
-    var i, tabcontent, tablinks;
+function manageHash(defaultID){
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    let locationId = window.location.hash.substring(1)
+    locationId = locationId === ''?defaultID:locationId
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(locationId).style.display = "block";
+    document.getElementById(locationId+"Link").classList.add("active")
+}
+
+function openTab(evt,idName){
+    let i, tabcontent, tablinks;
+    window.location.hash  = idName;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -8,6 +24,6 @@ function openTab(evt, cityName){
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(idName).style.display = "block";
     evt.currentTarget.classList.add("active")
 }
