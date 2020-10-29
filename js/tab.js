0d3c46b2be590a8d915332982240968fe2345864
Anisha1234 <aswain@redhat.com>
2020-10-29 10:17:09 +0530

    addition of function description
    The tab names,are not unique by the implementation and are given unique id to each of them according to their names(i.e id of setup is "setup", id of "best practices is "best").
    
    In order to add a new tab, we just need to make sure that the `content_div` id( i.e the content division needed to be displayed) is unique and the tabLink (i.e the id of each tab button) is the combination of the `content_div` id and "Link"
    
    for example, in the setup tab (line no. 67 in start.html);
    `<button id="setupLink" class="tablinks active" onClick="openTab(event,'setup')">Setup</button>`
    The tab button points to the `content_div` with id `setup` on click and the id for the button itself is `content_id(i.e setup)+"Link"` which becomes `setupLink`

--- js/tab.js
+++ js/tab.js
@@ -1,29 +1,53 @@
+//The manageHash function deals with the content changes on hitting a url with a particular tab id
 function manageHash(defaultID){
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    let locationId = window.location.hash.substring(1)
-    locationId = locationId === ''?defaultID:locationId
+    let tabcontent, tablinks;
+    //find contentId(id of the div, needed to be displayed) from the url 
+    let contentId = window.location.hash.substring(1)
+
+    //if no hash(id of div/tab) is given, take first tab as default tab
+    contentId = contentId === ''?defaultID:contentId
+
+    //On click on a tab, usually all the divisions go hidden except the content with targented id name 
+    //converting all the content div display to none
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    document.getElementById(locationId).style.display = "block";
-    document.getElementById(locationId+"Link").classList.add("active")
+    //visualizing the content according the calculated contentId(id of the div, needed to be displayed)
+    document.getElementById(contentId).style.display = "block";
+
+    // We need to add active class to the tablinks(tab buttons) in order to add css changes for an active tab
+    // remove all active tabs for all the tablinks as we don't know whic tab was active before
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // adding active class to the tab button which is clicked 
+    // Note: when we directly enter url, we do not know which element is clicked to get the active tab button
+    // so, we have by defualt set the the button id as "content_id+'Link'" i.e: tab_button_id = content_id+"Link"
+    document.getElementById(contentId+"Link").classList.add("active")   
 }
 
+//The function executes while changing tabs on click
+//On click, we know which tab we are changing to with dom and receive the id of the tab button and the content division
 function openTab(evt,idName){
-    let i, tabcontent, tablinks;
+    let tabcontent, tablinks;
+    // changing the url as per the tab change
     window.location.hash  = idName;
+
+     //converting all the content div display to none
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+    //visualizing the content of the tab according paseed id of the tab content
+    document.getElementById(idName).style.display = "block";
+
+    //remove all active tabs for all the tablinks as we don't know whic tab was active before
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(idName).style.display = "block";
-    evt.currentTarget.classList.add("active")
+    //adding active class to the tab button which is clicked 
+    evt.currentTarget.classList.add("active")  
 }
