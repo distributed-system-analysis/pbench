1a2e9f96097171bb890fd1ec867125cab8097381
Charles Short <chucks@redhat.com>
2020-11-02 09:06:10 -0500

    Rejig functional unit tests
    Use pytest helpers to create a reproducable envrionment
    that we do with the shell scripts. Eventually, this
    will replace the shell scripts and the gold files
    that we *shudder* use.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/test/functional/conftest.py
+++ lib/pbench/test/functional/conftest.py
@@ -1,7 +1,25 @@
+import os
+import subprocess
+
 import pytest
+
 from pbench.test.unit.agent.conftest import base_setup
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup(request, pytestconfig):
     return base_setup(request, pytestconfig)
+
+
+@pytest.helpers.register
+def capture(command):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    return out, err, proc.returncode
+
+
+@pytest.fixture
+def pbench_run(tmpdir):
+    pbench_run = tmpdir / "test/var/lib/pbench-agent"
+    os.makedirs(pbench_run)
+    yield pbench_run
