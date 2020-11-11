2186e0b37c29e6270533fa6639ef24ec3fb1c75c
Anisha1234 <aswain@redhat.com>
2020-11-11 12:33:59 +0530

    fixes accordion cards functionality

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,7 @@
 var acc = document.getElementsByClassName("accordion");
+window.addEventListener('load', (event) => {
 	for (i = 0; i < acc.length; i++) {
-		acc[i].addEventListener("click", function() {
+		acc[i].addEventListener("click", function () {
 			/* Toggle between adding and removing the "active" class,
 			to highlight the button that controls the panel */
 			this.classList.toggle("active");
@@ -18,19 +19,20 @@ var acc = document.getElementsByClassName("accordion");
 				panel.style.display = "block";
 				panel.style.borderLeft = "3px solid var(--pf-global--primary-color--100)"
 			}
-			if(this.classList.contains('active')){
+			if (this.classList.contains('active')) {
 				accorBtn.style.color = "var(--pf-global--Color--light-100)";
 				accorDesc.style.fontWeight = "700";
 				accorBtn.style.backgroundColor = "var(--pf-global--primary-color--100)"
 				accorDesc.style.color = "var(--pf-global--Color--light-100)";
-				accorBtn.childNodes[1].classList.replace("fa-angle-right","fa-angle-down")
+				accorBtn.childNodes[1].classList.replace("fa-angle-right", "fa-angle-down")
 			}
-			else{
-				arrowIcon.style.color= "var(--pf-color-black-300)";
+			else {
+				arrowIcon.style.color = "var(--pf-color-black-300)";
 				accorDesc.style.fontWeight = "400";
 				accorBtn.style.backgroundColor = "var(--pf-global--Color--light-100)"
 				accorDesc.style.color = "var(--pf-color-black-300)";
-				accorBtn.childNodes[1].classList.replace("fa-angle-down","fa-angle-right")
+				accorBtn.childNodes[1].classList.replace("fa-angle-down", "fa-angle-right")
 			}
 		});
 	}
+});
