3d120f55e01eda2c0b3fdecfe769f81c3def837b
Charles Short <chucks@redhat.com>
2020-11-16 09:56:42 -0500

    Remove duplicate code
    Remove duplicate capture method.
    
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/test/common/conftest.py
+++ lib/pbench/test/common/conftest.py
@@ -11,13 +11,6 @@ def setup(request, pytestconfig):
     return base_setup(request, pytestconfig)
 
 
-@pytest.helpers.register
-def capture(command):
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = proc.communicate()
-    return out, err, proc.returncode
-
-
 @pytest.fixture
 def pbench_run(tmpdir):
     pbench_run = tmpdir / "test/var/lib/pbench-agent"
---
+++ lib/pbench/test/conftest.py
@@ -0,0 +1,9 @@
+import subprocess
+
+import pytest
+
+@pytest.helpers.register
+def capture(command):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    return out, err, proc.returncode
