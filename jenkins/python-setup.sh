342f4e37e7d709a39101b328aa96e3246d4ced03
Peter Portante <peter.portante@redhat.com>
2020-11-17 09:58:50 -0500

    Add newline instead of a space

--- jenkins/python-setup.sh
+++ jenkins/python-setup.sh
@@ -7,8 +7,10 @@ if [[ ":${PATH:-}:" != *":${_pdir}:"* ]]; then
     export PATH=${_pdir}${PATH:+:${PATH}}
 fi
 
+NL=$'\n'
 while read -r _pdir; do
     if [[ ":${PYTHONPATH:-}:" != *":${_pdir}:"* ]]; then
-        export PYTHONPATH=${_pdir}${PYTHONPATH:+:${PYTHONPATH}}
+        PYTHONPATH=${_pdir}${PYTHONPATH:+:${PYTHONPATH}}
     fi
-done <<< "$(ls -1d ${_prefix}/lib*/python3.*/site-packages 2> /dev/null) $(head -n 1 ${_prefix}/lib*/python3.*/site-packages/pbench.egg-link 2> /dev/null)"
+done <<< "$(ls -1d ${_prefix}/lib*/python3.*/site-packages 2> /dev/null)${NL}$(head -n 1 ${_prefix}/lib*/python3.*/site-packages/pbench.egg-link 2> /dev/null)"
+export PYTHONPATH
