184c438eb201f96157ca31f7b01a0f4804bbd02a
arzoo14 <arzoopprajapati11@gmail.com>
2020-11-19 06:54:07 +0530

    dropdown cards added

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.autosectionlabel'
+extensions = [ 'sphinx.ext.autosectionlabel', 'sphinx_panels',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
