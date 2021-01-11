16f7a9524a3c131d4b4b6dd45f0635bdeb6bd5b0
Peter Portante <peter.portante@redhat.com>
2021-01-11 13:20:42 -0500

    Add 99.5 percentile support

--- agent/bench-scripts/test-bin/fio-histo-log-pctiles.py
+++ agent/bench-scripts/test-bin/fio-histo-log-pctiles.py
@@ -2,5 +2,5 @@
 
 echo "$0 $*" >> $_testlog
 
-echo "ts, samples, min, median, p90, p95, p99, max"
-echo "1000, 0, 1, 2, 3, 4, 5, 6"
+echo "ts, samples, min, median, p90, p95, p99, p99.5, max"
+echo "1000, 0, 1, 2, 3, 4, 5, 6, 7"
