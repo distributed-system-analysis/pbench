40d1142a74b9186f085a92e7b06b812dff79d335
Peter Portante <peter.portante@redhat.com>
2021-02-03 22:50:45 -0500

    Only emit the benchmark exit code on failure

---
+++ agent/bench-scripts/tests/pbench-trafficgen/test-41.trafficgen/binary-search.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -z "${*}" ]]; then
+    echo "${0}" >> ${_testlog}
+else
+    echo "${0} ${*}" >> ${_testlog}
+fi
+
+echo "Starting something other than a binary-search"
+exit 42
