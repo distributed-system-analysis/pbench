01b4cbd5a496c85dda832c407ec9f1eb961e9daa
Peter Portante <peter.portante@redhat.com>
2021-02-04 16:28:49 -0500

    Ensure pbench develop install occurs for tests
    In order for the Jenkins jobs to run correctly, we need to ensure that
    the pbench python3 module is installed, including the CLI binaries that
    need to be on the `PATH`.
    
    We also correct the version of `flake8` to be the one available in
    Fedora 32 (`3.7.7`), and add the missing RPMs to the development
    container to avoid unnecessary pip3 installs.  We also create a
    separate `docs/requirements.txt` file.
    
    Finally, we update the branch name file to properly reference `main`
    so that we use the right image tag the development container.

--- jenkins/python-setup.sh
+++ jenkins/python-setup.sh
@@ -1,6 +1,11 @@
 _prefix="${1}"
 
-pip3 install --no-cache-dir --progress-bar off --no-color --prefix="${_prefix}" -r ${progdir}/lint-requirements.txt -r ${progdir}/agent/requirements.txt -r ${progdir}/server/requirements.txt -r ${progdir}/agent/test-requirements.txt -r ${progdir}/server/test-requirements.txt
+if [[ -z "${_prefix}" ]]; then
+    printf -- "Missing prefix argument!" >&2
+    exit 1
+fi
+
+pip3 install --no-cache-dir --progress-bar off --no-color --prefix="${_prefix}" -r ${progdir}/lint-requirements.txt -r ${progdir}/docs/requirements.txt -r ${progdir}/agent/requirements.txt -r ${progdir}/server/requirements.txt -r ${progdir}/agent/test-requirements.txt -r ${progdir}/server/test-requirements.txt
 
 _pdir=${_prefix}/bin
 if [[ ":${PATH:-}:" != *":${_pdir}:"* ]]; then
@@ -14,3 +19,5 @@ while read -r _pdir; do
     fi
 done <<< "$(ls -1d ${_prefix}/lib*/python3.*/site-packages 2> /dev/null)${NL}$(head -n 1 ${_prefix}/lib*/python3.*/site-packages/pbench.egg-link 2> /dev/null)"
 export PYTHONPATH
+
+python3 setup.py develop --prefix="${_prefix}"
