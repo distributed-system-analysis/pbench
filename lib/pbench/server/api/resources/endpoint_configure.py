70b2693d613b575cebe73b2f27d3344e5f0f01d6
David Butenhof <dbutenho@redhat.com>
2021-03-19 16:33:55 -0400

    Move to automatic reverse-proxy configuration

--- lib/pbench/server/api/resources/endpoint_configure.py
+++ lib/pbench/server/api/resources/endpoint_configure.py
@@ -1,5 +1,8 @@
+import re
+
 from flask_restful import Resource, abort
-from flask import jsonify
+from flask import request, jsonify
+from urllib.parse import urljoin
 
 from pbench.server.api.resources.query_apis import get_index_prefix
 
@@ -18,20 +21,30 @@ class EndpointConfig(Resource):
         Args:
             config (PbenchServerConfig): server config values
             logger (Logger): message logging
+
+        Report the server configuration to a web client; web access can be
+        redirected through an external reverse proxy like NGINX by setting
+        the "proxy_host" configuration setting in the "pbench-server"
+        section of the pbench-server.cfg file. By default, the Pbench
+        server uses a local Apache reverse proxy routing through the HTTP
+        port (80), but this can be changed to any host name and port. All
+        server endpoints will be reported with respect to that address.
         """
         self.logger = logger
-        self.host = config.get("pbench-server", "proxy_host")
-        uri_prefix = config.rest_uri
-        self.uri = f"{self.host}{uri_prefix}"
+        self.host = config.get("pbench-server", "host")
+        self.uri_prefix = config.rest_uri
         self.prefix = get_index_prefix(config)
         self.commit_id = config.COMMIT_ID
+        self.forward_pattern = re.compile(r";\s*host\s*=\s*(?P<host>[^;\s]*)")
+        self.x_forward_pattern = re.compile(r"^\s*(?P<host>[^;\s,]*)")
 
     def get(self):
         """
-        Return server configuration information required by the UI
-        dashboard. This includes:
+        Return server configuration information required by web clients
+        including the Pbench dashboard UI. This includes:
 
         metadata: Information about the server configuration
+            identification: The Pbench server name and version
             result_index: The "root" index name for Pbench result data,
                 qualified by the current index version and prefix. In the
                 current ES schema, this is "v5.result-data-sample."
@@ -44,9 +57,8 @@ class EndpointConfig(Resource):
             run_toc_index: The Elasticsearch V7 index for run TOC data. In
                 the current ES schema, this is "v6.run-toc."
         api:    A list of the server APIs supported; we give a name, which
-                corresponds to either the current config.json property name
-                (e.g., "elasticsearch") or the current src/service method name
-                being replaced (e.g., "queryControllers")
+                identifies the service, and the full URI relative to the
+                configured host name and port (local or remote reverse proxy)
             results: Direct access to Pbench data sets for the dashboard; this
                 is currently direct HTTP access through Apache public_html, and
                 not an "API" as such; we will need to adopt this as a real
@@ -69,28 +81,59 @@ class EndpointConfig(Resource):
         in other APIs. We should consider persisting an equivalent of the
         mapping table built in "indexer.py" for use across the server APIs.
 
-        Meta TODO: We're giving the pass-through APIs for Elasticsearch and
-        GraphQL here, which implies adoption of Nikhil's and Fuqing's work to
-        move the dashboard away from direct access to the backend DB servers.
-        The alternative would be to expose the direct Elasticsearch and GraphQL
-        URIs here.
+        TODO: We provide Elasticsearch index root names here, which the dashboard
+        code needs to perform the queries we've not yet replaced with server-side
+        implementations. The entire "indices" section can be removed once that is
+        resolved.
         """
+        self.logger.info("Received these headers: {!r}", request.headers)
+        header = request.headers.get("Forwarded")
+        origin = None
+        if header:
+            m = self.forward_pattern.search(header)
+            if m:
+                origin = m.group("host")
+                self.logger.info("Forwarded: {}, ({})", header, origin)
+        if not origin:
+            header = request.headers.get("X-Forwarded-Host")
+            if header:
+                m = self.x_forward_pattern.search(header)
+                if m:
+                    origin = m.group("host")
+                    self.logger.info("X-Forwarded-Host: {}, ({})", header, origin)
+        if not origin:
+            origin = self.host
+        host = f"http://{origin}"
+        uri = urljoin(host, self.uri_prefix)
+        self.logger.info("'{}' : '{}' : '{}' : '{}'", self.host, origin, host, uri)
+
+        # Strip a trailing slash because the paths we'll add have an
+        # initial slash and we don't want two.
+        if uri.endswith("/"):
+            uri = uri.split(0, -1)
+
         try:
             endpoints = {
-                "metadata": {
-                    "identification": f"Pbench server {self.commit_id}",
+                "identification": f"Pbench server {self.commit_id}",
+                "indices": {
                     "run_index": f"{self.prefix}.v6.run-data.",
                     "run_toc_index": f"{self.prefix}.v6.run-toc.",
                     "result_index": f"{self.prefix}.v5.result-data-sample.",
                     "result_data_index": f"{self.prefix}.v5.result-data.",
                 },
                 "api": {
-                    "results": f"{self.host}",
-                    "elasticsearch": f"{self.uri}/elasticsearch",
-                    "endpoints": f"{self.uri}/endpoints",
-                    "graphql": f"{self.uri}/graphql",
-                    "queryControllers": f"{self.uri}/controllers/list",
-                    "queryMonthIndices": f"{self.uri}/controllers/months",
+                    "results": f"{host}/results",
+                    "elasticsearch": f"{uri}/elasticsearch",
+                    "endpoints": f"{uri}/endpoints",
+                    "graphql": f"{uri}/graphql",
+                    "controller_list": f"{uri}/controllers/list",
+                    "controller_months": f"{uri}/controllers/months",
+                    "dataset_list": f"{uri}/datasets/list",
+                    "dataset": f"{uri}/datasets",
+                    "register": f"{uri}/register",
+                    "login": f"{uri}/login",
+                    "logout": f"{uri}/logout",
+                    "user": f"{uri}/user/<string:username>",
                 },
             }
             response = jsonify(endpoints)
--- lib/pbench/test/unit/server/test_endpoint_configure.py
+++ lib/pbench/test/unit/server/test_endpoint_configure.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 from pbench.server.api.resources.query_apis import get_index_prefix
 
 
@@ -14,28 +16,54 @@ class TestEnpointConfig:
         """
         test_query Check that endpoint data matches the config file.
         """
-        host = server_config.get("pbench-server", "proxy_host")
+        host = server_config.get("pbench-server", "host")
+        self.check_config(client, server_config, host)
+
+    def test_proxy_query(self, client, server_config):
+        host = "proxy.example.com:8901"
+        forward = f"by=server.example.com;for=client.example.com;host={host};proto=http"
+        self.check_config(client, server_config, host, {"Forwarded": forward})
+
+    def test_x_forward_proxy_query(self, client, server_config):
+        host = "proxy.example.com:8902"
+        self.check_config(client, server_config, host, {"X-Forwarded-Host": host})
+
+    def test_x_forward_list_proxy_query(self, client, server_config):
+        host1 = "proxy.example.com:8902"
+        host2 = "proxy2.example.com"
+        self.check_config(
+            client, server_config, host1, {"X-Forwarded-Host": f"{host1}, {host2}"}
+        )
+
+    def check_config(self, client, server_config, host, my_headers={}):
         uri_prefix = server_config.rest_uri
-        uri = f"{host}{uri_prefix}"
+        host = "http://" + host
+        uri = urljoin(host, uri_prefix)
         prefix = get_index_prefix(server_config)
         expected_results = {
-            "metadata": {
-                "identification": f"Pbench server {server_config.COMMIT_ID}",
+            "identification": f"Pbench server {server_config.COMMIT_ID}",
+            "indices": {
                 "run_index": f"{prefix}.v6.run-data.",
                 "run_toc_index": f"{prefix}.v6.run-toc.",
                 "result_index": f"{prefix}.v5.result-data-sample.",
                 "result_data_index": f"{prefix}.v5.result-data.",
             },
             "api": {
-                "results": f"{host}",
+                "results": f"{host}/results",
                 "elasticsearch": f"{uri}/elasticsearch",
                 "endpoints": f"{uri}/endpoints",
                 "graphql": f"{uri}/graphql",
-                "queryControllers": f"{uri}/controllers/list",
-                "queryMonthIndices": f"{uri}/controllers/months",
+                "controller_list": f"{uri}/controllers/list",
+                "controller_months": f"{uri}/controllers/months",
+                "dataset_list": f"{uri}/datasets/list",
+                "dataset": f"{uri}/datasets",
+                "register": f"{uri}/register",
+                "login": f"{uri}/login",
+                "logout": f"{uri}/logout",
+                "user": f"{uri}/user/<string:username>",
             },
         }
 
-        response = client.get(f"{server_config.rest_uri}/endpoints")
+        response = client.get(f"{server_config.rest_uri}/endpoints", headers=my_headers)
         res_json = response.json
         assert res_json == expected_results
