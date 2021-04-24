b9f46774485c592ecb2b8ce2a374ead91af71ede
Niki Glenister <niki.j.glenister@gmail.com>
2021-04-23 20:00:17 -0400

    Added Xiaoxiong's demo script and changes to the test_client

--- Demo/src/test_client.py
+++ Demo/src/test_client.py
@@ -2,7 +2,14 @@
 * Implements a gRPC client that initializes a stub and makes an RPC call to
 * our server while propagating the span context in JSON text form
 *
-* Listens over port 50051 for incoming "SayHello" requests
+* Makes a request to the given host and port (localhost:50051 by default)
+* Makes multiple requests on a given interval for a given duration (100ms and 1000ms by default)
+*
+* Arguments:
+*   [host]
+*   [port]
+*   [interval in milliseconds]
+*   [duration in milliseconds]
 '''
 
 from __future__ import print_function
@@ -49,16 +56,7 @@ def run():
 
     tracer = init_tracer('distributed-test')
 
-    # start a span for the request
-    span = tracer.start_span('greet-request')
-
-    # inject this span's context into a dict, then convert to JSON
-    carrier = {}
-    tracer.inject(span_context=span.context, format=Format.TEXT_MAP, carrier=carrier)
-    context_json = json.dumps(carrier)
-
-    span.log_event("Injected span context into carrier")
-
+    # get host and port to send request
     try:
         host = sys.argv[1]
     except IndexError:
@@ -68,16 +66,49 @@ def run():
     except IndexError:
         port = "50051"
 
-    # call the SayHello remote procedure with the context in the request
-    with grpc.insecure_channel(f"{host}:{port}") as channel:
-        stub = node1_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(node1_pb2.HelloRequest(spanContext=context_json, name='client'))
-    print("Greeter client received: " + response.message)
+    # get interval and duration
+    try:
+        interval = int(sys.argv[3])
+    except IndexError:
+        interval = 100
+    try:
+        duration = int(sys.argv[4])
+    except IndexError:
+        duration = 1000
 
-    span.log_event("Completed request to gRPC server")
+    loop_n_times = int(duration / interval)
 
-    # finish the span
-    span.finish()
+    # start a span for the whole loop
+    main_span = tracer.start_span('looping-requests')
+    main_span.set_tag("interval", interval)
+    main_span.set_tag("duration", duration)
+
+    for i in range(loop_n_times):
+        # start a span for the request
+        span = tracer.start_span(f'greet-request-{i}')
+
+        # inject this span's context into a dict, then convert to JSON
+        carrier = {}
+        tracer.inject(span_context=span.context, format=Format.TEXT_MAP, carrier=carrier)
+        context_json = json.dumps(carrier)
+
+        span.log_event("Injected span context into carrier")
+
+        # call the SayHello remote procedure with the context in the request
+        with grpc.insecure_channel(f"{host}:{port}") as channel:
+            stub = node1_pb2_grpc.GreeterStub(channel)
+            response = stub.SayHello(node1_pb2.HelloRequest(spanContext=context_json, name='client'))
+        print("Greeter client received: " + response.message)
+
+        span.log_event("Completed request to gRPC server")
+
+        # finish the span
+        span.finish()
+
+        time.sleep(float(interval)/1000)
+
+    # close the main span
+    main_span.finish()
 
     # note: does not send if we don't sleep first
     time.sleep(2)   # yield to IOLoop to flush the spans - https://github.com/jaegertracing/jaeger-client-python/issues/50
