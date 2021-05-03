27d6dea306bc306ffd0970cc3fe5ac75fc94b3d3
Maxusmusti <meyceoz@redhat.com>
2021-05-03 18:15:08 -0400

    Allow live metric visualization for remote runs + Added customizable hosts/ports + Updated pcp visualizer and cleaned all

--- agent/containers/images/visualizers/run.py
+++ agent/containers/images/visualizers/run.py
@@ -2,56 +2,87 @@
 Starts by starting up grafana server, as well as any collectors (prometheus, pmproxy) if needed.
 
 Then sets up all grafana plugins, data sources, and dashboards through grafana API.
-Chooses what to upload/enable based off VIS_TYPE environment variable.
+Chooses what to upload/enable based off the visualizer type argument.
 
-If VIS_TYPE is 'live' (default for live-metric-visualizer):
+If the type is 'live' (default for live-metric-visualizer):
     - Node exporter/DCGM dashboards will be uploaded and enabled
     - Prometheus data source will be configured
     - Grafana-pcp plugin will be enabled
     - PCP redis and vector datasources will be configured
     - PCP default dashboards will be enabled
-If VIS_TYPE is 'prom' (default for prom-graf-visualizer):
+If the type is 'prom' (default for prom-graf-visualizer):
     - Node exporter/DCGM dashboards will be uploaded and enabled
     - Prometheus data source will be configured
-If VIS_TYPE is 'pcp' (default for soon-to-come pcp-graf-visualizer):
+If the type is 'pcp' (default for soon-to-come pcp-graf-visualizer):
     - Grafana-pcp plugin will be enabled
     - PCP redis and vector datasources will be configured
     - PCP default dashboards will be enabled
 """
 
 import os
+import sys
 import json
 import requests
 import time
 import subprocess
 
 # Start the collector (if needed)
-collector_cmd = os.environ["COLLECTOR"]
-if not collector_cmd == "":
-    collector = subprocess.Popen(collector_cmd.split(" "))
-    if os.environ["VIS_TYPE"] == "pcp":
+collector = None
+metric_type = sys.argv[1]
+if metric_type == "pcp":
+    cmd = [
+        "/usr/libexec/pcp/bin/pmproxy",
+        "--foreground",
+        "--timeseries",
+        "--port=44566",
+        "--redishost=localhost",
+        "--redisport=6379",
+        "--config=/etc/pcp/pmproxy/pmproxy.conf",
+    ]
+    redis_host = os.environ.get("REDIS_HOST", "NO_HOST")
+    if not redis_host == "NO_HOST":
+        cmd[4] = f"--redishost={redis_host}"
+        redis_port = os.environ.get("REDIS_PORT", "NO_PORT")
+        if not redis_port == "NO_PORT":
+            cmd[5] = f"--redisport={redis_port}"
+    else:
         subprocess.Popen("redis-server")
-        log_path = os.environ["PCP_ARCHIVE_DIR"]
-        for item in os.scandir(log_path):
-            if os.path.isdir(item):
-                rec_path = os.path.join(log_path, item.name)
-                args = [
-                    "pmseries",
-                    "--load",
-                    rec_path,
-                ]
-                print(args)
-                subprocess.Popen(args)
-else:
-    collector = None
+    collector = subprocess.Popen(cmd)
+    log_path = "/var/log/pcp/pmlogger"
+    for item in os.scandir(log_path):
+        if os.path.isdir(item):
+            rec_path = os.path.join(log_path, item.name)
+            args = [
+                "pmseries",
+                "--load",
+                rec_path,
+            ]
+            print(args)
+            subprocess.Popen(args)
+elif metric_type == "prom":
+    cmd = [
+        "./prometheus",
+        "--config.file=/data/prometheus.yml",
+        "--storage.tsdb.path=/data",
+    ]
+    collector = subprocess.Popen(cmd)
 
 # Start the grafana server
+os.environ["GF_DEFAULT_APP_MODE"] = os.environ.get("GF_DEFAULT_APP_MODE", "development")
+os.environ["GF_PATHS_DATA"] = os.environ.get("GF_PATHS_DATA", "/var/lib/grafana")
+os.environ["GF_PATHS_LOGS"] = os.environ.get("GF_PATHS_LOGS", "/var/log/grafana")
+os.environ["GF_PATHS_PLUGINS"] = os.environ.get(
+    "GF_PATHS_PLUGINS", "/var/lib/grafana/plugins"
+)
+os.environ["GF_PATHS_PROVISIONING"] = os.environ.get(
+    "GF_PATHS_PROVISIONING", "/etc/grafana/provisioning"
+)
 args = [
     "grafana-server",
     "-homepath",
-    os.environ["GF_PATHS_HOME"],
+    os.environ.get("GF_PATHS_HOME", "/usr/share/grafana"),
     "-config",
-    os.environ["GF_PATHS_CONFIG"],
+    os.environ.get("GF_PATHS_CONFIG", "/etc/grafana/grafana.ini"),
     "$@",
     "cfg:default.log.mode=console",
 ]
@@ -75,13 +106,26 @@ response = requests.post(
 token = json.loads(response.content.decode("utf-8"))["key"]
 headers["Authorization"] = f"Bearer {token}"
 
-metric_type = os.environ["VIS_TYPE"]
+if metric_type == "live":
+    host = os.environ.get("HOST", "localhost")
+    prom_host = os.environ.get("PROM_HOST", host)
+    pm_host = os.environ.get("PM_HOST", host)
+    prom_url = f"http://{prom_host}:"
+    pm_url = f"http://{pm_host}:"
+    prom_port = os.environ.get("PROM_PORT", "9090")
+    pm_port = os.environ.get("PM_PORT", "44566")
+else:
+    prom_url = "http://localhost:"
+    pm_url = prom_url
+    prom_port = "9090"
+    pm_port = "44566"
+
 if metric_type == "live" or metric_type == "prom":
     prom_source_url = f"{graf_base}api/datasources"
     payload = {
         "name": "prometheus",
         "type": "prometheus",
-        "url": "http://localhost:9090",
+        "url": prom_url + prom_port,
         "access": "proxy",
         "basicAuth": False,
     }
@@ -116,7 +160,7 @@ if metric_type == "live" or metric_type == "pcp":
     payload = {
         "name": "PCP Redis",
         "type": "pcp-redis-datasource",
-        "url": "http://localhost:44566",
+        "url": pm_url + pm_port,
         "access": "proxy",
         "basicAuth": False,
     }
@@ -126,7 +170,7 @@ if metric_type == "live" or metric_type == "pcp":
     payload = {
         "name": "PCP Vector",
         "type": "pcp-vector-datasource",
-        "url": "http://localhost:44566",
+        "url": pm_url + pm_port,
         "access": "proxy",
         "basicAuth": False,
     }
