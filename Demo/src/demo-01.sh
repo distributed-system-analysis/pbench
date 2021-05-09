19cb90e06f76ba330a779826e6744c614547c17b
Peter Portante <peter.portante@redhat.com>
2021-05-08 23:32:45 -0400

    More tweaks to how the demo runs

--- Demo/src/demo-01.sh
+++ Demo/src/demo-01.sh
@@ -34,7 +34,8 @@ fi
 export pbench_run=$(pwd)/run-dir
 rm -rf ${pbench_run}
 mkdir ${pbench_run}
-printf -- "${pbench_run}\n" > ${pbench_run}/.path
+#printf -- "${pbench_run}\n" > ${pbench_run}/.path
+cp -a ./demo-01-run-pbench.sh ${pbench_run}/
 export pbench_log=${pbench_run}/pbench.log
 
 for i in ${*}; do
@@ -84,7 +85,7 @@ wait_keypress 120
 
 # Start Tool Meister containers on remote hosts
 
-printf -- "\n\nNow let's start the three Tool Meister pods running on the given hosts...\n\n\t$ podman run --name pbench-agent-tool-meister --network host --ulimit nofile=65536:65536 --rm -d -e REDIS_HOST=${REDIS_HOST} -e REDIS_PORT=${REDIS_PORT} -e PARAM_KEY=tm-default-\$(hostname -f') -e _PBENCH_TOOL_MEISTER_LOG_LEVEL=debug quay.io/pbench/pbench-agent-tool-meister-${DISTRO}:${TAG}\n\n"
+printf -- "\n\nNow let's start the three Tool Meister pods running on the given hosts...\n\n\t$ podman run --name pbench-agent-tool-meister --network host --ulimit nofile=65536:65536 --rm -d -e REDIS_HOST=${REDIS_HOST} -e REDIS_PORT=${REDIS_PORT} -e PARAM_KEY=tm-default-\$(hostname -f) -e _PBENCH_TOOL_MEISTER_LOG_LEVEL=debug quay.io/pbench/pbench-agent-tool-meister-${DISTRO}:${TAG}\n\n"
 
 wait_keypress 120
 
@@ -94,9 +95,7 @@ wait_keypress 120
 
 printf -- "\n\nNow let's start the local Tool Data Sink to pull tool data into our local volume.\n\n"
 
-set -x
-podman run --name pbench-agent-tool-data-sink --network host --volume ${pbench_run}:/var/lib/pbench-agent:Z --ulimit nofile=65536:65536 --rm -d -e REDIS_HOST=${REDIS_HOST} -e REDIS_PORT=${REDIS_PORT} -e PARAM_KEY=tds-default -e _PBENCH_TOOL_DATA_SINK_LOG_LEVEL=debug quay.io/pbench/pbench-agent-tool-data-sink-${DISTRO}:${TAG}
-set +x
+printf -- "podman run --name pbench-agent-tool-data-sink --network host --volume ${pbench_run}:/var/lib/pbench-agent:z --ulimit nofile=65536:65536 --rm -d -e REDIS_HOST=${REDIS_HOST} -e REDIS_PORT=${REDIS_PORT} -e PARAM_KEY=tds-default -e _PBENCH_TOOL_DATA_SINK_LOG_LEVEL=debug quay.io/pbench/pbench-agent-tool-data-sink-${DISTRO}:${TAG}\n\n"
 
 printf -- "\n\nNow let's look at the 'podman logs' output from these containers;\n\tnotice we did not start a Redis server yet,\n\tthey'll just be waiting for it to show up.\n\n"
 
@@ -105,15 +104,9 @@ wait_keypress 120
 
 printf -- "\n\nStart the Redis server on ${REDIS_HOST}:${REDIS_PORT}\n\t$ podman run --name demo-tm-redis --network host --rm -d docker.io/library/redis:latest\n\n\tNote TDS and TMs notice Redis server,\n\tbut now wait for their 'PARAM_KEY' to show up.\n\n"
 
-set -x
-podman run --name redis-server --network host --rm -d docker.io/library/redis:latest
-set +x
-
 wait_keypress 120
 
 
-cp -a ./demo-01-run-pbench.sh ${pbench_run}/
-
-podman run -it --rm --network host --volume ${pbench_run}:/var/lib/pbench-agent:Z quay.io/pbench/pbench-agent-base-${DISTRO}:${TAG} /bin/bash
+podman run -it --rm --network host --volume ${pbench_run}:/var/lib/pbench-agent:z quay.io/pbench/pbench-agent-base-${DISTRO}:${TAG} /bin/bash
 
 exit 0
