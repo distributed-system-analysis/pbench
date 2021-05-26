4b086ccbc7c7a76df68fa7b2d5d6c0029842eb58
Nick Dokos <ndokos@redhat.com>
2021-05-26 15:02:23 -0400

    Simplify fixtures
    Add the monkeypatch.setenv to the agent_config fixture and get
    rid of all extraneous fixtures in the tests.

--- lib/pbench/test/functional/agent/cli/commands/tools/test_tools_list.py
+++ lib/pbench/test/functional/agent/cli/commands/tools/test_tools_list.py
@@ -8,47 +8,33 @@ class Test_list_tool_no_tools_registered:
         assert b"Usage: pbench-list-tools [OPTIONS]" in out
         assert exitcode == 0
 
-    def test_no_args(self, monkeypatch, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_no_args(self, agent_config):
         command = ["pbench-list-tools"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"" == out
         assert exitcode == 0
 
-    def test_name(self, monkeypatch, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_name(self, agent_config):
         command = ["pbench-list-tools", "--name", "foo"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 2
 
-    def test_group(self, monkeypatch, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_group(self, agent_config):
         command = ["pbench-list-tools", "--group", "foo"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
 
-    def test_group_name(self, monkeypatch, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_group_name(self, agent_config):
         command = ["pbench-list-tools", "--group", "foo", "--name", "iostat"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
 
-    def test_group_options(self, monkeypatch, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_group_options(self, agent_config):
         command = ["pbench-list-tools", "--group", "foo", "--with-option"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
 
-    def test_group_name_options(
-        self, monkeypatch, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_group_name_options(self, agent_config):
         command = [
             "pbench-list-tools",
             "--group",
@@ -60,9 +46,7 @@ class Test_list_tool_no_tools_registered:
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
 
-    def test_option(self, monkeypatch, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_option(self, agent_config):
         command = ["pbench-list-tools", "--with-option"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"" == out
@@ -78,82 +62,52 @@ class Test_list_tool_tools_registered:
         tool.touch()
         return tool
 
-    def test_help(self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg):
+    def test_help(self, tool, agent_config):
         command = ["pbench-list-tools", "--help"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"Usage: pbench-list-tools [OPTIONS]" in out
         assert exitcode == 0
 
-    def test_no_args(self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_no_args(self, tool, agent_config):
         command = ["pbench-list-tools"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"default: testhost.example.com ['perf']\n" == out
         assert exitcode == 0
 
-    def test_group_existing(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_group_existing(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "default"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"default: testhost.example.com ['perf']\n" == out
         assert exitcode == 0
 
-    def test_name_existing(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_name_existing(self, tool, agent_config):
         command = ["pbench-list-tools", "-n", "perf"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"tool name: perf groups: ['default']\n" == out
         assert exitcode == 0
 
-    def test_non_existent_group(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_non_existent_group(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "unknown"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
 
-    def test_non_existent_name(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_non_existent_name(self, tool, agent_config):
         command = ["pbench-list-tools", "-n", "unknown"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 2
 
-    def test_existing_group_name(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_existing_group_name(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "default", "--name", "perf"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert b"tool name: perf groups: ['default']\n" == out
         assert exitcode == 0
 
-    def test_existing_group_non_existent_name(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_existing_group_non_existent_name(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "default", "--name", "unknown"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 2
 
-    def test_non_existent_group_existing_name(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_non_existent_group_existing_name(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "unknown", "--name", "perf"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
@@ -162,7 +116,6 @@ class Test_list_tool_tools_registered:
 class Test_list_tool_tools_registered_with_options:
     @pytest.fixture
     def tool(self, pbench_run):
-
         for group in ["default", "test"]:
             p = pbench_run / f"tools-v1-{group}" / "testhost.example.com"
             p.mkdir(parents=True)
@@ -172,11 +125,7 @@ class Test_list_tool_tools_registered_with_options:
             tool.write_text("--interval=300")
         return
 
-    def test_existing_group_options(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_existing_group_options(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "default", "--with-option"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert (
@@ -185,20 +134,12 @@ class Test_list_tool_tools_registered_with_options:
         )
         assert exitcode == 0
 
-    def test_non_existent_group_options(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_non_existent_group_options(self, tool, agent_config):
         command = ["pbench-list-tools", "--group", "unknown", "--with-option"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert exitcode == 1
 
-    def test_existing_group_name_options(
-        self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg
-    ):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_existing_group_name_options(self, tool, agent_config):
         command = [
             "pbench-list-tools",
             "--group",
@@ -214,9 +155,7 @@ class Test_list_tool_tools_registered_with_options:
         )
         assert exitcode == 0
 
-    def test_option(self, monkeypatch, tool, agent_config, pbench_run, pbench_cfg):
-        monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-
+    def test_option(self, tool, agent_config):
         command = ["pbench-list-tools", "--with-option"]
         out, err, exitcode = pytest.helpers.capture(command)
         assert (
