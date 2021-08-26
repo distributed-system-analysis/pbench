f0ae7beab15d7646a0df0b2300fb9b7cea43b42a
Peter Portante <peter.portante@redhat.com>
2021-08-26 00:10:32 -0400

    Add reporting for the last 2 weeks
    Also fix colunmns when a bucket of data does not have one or more
    satellites in it.  We also fix the column widths to accommodate a
    3 digit percentage (100.00%).

--- server/bin/pbench-tarball-stats.py
+++ server/bin/pbench-tarball-stats.py
@@ -15,7 +15,7 @@ import re
 import sys
 
 from argparse import ArgumentParser, Namespace
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import jinja2
 
@@ -91,7 +91,7 @@ def get_sat_name(ctrl: str):
     return "main" if len(parts) == 1 else parts[0]
 
 
-def transform_buckets(buckets: dict):
+def transform_buckets(buckets: dict, sat_names: list):
     """Transform a given bucket to calculate the % tar balls by the origin
     server.
     """
@@ -102,6 +102,9 @@ def transform_buckets(buckets: dict):
         for item in bucket_list:
             satellites[get_sat_name(item.ctrl)] += 1
         sats_pct = {}
+        # Ensure all known satellite names have a default of 0.00%.
+        for name in sat_names:
+            sats_pct[name] = 0.0
         for name, total in sorted(satellites.items()):
             pct = (total / count) * 100.0
             sats_pct[name] = pct
@@ -125,8 +128,10 @@ def transform_buckets(buckets: dict):
 #     .ctrls      - dictionary of controllers with bad tar balls
 #   by_year       - dictionary mapping counts of tar balls for each given
 #                   year generated, with mappings per satellite server
-#   by_month      - dictionary mapping counts of tar balls for each given
-#                   month generated, with mappings per satellite server
+#   by_month      - same as by_year, just "by month"
+#   by_week       - list of dictionares of weekly mappings, similar to by_year,
+#                   but with data for a given week, where the display names are
+#                   the Sunday on which the week ends.
 #
 report_tmpl = """Summary Statistics for Tar Balls (external data only):
 
@@ -157,15 +162,23 @@ report_tmpl = """Summary Statistics for Tar Balls (external data only):
 
     {% endif %}
 {% if server_origin.keys()|length > 0 %}
+{% if by_week.keys()|length > 0 %}
+Tar Ball Counts broken down by weeks, with satellite percentages:
+
+Week Ending  Total Count{% for name in server_origin.keys()|sort %}   {{ "% {0:>5s}".format(name) }}{% endfor +%}
+{% for name,value in by_week.items()|sort %}
+{{ "{0:<10s}".format(name) }}   {{ "{:11n}".format(value.count) }}{% for name in server_origin.keys()|sort %}   {{ "{:6.2f}%".format(value.sats_pct[name]) }}{% endfor +%}
+{% endfor %}
+{% endif %}
 
 Tar Ball Counts broken down by Year and Month, with satellite percentages:
 
-Year/Month   Total Count{% for name in server_origin.keys()|sort %}   {{ "% {0:>4s}".format(name) }}{% endfor +%}
-{% for name,value in by_year.items() %}
-{{ "{0:<10s}".format(name) }}   {{ "{:11d}".format(value.count) }}{% for name in server_origin.keys()|sort %}   {{ "{:4.2f}%".format(value.sats_pct[name]) }}{% endfor +%}
+Year/Month   Total Count{% for name in server_origin.keys()|sort %}   {{ "% {0:>5s}".format(name) }}{% endfor +%}
+{% for name,value in by_year.items()|sort %}
+{{ "{0:<10s}".format(name) }}   {{ "{:11n}".format(value.count) }}{% for name in server_origin.keys()|sort %}   {{ "{:6.2f}%".format(value.sats_pct[name]) }}{% endfor +%}
 {% endfor %}
-{% for name,value in by_month.items() %}
-{{ "{0:<10s}".format(name) }}   {{ "{:11d}".format(value.count) }}{% for name in server_origin.keys()|sort %}   {{ "{:4.2f}%".format(value.sats_pct[name]) }}{% endfor +%}
+{% for name,value in by_month.items()|sort %}
+{{ "{0:<10s}".format(name) }}   {{ "{:11n}".format(value.count) }}{% for name in server_origin.keys()|sort %}   {{ "{:6.2f}%".format(value.sats_pct[name]) }}{% endfor +%}
 {% endfor %}
 {% endif %}"""
 
@@ -218,6 +231,15 @@ def main(options: Namespace) -> int:
 
     by_year = collections.defaultdict(list)
     by_month = collections.defaultdict(list)
+    by_week = collections.defaultdict(list)
+
+    sevendays = timedelta(days=7)
+    now = datetime.utcnow()
+    curr_week_end = now - timedelta(days=now.isocalendar()[2]) + sevendays
+    curr_week_end_s = curr_week_end.strftime("%Y-%m-%d")
+    last_week_end = now - timedelta(days=now.isocalendar()[2], weeks=1) + sevendays
+    last_week_end_s = last_week_end.strftime("%Y-%m-%d")
+    weeks = frozenset((curr_week_end_s, last_week_end_s))
 
     start = pbench._time()
 
@@ -239,6 +261,12 @@ def main(options: Namespace) -> int:
             month = tb_rec.dt.month
             ym = f"{year:4d}-{month:02d}"
             by_month[ym].append(tb_rec)
+            week_end = (
+                tb_rec.dt - timedelta(days=tb_rec.dt.isocalendar()[2]) + sevendays
+            )
+            week_end_s = week_end.strftime("%Y-%m-%d")
+            if week_end_s in weeks:
+                by_week[week_end_s].append(tb_rec)
 
     satellites_ctrl = collections.Counter()
     if good:
@@ -255,8 +283,9 @@ def main(options: Namespace) -> int:
             bad=TarBallStats(count=invalid_cnt, size=invalid_size, ctrls=invalid),
             server_origin=satellites_tb,
             satellites=satellites_ctrl,
-            by_year=transform_buckets(by_year),
-            by_month=transform_buckets(by_month),
+            by_year=transform_buckets(by_year, satellites_tb.keys()),
+            by_month=transform_buckets(by_month, satellites_tb.keys()),
+            by_week=transform_buckets(by_week, satellites_tb.keys()),
         )
     )
 
