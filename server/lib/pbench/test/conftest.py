519f9031cd7e824e9d8d831b68267ffc30a88bb2
Riya <riyad17031998@gmail.com>
2021-09-13 21:23:05 +0530

    Put-Shim Server
    Fixes #2100
    
    put-shim server will act as an interface between v0.69 pbench-agents
    and v0.72 pbench-server. It will receive the tarballs using ssh
    transfer method from 0.69 agent and will forward them to 0.72 server
    using the put request.
    
    * Add put-shim-server tests as pytests
    
    It tests the following:
    - token's availability
    - Receive directory is present or not
    - Receive directory is a valid directory
    - tar ball not found error
    - connection error
    - successful processing of tar ball
    - zero tar ball to be processed
    - multiple tar balls to be processed
    
    * Add put-shim scripts to spec and Makefile
    
    * Add and run pytests from tox and jenkins respectively
    
    - Add pytests to the tox
    - Run pytests from jenkins environment

---
+++ server/lib/pbench/test/conftest.py
@@ -0,0 +1,38 @@
+"""This module builts base setup for Server side testing.
+It is implicitly included by PyTest for all unit tests and provides
+test fixtures and other definitions common across the Server unit tests"""
+import logging
+import pytest
+import tempfile
+
+from pathlib import Path
+
+from pbench import _PbenchLogFormatter, _StyleAdapter
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup(request, pytestconfig):
+    """Test package setup for pbench-server"""
+
+    # Create a single temporary directory for the "/srv/pbench" and
+    # "/opt/pbench-server" directories.
+    tmp = tempfile.TemporaryDirectory(suffix=".d", prefix="pbench-server-unit-tests.")
+    tmp_d = Path(tmp.name)
+
+    pytestconfig.cache.set("TMP", str(tmp_d))
+    request.addfinalizer(lambda: tmp.cleanup())
+
+
+@pytest.fixture
+def test_logger(pytestconfig):
+    tmp = Path(pytestconfig.cache.get("TMP", None))
+    logger = logging.getLogger("testing")
+    handler = logging.FileHandler(tmp / "test.log")
+    handler.setLevel(logging.ERROR)
+    logfmt = (
+        "1970-01-01T00:00:42.000000 {levelname} {name}.{module} {funcName} -- {message}"
+    )
+    formatter = _PbenchLogFormatter(fmt=logfmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return _StyleAdapter(logger)
