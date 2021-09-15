a3e281d57f80a95eccd15ae1c354ddb22f22ed3d
Nick Dokos <ndokos@redhat.com>
2021-09-15 16:50:57 -0400

    Add functional tests for pbench-list-tools
    Although all the tests pass currently, they certainly should not.
    There are three classes of tests that IMO should not pass:
    
    - `--name <unknown name>` exits with status 0, the same
      as the 0.69.9 version. OTOH, `--group <unknown group>` exits with status
      1.
    
    - `--with-option` output is ugly (and different from 0.69.9 which is
      also pretty ugly).
    
    - `--name <known name>  --group <known-group> --with-option` gives
      wrong output: it does not produce the options. 0.69.9 signals an
      error here, complaining that having both name *and* group specified
      cannot be done: that seems specious to me.
    
    The question is what to fix in this PR and what to defer to a future
    one.

--- lib/pbench/test/functional/agent/cli/commands/tools/test_tools_list.py
+++ lib/pbench/test/functional/agent/cli/commands/tools/test_tools_list.py
@@ -1,31 +1,167 @@
 import pytest
 
 
-def test_pbench_list_tools_help():
-    command = ["pbench-list-tools", "--help"]
-    out, err, exitcode = pytest.helpers.capture(command)
-    assert b"Usage: pbench-list-tools [OPTIONS]" in out
-    assert exitcode == 0
+class Test_list_tool_no_tools_registered:
+    def test_help(self):
+        command = ["pbench-list-tools", "--help"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"Usage: pbench-list-tools [OPTIONS]" in out
+        assert exitcode == 0
+
+    def test_no_args(self, agent_config):
+        command = ["pbench-list-tools"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"" == out
+        assert exitcode == 0
+
+    def test_name(self, agent_config):
+        command = ["pbench-list-tools", "--name", "foo"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        # XXX 0.69.9 backward compatibility: non-existent names do no return errors
+        assert exitcode == 0
+
+    def test_group(self, agent_config):
+        command = ["pbench-list-tools", "--group", "foo"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+    def test_group_name(self, agent_config):
+        command = ["pbench-list-tools", "--group", "foo", "--name", "iostat"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+    def test_group_options(self, agent_config):
+        command = ["pbench-list-tools", "--group", "foo", "--with-option"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+    def test_group_name_options(self, agent_config):
+        command = [
+            "pbench-list-tools",
+            "--group",
+            "foo",
+            "--name",
+            "iostat",
+            "--with-option",
+        ]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+    def test_option(self, agent_config):
+        command = ["pbench-list-tools", "--with-option"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"" == out
+        assert exitcode == 0
 
 
-def test_list_tool(monkeypatch, agent_config, pbench_run, pbench_cfg):
-    monkeypatch.setenv("_PBENCH_AGENT_CONFIG", str(pbench_cfg))
-    p = pbench_run / "tools-v1-default" / "testhost.example.com"
-    p.mkdir(parents=True)
-    tool = p / "perf"
-    tool.touch()
+class Test_list_tool_tools_registered:
+    @pytest.fixture
+    def tool(self, pbench_run):
+        p = pbench_run / "tools-v1-default" / "testhost.example.com"
+        p.mkdir(parents=True)
+        tool = p / "perf"
+        tool.touch()
+        return tool
 
-    command = ["pbench-list-tools"]
-    out, err, exitcode = pytest.helpers.capture(command)
-    assert exitcode == 0
-    assert b"default: testhost.example.com ['perf']" in out
+    def test_help(self, tool, agent_config):
+        command = ["pbench-list-tools", "--help"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"Usage: pbench-list-tools [OPTIONS]" in out
+        assert exitcode == 0
 
-    command = ["pbench-list-tools", "--group", "default"]
-    out, err, exitcode = pytest.helpers.capture(command)
-    assert exitcode == 0
-    assert b"default: testhost.example.com ['perf']" in out
+    def test_no_args(self, tool, agent_config):
+        command = ["pbench-list-tools"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"default: testhost.example.com ['perf']\n" == out
+        assert exitcode == 0
 
-    command = ["pbench-list-tools", "-n", "perf"]
-    out, err, exitcode = pytest.helpers.capture(command)
-    assert exitcode == 0
-    assert b"tool name: perf groups: default" in out
+    def test_group_existing(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "default"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"default: testhost.example.com ['perf']\n" == out
+        assert exitcode == 0
+
+    def test_name_existing(self, tool, agent_config):
+        command = ["pbench-list-tools", "-n", "perf"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"tool name: perf groups: default\n" == out
+        assert exitcode == 0
+
+    def test_non_existent_group(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "unknown"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+    def test_non_existent_name(self, tool, agent_config):
+        command = ["pbench-list-tools", "-n", "unknown"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        # XXX 0.69.9 backward compatibility: non-existent names do not return errors
+        assert exitcode == 0
+
+    def test_existing_group_name(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "default", "--name", "perf"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert b"tool name: perf groups: default\n" == out
+        assert exitcode == 0
+
+    def test_existing_group_non_existent_name(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "default", "--name", "unknown"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 0
+
+    def test_non_existent_group_existing_name(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "unknown", "--name", "perf"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+
+class Test_list_tool_tools_registered_with_options:
+    @pytest.fixture
+    def tool(self, pbench_run):
+        for group in ["default", "test"]:
+            p = pbench_run / f"tools-v1-{group}" / "testhost.example.com"
+            p.mkdir(parents=True)
+            tool = p / "iostat"
+            tool.write_text("--interval=30")
+            tool = p / "mpstat"
+            tool.write_text("--interval=300")
+        return
+
+    def test_existing_group_options(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "default", "--with-option"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        # XXX this is the current output - pretty ugly
+        assert (
+            b"default: testhost.example.com [('iostat', '--interval=30'), ('mpstat', '--interval=300')]\n"
+            == out
+        )
+        assert exitcode == 0
+
+    def test_non_existent_group_options(self, tool, agent_config):
+        command = ["pbench-list-tools", "--group", "unknown", "--with-option"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        assert exitcode == 1
+
+    def test_existing_group_name_options(self, tool, agent_config):
+        command = [
+            "pbench-list-tools",
+            "--group",
+            "default",
+            "--name",
+            "mpstat",
+            "--with-option",
+        ]
+        out, err, exitcode = pytest.helpers.capture(command)
+        # XXX this is the current output - obviously wrong
+        assert b"tool name: mpstat groups: default\n" == out
+        assert exitcode == 0
+
+    def test_option(self, tool, agent_config):
+        command = ["pbench-list-tools", "--with-option"]
+        out, err, exitcode = pytest.helpers.capture(command)
+        # XXX this is the current output - pretty ugly
+        assert (
+            b"default: testhost.example.com [('iostat', '--interval=30'), ('mpstat', '--interval=300')]\ntest: testhost.example.com [('iostat', '--interval=30'), ('mpstat', '--interval=300')]\n"
+            == out
+        )
+        assert exitcode == 0
