bd99ead446d1fe98eb8ac967c5ee1f0f2b5bb739
Peter Portante <peter.portante@redhat.com>
2021-12-02 20:52:15 -0500

    Start using syslog identifier
    This makes journalctl entries go from looking like (notice the plain
    "journal" string that formats because the identifier is missing):
    
       Dec 03 02:43:02 srv.example.com journal[1402211]: WARNING 1402211 \
       140429661997952 pbench-backup-tarballs.pbench-backup-tarballs \
       sanity_check 132 -- Bucket not defined in config file - S3 backup \
       is disabled.
    
    To (each pbench command being run shows up):
    
       Dec 03 02:43:02 srv.example.com pbench-backup-tarballs[1402211]: \
       WARNING 1402211 140429661997952 \
       pbench-backup-tarballs.pbench-backup-tarballs \
       sanity_check 132 -- Bucket not defined in config file - S3 backup \
       is disabled.
    
    And one can use the journalctl `--identifier` switch to filter as well:
    
       journalctl -f --identifier=pbench-unpack-tarballs-small

--- server/lib/pbench/__init__.py
+++ server/lib/pbench/__init__.py
@@ -167,12 +167,14 @@ def get_pbench_logger(caller, config):
             handler = logging.FileHandler(os.path.join(logdir, "{}.log".format(caller)))
         elif config.logger_type == "devlog":
             handler = handlers.SysLogHandler(address="/dev/log")
+            handler.ident = caller
         elif (
             config.logger_type == "hostport"
         ):  # hostport logger type uses UDP-based logging
             handler = handlers.SysLogHandler(
                 address=(config.logger_host, int(config.logger_port))
             )
+            handler.ident = caller
         else:
             raise Exception("Unsupported logger type")
 
