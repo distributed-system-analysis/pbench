distributed-system-analysis/pbench

commit 55efafd5cdf2375f84a686a14ea3a6c56547bd21

pbench-list-tools and pbench-register-tools inconsistency with env and config values (#2606)
Fix pbench-list-tools and pbench-register tools setting of pbench_run (Fixes #2473); add tests.
Remove duplicate pbench_run PyTest fixture.

lib/pbench/agent/base.py
@@ -21,12 +21,17 @@ class BaseCommand(metaclass=abc.ABCMeta):
         self.config = PbenchAgentConfig(self.context.config)
         self.name = os.path.basename(sys.argv[0])
 
-        self.pbench_run = self.config.pbench_run
+        env_pbench_run = os.environ.get("pbench_run")
+        self.pbench_run = pathlib.Path(
+            env_pbench_run if env_pbench_run else self.config.pbench_run
+        )
+
         if not self.pbench_run.exists():
-            click.secho(
-                f"[ERROR] the provided pbench run directory, {self.pbench_run}, does not exist"
+            click.echo(
+                f"[ERROR] pbench run directory does not exist, {self.pbench_run}",
+                err=True,
             )
-            sys.exit(1)
+            click.get_current_context().exit(1)
 
         # the pbench temporary directory is always relative to the $pbench_run
         # directory

lib/pbench/test/functional/conftest.py
@@ -1,7 +1,5 @@
-import os
-import subprocess
-
 import pytest
+import subprocess
 
 from pbench.test.unit.agent.conftest import base_setup
 
@@ -19,7 +17,9 @@ def capture(command):
 
 
 @pytest.fixture
-def pbench_run(tmpdir):
-    pbench_run = tmpdir / "test/var/lib/pbench-agent"
-    os.makedirs(pbench_run)
-    yield pbench_run
+def pbench_run(monkeypatch, tmp_path):
+    monkeypatch.delenv("pbench_run")
+    pbench_agent_d = tmp_path / "var" / "lib" / "pbench-agent"
+    pbench_agent_d.mkdir(parents=True, exist_ok=True)
+
+    yield pbench_agent_d

lib/pbench/test/functional/agent/cli/commands/conftest.py
@@ -22,14 +22,6 @@ def pbench_cfg(tmp_path, opt_pbench):
 
 
 @pytest.fixture
-def pbench_run(tmp_path):
-    pbench_agent_d = tmp_path / "var" / "lib" / "pbench-agent"
-    pbench_agent_d.mkdir(parents=True, exist_ok=True)
-
-    yield pbench_agent_d
-
-
-@pytest.fixture
 def agent_config(monkeypatch, tmp_path, opt_pbench, pbench_cfg, pbench_run):
     shutil.copyfile("./agent/config/pbench-agent-default.cfg", pbench_cfg)
 

lib/pbench/test/functional/agent/cli/commands/test_config.py
@@ -1,0 +1,40 @@
+import os
+import pytest
+
+
+def test_pbench_run(monkeypatch, tmpdir, agent_config, pbench_run):  # noqa F811
+    """Use pbench_run value from pbench-agent.cfg file"""
+    assert os.environ.get("pbench_run") is None
+    command = ["pbench-list-tools"]
+    out, err, exitcode = pytest.helpers.capture(command)
+    assert exitcode == 0
+
+
+def test_pbench_run_dir_existence(monkeypatch, agent_config, pbench_run):
+    """Verify that pbench_list_tools fail when pbench_run is defined
+    to a directory that doesn't exist"""
+    monkeypatch.setenv("pbench_run", f"{pbench_run}/test")
+    command = ["pbench-list-tools"]
+    out, err, exitcode = pytest.helpers.capture(command)
+    assert exitcode == 1
+    assert b"" == out
+    assert b"[ERROR] pbench run directory does not exist," in err
+
+
+def test_pbench_dir_existence(monkeypatch, pbench_run, agent_config):
+    """Verify the existence of existing pbench_run directory
+    and successful completion of command"""
+    pbench_run_d = pbench_run / "test_dir"
+    pbench_run_d.mkdir(parents=True, exist_ok=True)
+    monkeypatch.setenv("pbench_run", str(pbench_run_d))
+    p = pbench_run_d / "tools-v1-default" / "th1.example.com"
+    p.mkdir(parents=True)
+    tool = p / "iostat"
+    tool.write_text("--interval=30")
+    tool = p / "mpstat"
+    tool.write_text("--interval=300")
+    command = ["pbench-list-tools"]
+    out, err, exitcode = pytest.helpers.capture(command)
+    assert exitcode == 0
+    assert b"group: default; host: th1.example.com; tools: iostat, mpstat\n" in out
+    assert b"" == err

commit af4fc3eb2e3aa7238711b118aea0a6b03c2babda

Remove `BaseCommand` environment manipulations
There was some thinking that the Tool Meister sub-system needed various
environment variables set in the `BaseCommand` class.  Because of how
the `pytest` unit tests are run, where the `os.environ` dictionary is
shared across tests, these variables would show up for any test that
ran subsequent to a test that invoked a `BaseCommand` sub-class.

We experimented with removing them entirely, and could not find any test
that failed as a result.  This also resolves the inadvertant problem
introduced by PR #2606 which caused `tox -e py3-server` runs to fail.

Fixes #2647.

lib/pbench/agent/base.py
@@ -63,12 +63,7 @@ class BaseCommand(metaclass=abc.ABCMeta):
         self.logger = setup_logging(debug=False, logfile=self.pbench_log)
 
         self.ssh_opts = self.config.ssh_opts
-        os.environ["ssh_opts"] = self.ssh_opts
-
         self.scp_opts = self.config.scp_opts
-        os.environ["scp_opts"] = self.scp_opts
-
-        os.environ["_pbench_debug_mode"] = "0"
         if os.environ.get("_PBENCH_UNIT_TESTS"):
             self.date = "1900-01-01T00:00:00"
             self.date_suffix = "1900.01.01T00.00.00"
@@ -79,22 +74,6 @@ class BaseCommand(metaclass=abc.ABCMeta):
             self.date_suffix = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H.%M.%s")
             self.hostname = socket.gethostname()
             self.full_hostname = socket.getfqdn()
-
-        # Backwards compatibility and for toolmeister
-        pbench_env = {
-            "date": self.date,
-            "date_suffix": self.date_suffix,
-            "hostname": self.hostname,
-            "full_hostname": self.full_hostname,
-            "pbench_run": str(self.pbench_run),
-            "pbench_install_dir": str(self.pbench_install_dir),
-            "pbench_tmp": str(self.pbench_tmp),
-            "pbench_log": str(self.pbench_log),
-            "pbench_bspp_dir": str(self.pbench_bspp_dir),
-            "pbench_lib_dir": str(self.pbench_lib_dir),
-        }
-        for k, v in pbench_env.items():
-            os.environ[k] = v
 
     @abc.abstractmethod
     def execute(self):

lib/pbench/test/functional/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import subprocess
 
@@ -18,7 +19,7 @@ def capture(command):
 
 @pytest.fixture
 def pbench_run(monkeypatch, tmp_path):
-    monkeypatch.delenv("pbench_run")
+    assert "pbench_run" not in os.environ
     pbench_agent_d = tmp_path / "var" / "lib" / "pbench-agent"
     pbench_agent_d.mkdir(parents=True, exist_ok=True)
 