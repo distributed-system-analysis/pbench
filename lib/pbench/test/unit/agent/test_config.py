0b3c2d4029aaccbbcb2b5fc3b855c48591551a25
Riya <riyad17031998@gmail.com>
2022-03-11 00:18:48 +0530

    Converting 'test_config.py' functional test to unit test

--- lib/pbench/test/unit/agent/test_config.py
+++ lib/pbench/test/unit/agent/test_config.py
@@ -1,8 +1,12 @@
+import click
 import os
 import pytest
 import shutil
 
+from pathlib import Path
+
 from pbench.agent import PbenchAgentConfig
+from pbench.agent.base import BaseCommand
 from pbench.common.exceptions import BadConfig
 
 
@@ -34,3 +38,72 @@ def test_results_attr():
         "pbench"
         in PbenchAgentConfig(os.environ["_PBENCH_AGENT_CONFIG"]).results["user"]
     )
+
+
+# Monkeypatching different Classes and Methods to test Pbench-run value
+
+
+class MockPbenchAgentConfig:
+    """Mock class for PbenchAgentConfig"""
+
+    def __init__(self, cfg_name: str):
+        self.pbench_run = "/test/pbench-run"
+        self.pbench_log = "logger"
+        self.pbench_install_dir = Path("/test/install_dir")
+        self.ssh_opts = "/ssh_opts"
+        self.scp_opts = "/scp_opts"
+
+
+def mock_exist(path: Path):
+    """mock method for Path.exist"""
+    return True
+
+
+def mock_current_context():
+    """mock get_current_context from click package"""
+    None
+
+
+class MockContext:
+    """Mocking Context"""
+
+    config = "/test/pbench-config-path"
+
+
+class TestCommand(BaseCommand):
+    """Mock of class that extends BaseCommand"""
+
+    def __init__(self):
+        super(TestCommand, self).__init__(MockContext())
+
+    def execute(self):
+        pass
+
+
+def test_pbench_run_path(monkeypatch):
+    """Use pbench_run value from pbench-agent.cfg file"""
+    assert os.environ.get("pbench_run") is None
+    monkeypatch.setattr(PbenchAgentConfig, "__init__", MockPbenchAgentConfig.__init__)
+    monkeypatch.setattr(Path, "exists", mock_exist)
+    pbench_run_val = TestCommand().pbench_run
+    assert str(pbench_run_val) == "/test/pbench-run"
+
+
+def test_pbench_run_as_env(monkeypatch):
+    """Use pbench_run value, passed manually through
+    `pbench-run` environment variable"""
+    monkeypatch.setenv("pbench_run", "/test/pbench-run/environ")
+    monkeypatch.setattr(PbenchAgentConfig, "__init__", MockPbenchAgentConfig.__init__)
+    monkeypatch.setattr(Path, "exists", mock_exist)
+    pbench_run_val = TestCommand().pbench_run
+    assert str(pbench_run_val) == "/test/pbench-run/environ"
+
+
+def test_pbench_run_dir_existence(monkeypatch):
+    """Verify that pbench_list_tools fail when pbench_run is defined
+    to a directory that doesn't exist"""
+    monkeypatch.setattr(PbenchAgentConfig, "__init__", MockPbenchAgentConfig.__init__)
+    monkeypatch.setattr(click, "get_current_context", mock_current_context)
+    pbench_run = TestCommand().pbench_run
+    assert pbench_run
+    # assert pbench_run.startswith("[ERROR] pbench run directory does not exist")
