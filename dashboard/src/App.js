ddd52a5f69f59cb7ad526c70c22f17e60f40f7c5
MVarshini <varm@redhat.com>
2022-04-01 18:40:55 +0530

    Auth Layout

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import favicon from './assets/logo/favicon.ico';
 import './App.css';
 import MainLayout from "./modules/containers/MainLayout/index";
+import AuthLayout from "./modules/containers/AuthLayout/index";
+import * as AppRoutes from "./utils/routeConstants";
+import LoginComponent from './modules/components/LoginComponent';
 import '@patternfly/patternfly/patternfly.css';
-
 
 function App() {  
   
@@ -11,12 +14,20 @@ function App() {
     const faviconLogo = document.getElementById("favicon");
     faviconLogo.setAttribute("href", favicon);
   }, [])
+  
 
- 
+  
   return (
     <div className="App">
-      Pbench Dashboard
-      <MainLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path={AppRoutes.AUTH} element={<AuthLayout />}>
+            <Route path={AppRoutes.AUTH_LOGIN} element={<LoginComponent />} />          
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div> 
   );
 }
---
+++ dashboard/src/utils/routeConstants.js
@@ -0,0 +1,2 @@
+export const AUTH = "auth"
+export const AUTH_LOGIN = "login"
