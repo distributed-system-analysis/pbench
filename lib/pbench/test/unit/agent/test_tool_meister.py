cff0f049d243f4453b40302112933d72dc1816a9
Peter Portante <peter.portante@redhat.com>
2022-04-05 21:32:52 -0400

    Move the single-use mock methods to where used
    We also promote the `tool_meister` mock to the module level, and ensure
    the `functions_called` is cleared after use.

--- lib/pbench/test/unit/agent/test_tool_meister.py
+++ lib/pbench/test/unit/agent/test_tool_meister.py
@@ -15,34 +15,35 @@ from pbench.agent.tool_meister import ToolMeister, ToolMeisterError
 
 tar_file = "test.tar.xz"
 tmp_dir = pathlib.Path("nonexistent/tmp/dir")
+tm_params = {
+    "benchmark_run_dir": "",
+    "channel_prefix": "",
+    "tds_hostname": "test.hostname.com",
+    "tds_port": 4242,
+    "controller": "test.hostname.com",
+    "group": "",
+    "hostname": "test.hostname.com",
+    "label": None,
+    "tool_metadata": {"persistent": {}, "transient": {}},
+    "tools": [],
+}
+
+
+@pytest.fixture
+def tool_meister():
+    return ToolMeister(
+        pbench_install_dir=None,
+        tmp_dir=None,
+        tar_path="tar_path",
+        sysinfo_dump=None,
+        params=tm_params,
+        redis_server=None,
+        logger=logging.getLogger(),
+    )
 
 
 class TestCreateTar:
-    tm_params = {
-        "benchmark_run_dir": "",
-        "channel_prefix": "",
-        "tds_hostname": "test.hostname.com",
-        "tds_port": 4242,
-        "controller": "test.hostname.com",
-        "group": "",
-        "hostname": "test.hostname.com",
-        "label": None,
-        "tool_metadata": {"persistent": {}, "transient": {}},
-        "tools": [],
-    }
-
-    @staticmethod
-    @pytest.fixture
-    def tool_meister():
-        return ToolMeister(
-            pbench_install_dir=None,
-            tmp_dir=None,
-            tar_path="tar_path",
-            sysinfo_dump=None,
-            params=__class__.tm_params,
-            redis_server=None,
-            logger=logging.getLogger(),
-        )
+    """Test the ToolMeister._create_tar() method behaviors."""
 
     @staticmethod
     def test_create_tar(tool_meister, monkeypatch):
@@ -113,7 +114,8 @@ class TestCreateTar:
         assert cp.returncode == 1
         assert cp.stdout == b"Some error running tar command, empty tar creation failed"
         assert (
-            f"Tarball creation failed with 1 (stdout 'Some error running tar command, empty tar creation failed') on {tmp_dir}: Re-trying now"
+            "Tarball creation failed with 1 (stdout 'Some error running tar"
+            f" command, empty tar creation failed') on {tmp_dir}: Re-trying now"
             in caplog.text
         )
 
@@ -122,15 +124,9 @@ class TestSendDirectory:
     """Test ToolMeister._send_directory()'s use of ._create_tar()"""
 
     # Record all the mock functions called by a test
-    function_called = []
+    functions_called = []
 
-    directory = tmp_dir / f"{TestCreateTar.tm_params['hostname']}"
-
-    @staticmethod
-    def add_http_mock_response(
-        uri: str, code: HTTPStatus = HTTPStatus.OK, text: str = "succeeded"
-    ):
-        responses.add(responses.PUT, uri, status=code, body=text)
+    directory = tmp_dir / f"{tm_params['hostname']}"
 
     @staticmethod
     def fake_create_tar(returncode: int, stdout: bytes):
@@ -139,60 +135,47 @@ class TestSendDirectory:
                 args=[], returncode=returncode, stdout=stdout, stderr=None
             )
 
-        TestSendDirectory.function_called.append("fake_create_tar")
+        __class__.functions_called.append("fake_create_tar")
         return f
 
-    @staticmethod
-    def fake_unlink(*args):
-        assert args[0] == pathlib.Path(f"{TestSendDirectory.directory}.tar.xz")
-        TestSendDirectory.function_called.append("fake_unlink")
-        pass
-
-    @staticmethod
-    def fake_open(*args):
-        assert args[0] == pathlib.Path(f"{TestSendDirectory.directory}.tar.xz")
-        TestSendDirectory.function_called.append("fake_open")
-        return io.StringIO()
-
-    def fake_rmtree(self, directory: pathlib.Path):
-        assert directory == tmp_dir
-        self.function_called.append("fake_rmtree")
-        pass
-
-    def fake_md5(self, tar_file: pathlib.Path):
-        assert tar_file == pathlib.Path(f"{self.directory}.tar.xz")
-        self.function_called.append("fake_md5")
-        return 10, "random_md5"
-
     @responses.activate
-    def test_tar_create_success(self, monkeypatch):
+    def test_tar_create_success(self, tool_meister, monkeypatch):
         """This test should pass the tar creation in send directory"""
 
-        monkeypatch.setattr(shutil, "rmtree", self.fake_rmtree)
-        monkeypatch.setattr("pbench.agent.tool_meister.md5sum", self.fake_md5)
-        monkeypatch.setattr(pathlib.Path, "unlink", self.fake_unlink)
-        monkeypatch.setattr(pathlib.Path, "open", self.fake_open)
+        def fake_unlink(*args):
+            assert args[0] == pathlib.Path(f"{self.directory}.tar.xz")
+            self.functions_called.append("fake_unlink")
 
-        tm = ToolMeister(
-            pbench_install_dir=None,
-            tmp_dir=None,
-            tar_path="tar_path",
-            sysinfo_dump=None,
-            params=TestCreateTar.tm_params,
-            redis_server=None,
-            logger=logging.getLogger(),
-        )
+        def fake_open(*args):
+            assert args[0] == pathlib.Path(f"{self.directory}.tar.xz")
+            self.functions_called.append("fake_open")
+            return io.StringIO()
 
-        monkeypatch.setattr(tm, "_create_tar", self.fake_create_tar(0, b""))
+        def fake_rmtree(directory: pathlib.Path):
+            assert directory == tmp_dir
+            self.functions_called.append("fake_rmtree")
+
+        def fake_md5(tar_file: pathlib.Path):
+            assert tar_file == pathlib.Path(f"{self.directory}.tar.xz")
+            self.functions_called.append("fake_md5")
+            return 10, "random_md5"
+
+        monkeypatch.setattr(shutil, "rmtree", fake_rmtree)
+        monkeypatch.setattr("pbench.agent.tool_meister.md5sum", fake_md5)
+        monkeypatch.setattr(pathlib.Path, "unlink", fake_unlink)
+        monkeypatch.setattr(pathlib.Path, "open", fake_open)
+
+        monkeypatch.setattr(tool_meister, "_create_tar", self.fake_create_tar(0, b""))
 
         url = (
-            f"http://{TestCreateTar.tm_params['tds_hostname']}:{TestCreateTar.tm_params['tds_port']}/uri"
-            f"/ctx/{TestCreateTar.tm_params['hostname']}"
+            f"http://{tm_params['tds_hostname']}:{tm_params['tds_port']}/uri"
+            f"/ctx/{tm_params['hostname']}"
         )
-        self.add_http_mock_response(url)
+        responses.add(responses.PUT, url, status=HTTPStatus.OK, body="succeeded")
 
-        failures = tm._send_directory(self.directory, "uri", "ctx")
-        assert self.function_called == [
+        failures = tool_meister._send_directory(self.directory, "uri", "ctx")
+        functions_called, self.functions_called = self.functions_called, []
+        assert functions_called == [
             "fake_create_tar",
             "fake_md5",
             "fake_open",
@@ -201,25 +184,19 @@ class TestSendDirectory:
         ]
         assert failures == 0
 
-    def test_tar_create_failure(self, monkeypatch):
+    def test_tar_create_failure(self, tool_meister, monkeypatch):
         """Check if the tar creation error is properly captured in send_directory"""
-        tm = ToolMeister(
-            pbench_install_dir=None,
-            tmp_dir=None,
-            tar_path="tar_path",
-            sysinfo_dump=None,
-            params=TestCreateTar.tm_params,
-            redis_server=None,
-            logger=logging.getLogger(),
-        )
-
         monkeypatch.setattr(
-            tm, "_create_tar", self.fake_create_tar(1, b"Error in tarball creation")
+            tool_meister,
+            "_create_tar",
+            self.fake_create_tar(1, b"Error in tarball creation"),
         )
 
         with pytest.raises(ToolMeisterError) as exc:
-            failures = tm._send_directory(self.directory, "uri", "ctx")
+            failures = tool_meister._send_directory(self.directory, "uri", "ctx")
             assert failures == 1
+        functions_called, self.functions_called = self.functions_called, []
+        assert functions_called == ["fake_create_tar"]
         assert f"Failed to create an empty tar {self.directory}.tar.xz" in str(
             exc.value
         )
