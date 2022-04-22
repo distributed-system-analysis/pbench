9c1f41bde46c9f3fcad64c5b56cb62c076ea420c
MVarshini <varm@redhat.com>
2022-04-22 14:15:44 +0530

    New line added

--- dashboard/src/modules/components/AuthComponent/common-components.jsx
+++ dashboard/src/modules/components/AuthComponent/common-components.jsx
@@ -165,3 +165,4 @@ export const NoLoginComponent = () => {
     </div>
   );
 };
+ 
--- dashboard/src/utils/apiConstants.js
+++ dashboard/src/utils/apiConstants.js
@@ -1,4 +1,4 @@
 export const LOCAL_BASE_URL = "http://10.1.170.201/";
 
 export const REGISTER = "api/v1/register";
-export const LOGIN = "api/v1/login"
+export const LOGIN = "api/v1/login";
