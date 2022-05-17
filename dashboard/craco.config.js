1c752052072b5c2167a7d25bff19cbdded5e0c7b
MVarshini <varm@redhat.com>
2022-05-18 01:42:23 +0530

    craco file to load less

---
+++ dashboard/craco.config.js
@@ -0,0 +1,19 @@
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#1DA57A",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
