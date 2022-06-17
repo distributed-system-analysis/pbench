5e19f458ee22f735c37f1a50780d6b96c4ae1dea
Webb Scales <wscales@redhat.com>
2022-06-17 17:33:53 -0400

    Tweaks to the Dashboard to make it pass lint/unit-tests. (Hopefully, these will be merged from other PRs.)

--- dashboard/src/App.test.js
+++ dashboard/src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from "react-redux";
+import store from "store/store";
+import React from 'react';
+
+const AppWrapper = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<AppWrapper />);
+  const linkElement = screen.getByText('Explore');
   expect(linkElement).toBeInTheDocument();
 });
