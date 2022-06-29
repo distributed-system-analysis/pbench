5da37367de198d3c16d08d4a0a34dc2c88ce507c
siddardh <sira@redhat27!>
2022-06-29 13:37:27 +0530

    Refactored unittest scripts in test_datasets_inventory.py

--- lib/pbench/test/unit/server/test_datasets_inventory.py
+++ lib/pbench/test/unit/server/test_datasets_inventory.py
@@ -12,15 +12,13 @@ if TYPE_CHECKING:
     from _typeshed.wsgi import WSGIEnvironment
     from flask.wrappers import Response
 
-from pbench.server import PbenchServerConfig
 from pbench.server.database.models.datasets import Dataset, DatasetNotFound
 from pbench.server.filetree import FileTree
-from pbench.test.unit.server.conftest import generic_password
 
 
 class TestDatasetsAccess:
     @pytest.fixture()
-    def query_get_as(self, client, server_config, more_datasets):
+    def query_get_as(self, client, server_config, more_datasets, pbench_token):
         """
         Helper fixture to perform the API query and validate an expected
         return status.
@@ -32,14 +30,13 @@ class TestDatasetsAccess:
         """
 
         def query_api(
-            dataset: str, username: str, expected_status: HTTPStatus, path: str
+            dataset: str, expected_status: HTTPStatus, path: str
         ) -> requests.Response:
             try:
                 dataset_id = Dataset.query(name=dataset).resource_id
             except DatasetNotFound:
                 dataset_id = dataset  # Allow passing deliberately bad value
-            token = self.token(client, server_config, username)
-            headers = {"authorization": f"bearer {token}"}
+            headers = {"authorization": f"bearer {pbench_token}"}
             response = client.get(
                 f"{server_config.rest_uri}/datasets/inventory/{dataset_id}/{path}",
                 headers=headers,
@@ -48,16 +45,6 @@ class TestDatasetsAccess:
             return response
 
         return query_api
-
-    def token(self, client, config: PbenchServerConfig, user: str) -> str:
-        response = client.post(
-            f"{config.rest_uri}/login",
-            json={"username": user, "password": generic_password},
-        )
-        assert response.status_code == HTTPStatus.OK
-        data = response.json
-        assert data["auth_token"]
-        return data["auth_token"]
 
     def mock_send_file(
         self,
@@ -84,14 +71,20 @@ class TestDatasetsAccess:
 
     def test_get_no_dataset(self, query_get_as):
         response = query_get_as(
-            "nonexistent-dataset", "drb", HTTPStatus.NOT_FOUND, "metadata.log"
+            "nonexistent-dataset", HTTPStatus.NOT_FOUND, "metadata.log"
         )
         assert response.json == {"message": "Dataset 'nonexistent-dataset' not found"}
 
-    def test_dataset_not_present(self, query_get_as, monkeypatch):
-        response = query_get_as("fio_2", "test", HTTPStatus.NOT_FOUND, "metadata.log")
+    def test_dataset_not_present(self, query_get_as):
+        response = query_get_as("fio_2", HTTPStatus.NOT_FOUND, "metadata.log")
         assert response.json == {
             "message": "The dataset tarball named 'fio_2' is not present in the file tree"
+        }
+
+    def test_unauthorized_access(self, query_get_as):
+        response = query_get_as("test", HTTPStatus.FORBIDDEN, "metadata.log")
+        assert response.json == {
+            "message": "User drb is not authorized to READ a resource owned by test with private access"
         }
 
     def test_dataset_is_not_unpacked(self, query_get_as, monkeypatch):
@@ -102,7 +95,8 @@ class TestDatasetsAccess:
             return Tarball
 
         monkeypatch.setattr(FileTree, "find_dataset", mock_find_not_unpacked)
-        response = query_get_as("fio_2", "test", HTTPStatus.NOT_FOUND, "1-default")
+
+        response = query_get_as("fio_2", HTTPStatus.NOT_FOUND, "1-default")
         assert response.json == {"message": "The dataset is not unpacked"}
 
     def test_path_is_directory(self, query_get_as, monkeypatch):
@@ -110,9 +104,7 @@ class TestDatasetsAccess:
         monkeypatch.setattr(Path, "is_file", lambda self: False)
         monkeypatch.setattr(Path, "exists", lambda self: True)
 
-        response = query_get_as(
-            "fio_2", "test", HTTPStatus.UNSUPPORTED_MEDIA_TYPE, "1-default"
-        )
+        response = query_get_as("fio_2", HTTPStatus.UNSUPPORTED_MEDIA_TYPE, "1-default")
         assert response.json == {
             "message": "The specified path does not refer to a regular file"
         }
@@ -122,7 +114,7 @@ class TestDatasetsAccess:
         monkeypatch.setattr(Path, "is_file", lambda self: False)
         monkeypatch.setattr(Path, "exists", lambda self: False)
 
-        response = query_get_as("fio_2", "test", HTTPStatus.NOT_FOUND, "1-default")
+        response = query_get_as("fio_2", HTTPStatus.NOT_FOUND, "1-default")
         assert response.json == {
             "message": "The specified path does not refer to a file"
         }
@@ -132,5 +124,5 @@ class TestDatasetsAccess:
         monkeypatch.setattr(Path, "is_file", lambda self: True)
         monkeypatch.setattr(werkzeug.utils, "send_file", self.mock_send_file)
 
-        response = query_get_as("fio_1", "drb", HTTPStatus.OK, "1-default/default.csv")
+        response = query_get_as("fio_2", HTTPStatus.OK, "1-default/default.csv")
         assert response.status_code == HTTPStatus.OK
