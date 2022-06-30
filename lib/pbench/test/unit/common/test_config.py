68d6fde2f456ed9e195025a6ff437b9710a54c57
Riya <riyad17031998@gmail.com>
2022-07-01 01:33:14 +0530

    PbenchConfig unit-test Test Coverage (#2911)
    PBENCH-213
    
    Increases the test coverage for the PbenchConfig class.
    Removed unnecessary file usage along the script.

--- lib/pbench/test/unit/common/test_config.py
+++ lib/pbench/test/unit/common/test_config.py
@@ -1,68 +1,162 @@
 """Test PbenchConfig class and objects
 """
-
 import pytest
 
-from pathlib import Path
+from configparser import ConfigParser, NoOptionError, NoSectionError
+from typing import List, Dict, Any
 
 from pbench import PbenchConfig
+from pbench.common import configtools
 from pbench.common.exceptions import BadConfig
 
 
-_config_path_prefix = Path("lib/pbench/test/unit/common/config")
+class MockedConfigParser:
+    """Mocked ConfigParser"""
+
+    def __init__(self):
+        self._sections = {}
+
+    def get(self, section: str, option: str, fallback=None) -> str:
+        """returns value of the given `section` and `option`"""
+
+        if section not in self._sections:
+            raise NoSectionError(section)
+        if option not in self._sections[section]:
+            raise NoOptionError(option, section)
+        value = self._sections[section][option]
+        if not value:
+            if not fallback:
+                raise ValueError(option, section)
+            else:
+                return fallback
+        return value
 
 
 class TestPbenchConfig:
-    def test_empty_config(self):
-        config = PbenchConfig(_config_path_prefix / "pbench.cfg")
-        assert config.TZ == "UTC", f"Unexpected TZ value, {config.TZ!r}"
-        assert (
-            config.log_fmt is None
-        ), f"Unexpected log format value, {config.log_fmt!r}"
-        assert (
-            config.default_logging_level == "INFO"
-        ), f"Unexpected default logging level, {config.default_logging_level!r}"
-        assert (
-            config.log_using_caller_directory is False
-        ), f"Unexpected 'log using caller directory' boolean, {config.log_using_caller_directory!r}"
-        assert config.log_dir is None, f"Unexpected log directory, {config.log_dir!r}"
-        assert (
-            config.logger_type == "devlog"
-        ), f"Unexpected logger type, {config.logger_type!r}"
-        with pytest.raises(AttributeError):
-            print(f"{config.logger_host!r}")
-        with pytest.raises(AttributeError):
-            print(f"{config.logger_port!r}")
-        assert "42" == config.get(
-            "other", "foobar"
-        ), "Failed to fetch 'foobar' from 'DEFAULT' section"
-        assert "43" == config.get(
-            "other", "barfoo"
-        ), "Failed to fetch 'barfoo' from 'other' section"
-        assert isinstance(
-            config.files, list
-        ), f"Unexpected object class for 'files', {config.files.__class__!r}"
+    @staticmethod
+    def logger_test_setup(monkeypatch, sections: Dict[str, Any]):
+        def mock_ConfigParser_init(self):
+            self._sections = sections
 
-    def test_log_dir_provided(self):
-        config = PbenchConfig(_config_path_prefix / "logdir.cfg")
-        assert (
-            config.log_dir == "/srv/log/directory"
-        ), f"Unexpected log directory, {config.log_dir!r}"
+        monkeypatch.setattr(configtools, "file_list", TestPbenchConfig.mock_file_list)
+        monkeypatch.setattr(ConfigParser, "__init__", mock_ConfigParser_init)
+        monkeypatch.setattr(ConfigParser, "get", MockedConfigParser.get)
 
-    def test_logger_type_provided(self):
-        config = PbenchConfig(_config_path_prefix / "hostport.cfg")
-        assert (
-            config.logger_type == "hostport"
-        ), f"Unexpected logger type, {config.logger_type!r}"
-        assert (
-            config.logger_host == "logger.example.com"
-        ), f"Unexpected logger host value, {config.logger_host!r}"
-        assert (
-            config.logger_port == "42"
-        ), f"Unexpected logger port value, {config.logger_port!r}"
+    def mock_file_list(cfg_name: str) -> List[str]:
+        """file_list mock module"""
+        return [cfg_name]
 
-    def test_logger_type_hostport_missing(self):
-        with pytest.raises(BadConfig):
-            PbenchConfig(_config_path_prefix / "hostport-missing.cfg")
-        with pytest.raises(BadConfig):
-            PbenchConfig(_config_path_prefix / "hostport-missing-port.cfg")
+    def test_default_logger(self, monkeypatch):
+        """Show that PbenchConfig uses default values when NO
+        configuration values are provided for `logging`"""
+
+        self.logger_test_setup(monkeypatch, {})
+        config = PbenchConfig("pbench.cfg")
+        assert config.logger_type == "devlog"
+        assert config.log_dir is None
+        assert config.log_using_caller_directory is False
+        assert config.default_logging_level == "INFO"
+        assert config.log_fmt is None
+        assert config.TZ == "UTC"
+
+    def test_custom_logger(self, monkeypatch):
+        """Show that when configuration values are provided,
+        PbenchConfig uses those values instead of default values"""
+
+        logger_type = "file"
+        log_dir = "/mock/log_dir"
+        logging_level = "Devlog"
+        log_format = "1970-01-01T00:00:42.000000 -- 'message'"
+        self.logger_test_setup(
+            monkeypatch,
+            {
+                "logging": {
+                    "logger_type": logger_type,
+                    "log_dir": log_dir,
+                    "logging_level": logging_level,
+                    "log_format": log_format,
+                }
+            },
+        )
+        config = PbenchConfig("pbench.cfg")
+        assert config.logger_type == logger_type
+        assert config.log_dir == log_dir
+        assert config.log_using_caller_directory is False
+        assert config.default_logging_level == logging_level
+        assert config.log_fmt == log_format
+        assert config.TZ == "UTC"
+
+    @pytest.mark.parametrize(
+        "config_value, error_type, expected_error_msg",
+        [
+            (
+                {"logging": {"logger_type": "hostport"}},
+                BadConfig,
+                "No option 'logger_host' in section: 'logging'",
+            ),
+            (
+                {"logging": {"logger_type": "hostport", "logger_host": ""}},
+                ValueError,
+                "ValueError('logger_host', 'logging')",
+            ),
+            (
+                {
+                    "logging": {
+                        "logger_type": "hostport",
+                        "logger_host": "logger.example.com",
+                    }
+                },
+                BadConfig,
+                "No option 'logger_port' in section: 'logging'",
+            ),
+            (
+                {
+                    "logging": {
+                        "logger_type": "hostport",
+                        "logger_host": "logger.example.com",
+                        "logger_port": "",
+                    }
+                },
+                ValueError,
+                "ValueError('logger_port', 'logging')",
+            ),
+        ],
+    )
+    def test_bad_logger_hostport(
+        self, config_value, error_type, expected_error_msg, monkeypatch
+    ):
+        """Shows that PbenchConfig raises 'BadConfig' and 'ValueError' when
+        the configuration does not contain required option or an empty
+        value respectively."""
+
+        self.logger_test_setup(monkeypatch, config_value)
+        with pytest.raises(error_type) as exc:
+            PbenchConfig("pbench.cfg")
+        assert expected_error_msg in str(exc)
+
+    def test_logger_hostport(self, monkeypatch):
+        """Show that PbenchConfig processes smoothly when the configuration
+        contains required values"""
+
+        logger_type = "hostport"
+        logger_host = "logger.example.com"
+        logger_port = "42"
+        self.logger_test_setup(
+            monkeypatch,
+            {
+                "logging": {
+                    "logger_type": logger_type,
+                    "logger_host": logger_host,
+                    "logger_port": logger_port,
+                }
+            },
+        )
+        config = PbenchConfig("pbench.cfg")
+        assert config.logger_type == logger_type
+        assert config.logger_host == logger_host
+        assert config.logger_port == logger_port
+        assert config.log_dir is None
+        assert config.log_using_caller_directory is False
+        assert config.default_logging_level == "INFO"
+        assert config.log_fmt is None
+        assert config.TZ == "UTC"
