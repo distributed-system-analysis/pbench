c3561deb63d398de45bf38cb7b4744b8a76d2d9c
Peter Portante <peter.portante@redhat.com>
2022-07-08 23:17:57 -0400

    Correct use of `sha1` hash in rpm builds
    Backport of 712506074 from PR #2937.

--- utils/rpm.mk
+++ utils/rpm.mk
@@ -68,7 +68,7 @@ submodules:
 ${subcomps}:
 	make -C ${PBENCHTOP}/$@ DESTDIR=${TBDIR}/$@ install
 
-$(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm: srpm
+$(RPMSRPM)/$(prog)-$(version)-$(seqno)g$(sha1).src.rpm: srpm
 
 ifdef COPR_USER
 _copr_user = ${COPR_USER}
@@ -78,7 +78,7 @@ endif
 
 COPR_TARGETS = copr copr-test
 .PHONY: ${COPR_TARGETS}
-${COPR_TARGETS}: $(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm
+${COPR_TARGETS}: $(RPMSRPM)/$(prog)-$(version)-$(seqno)g$(sha1).src.rpm
 	copr-cli build ${CHROOTS} $(_copr_user)/$(subst copr,pbench,$@) $(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
 
 .PHONY: distclean
