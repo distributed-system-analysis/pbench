315fe4c3ca87f714ce1e9a591840c310b8423a0d
Peter Portante <peter.portante@redhat.com>
2022-09-15 18:14:22 -0400

    Correct Read The Docs builds
    Starting around August 22nd, 2022, the Read The Docs builds began
    failing, likely due to the obsolescence of the `sphinx-panels` module.
    
    While verifying the build was fixed, we corrected a number of problems
    found with `Makefile` targets, the formatting of the configuration file,
    two warnings in the user guide, and dropped the Travis-CI documentation
    entirely.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ release = ""
 # ones.
 extensions = [
     "sphinx.ext.autosectionlabel",
-    "sphinx_panels",
+    "sphinx_design",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -82,7 +82,6 @@ html_theme = "sphinx_rtd_theme"
 if not os.environ.get("READTHEDOCS") == "True":
     import sphinx_rtd_theme
 
-    html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -94,7 +93,7 @@ if not os.environ.get("READTHEDOCS") == "True":
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
