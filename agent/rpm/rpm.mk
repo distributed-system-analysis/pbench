8906e9df1a52073ff635406fecc608a6ed60803c
Peter Portante <peter.portante@redhat.com>
2022-10-17 21:47:36 -0400

    Endow the `b0.69` branch with version COPR builds
    In order to work with the mechanisms of COPR RPM builds, where only one
    version of a RPM is available a COPR project/repository at a time, we
    need to use a versioned COPR project reference.

--- agent/rpm/rpm.mk
+++ agent/rpm/rpm.mk
@@ -1,7 +1,7 @@
 # -*- mode: makefile -*-
 .PHONY: check
 check:
-	if [ "${version}" == "" ] ;then echo "version undefined" > /dev/stderr; exit 1 ;fi
+	if [ "${VERSION}" == "" ] ;then echo "VERSION undefined" > /dev/stderr; exit 1 ;fi
 	if [ "${prog}" == "" ] ;then echo "prog undefined" > /dev/stderr ; exit 2 ;fi
 
 RPMDIRS = BUILD BUILDROOT SPECS SOURCES SRPMS RPMS
@@ -23,11 +23,11 @@ RPMSPEC = ${HOME}/rpmbuild/SPECS
 ###########################################################################
 
 # building on COPR.
-# version and sha1 have to be provided by the including Makefile.
+# VERSION and sha1 have to be provided by the including Makefile.
 # The including Makefile also has to provide the srpm target.
 
 .SECONDEXPANSION:
-$(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm: srpm
+$(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm: srpm
 
 ifdef COPR_USER
 _copr_user = ${COPR_USER}
@@ -35,8 +35,8 @@ else
 _copr_user = ${USER}
 endif
 
-copr\
-copr-test:	$(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm
-	copr-cli build ${CHROOTS} $(_copr_user)/$(subst copr,pbench,$@) $(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
+copr \
+copr-test:	$(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
+	copr-cli build $(CHROOTS) $(_copr_user)/$(subst copr,pbench-$(MAJORMINOR),$@) $(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
 
 veryclean:: clean rpm-clean
