47a7cdf77eccafdc67fdb8a64511dbf2cf4b3b7f
Peter Portante <peter.portante@redhat.com>
2022-10-17 21:47:36 -0400

    Endow the `b0.69` branch with version COPR builds
    In order to work with the mechanisms of COPR RPM builds, where only one
    version of a RPM is available a COPR project/repository at a time, we
    need to use a versioned COPR project reference.

--- server/rpm/rpm.mk
+++ server/rpm/rpm.mk
@@ -1,16 +1,18 @@
 # -*- mode: makefile -*-
 .PHONY: check
 check:
-	if [ "${version}" == "" ] ;then echo "version undefined" > /dev/stderr; exit 1 ;fi
+	if [ "${VERSION}" == "" ] ;then echo "VERSION undefined" > /dev/stderr; exit 1 ;fi
 	if [ "${prog}" == "" ] ;then echo "prog undefined" > /dev/stderr ; exit 2 ;fi
+
+RPMDIRS = BUILD BUILDROOT SPECS SOURCES SRPMS RPMS
 
 .PHONY: rpm-dirs
 rpm-dirs:
-	for i in BUILD BUILDROOT SPECS SOURCES SRPMS RPMS; do mkdir -p ${HOME}/rpmbuild/$$i ; done
+	for i in ${RPMDIRS}; do mkdir -p ${HOME}/rpmbuild/$$i ; done
 
 .PHONY: rpm-clean
-rpm-clean: rpm-dirs
-	for i in BUILD BUILDROOT SPECS SOURCES SRPMS RPMS; do rm -rf ${HOME}/rpmbuild/$$i/* ; done
+rpm-clean:
+	for i in ${RPMDIRS}; do rm -rf ${HOME}/rpmbuild/$$i/* ; done
 
 dot-clean:
 	rm -f .rpm-copy .spec-copy .build
@@ -18,7 +20,7 @@ dot-clean:
 .PHONY: spec-copy
 spec-copy: .spec-copy
 .spec-copy:
-	set-version-release ${version} $(prog).spec ${USE_GIT_SHA1} > ${HOME}/rpmbuild/SPECS/${prog}.spec && touch .spec-copy
+	set-version-release ${version} ${prog}.spec ${USE_GIT_SHA1} > ${HOME}/rpmbuild/SPECS/${prog}.spec && touch .spec-copy
 
 ###########################################################################
 # these are used in the Makefiles that include this file
@@ -29,11 +31,11 @@ RPMSPEC = ${HOME}/rpmbuild/SPECS
 ###########################################################################
 
 # building on COPR.
-# version and sha1 have to be provided by the including Makefile.
+# VERSION and sha1 have to be provided by the including Makefile.
 # The including Makefile also has to provide the srpm target.
 
 .SECONDEXPANSION:
-$(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm: srpm
+$(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm: srpm
 
 ifdef COPR_USER
 _copr_user = ${COPR_USER}
@@ -41,13 +43,9 @@ else
 _copr_user = ${USER}
 endif
 
-copr\
-copr-test \
-copr-interim \
-copr-index \
-copr-inotify \
-copr-dashboard:	$(RPMSRPM)/$(prog)-$(version)-$(seqno)$(sha1).src.rpm
-	copr-cli build $(_copr_user)/$(subst copr,pbench,$@) $(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
+copr \
+copr-test:	$(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
+	copr-cli build $(CHROOTS) $(_copr_user)/$(subst copr,pbench-$(MAJORMINOR),$@) $(RPMSRPM)/$(prog)-$(VERSION)-$(seqno)g$(sha1).src.rpm
 
 # The default clean target
 clean:: dot-clean
