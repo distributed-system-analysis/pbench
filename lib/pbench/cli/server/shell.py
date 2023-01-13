27b1bd026165a201f3f201c04fe04b2e165fcd61
Peter Portante <peter.portante@redhat.com>
2023-01-13 16:46:11 -0500

    Consolidate database and OIDC knowledge
    Correct where `configparser` errors are imported.
    
    Consolidate database knowledge by moving the intricate details of
    working with the database to the `Database` class as static methods.
    
    We also clean up the alembic `env.py` file as a result.
    
    Consolidate OIDC knowledge in `OpenIDClient` class.

--- lib/pbench/cli/server/shell.py
+++ lib/pbench/cli/server/shell.py
@@ -1,23 +1,15 @@
 from configparser import NoOptionError, NoSectionError
-from http import HTTPStatus
 from logging import Logger
 import os
 from pathlib import Path
 import site
-import socket
 import subprocess
 import sys
-import time
-from urllib.parse import urlparse
-
-import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from sqlalchemy_utils import create_database, database_exists
 
 from pbench.common.exceptions import BadConfig, ConfigFileNotSpecified
 from pbench.common.logger import get_pbench_logger
 from pbench.server.api import create_app, get_server_config
+from pbench.server.auth import OpenIDClient
 from pbench.server.database.database import Database
 
 PROG = "pbench-shell"
@@ -45,96 +37,6 @@ def find_the_unicorn(logger: Logger):
             "Found a local unicorn: augmenting server PATH to {}",
             os.environ["PATH"],
         )
-
-
-def wait_for_database(db_uri: str, timeout: int):
-    """Wait for the database server to become available.  While we encounter
-    "connection refused", sleep one second, and then try again.
-
-    No connection attempt is made for a database URI without a hostname.
-
-    The timeout argument to `create_connection()` does not play into the retry
-    logic, see:
-
-      https://docs.python.org/3.9/library/socket.html#socket.create_connection
-
-    Arguments:
-
-        timeout: integer number of seconds to wait before giving up attempts to
-                 connect to the database
-
-    Raises a BadConfig exception if the DB URI specifies a host without a port,
-    and the ConnectionRefusedError enountered after the timeout.
-    """
-    url = urlparse(db_uri)
-    if not url.hostname:
-        return
-    if not url.port:
-        raise BadConfig("Database URI must contain a port number")
-    end = time.time() + timeout
-    while True:
-        try:
-            with socket.create_connection((url.hostname, url.port), timeout=1):
-                break
-        except ConnectionRefusedError:
-            if time.time() > end:
-                raise
-            time.sleep(1)
-
-
-def keycloak_connection(oidc_server: str, logger: Logger) -> int:
-    """
-     Checks if the Keycloak server is up and accepting the connections.
-     The connection check does the GET request on the oidc server /health
-     endpoint and the sample response returned by the /health endpoint looks
-     like the following:
-        {
-            "status": "UP", # if the server is up
-            "checks": []
-        }
-     Note: The Keycloak server needs to be started with health-enabled on.
-     Args:
-        oidc_server: OIDC server to verify
-        logger: logger
-    Returns:
-        0 if successful
-        1 if unsuccessful
-    """
-    session = requests.Session()
-    # The connection check will retry multiple times unless successful, viz.,
-    # [0.0s, 4.0s, 8.0s, 16.0s, ..., 120.0s]. urllib3 will sleep for:
-    # {backoff factor} * (2 ^ ({number of total retries} - 1)) seconds between
-    # the retries. However, the sleep will never be longer than backoff_max
-    # which defaults to 120.0s More detailed documentation on Retry and
-    # backoff_factor can be found at:
-    # https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html#module-urllib3.util.retry
-    retry = Retry(
-        total=8,
-        backoff_factor=2,
-        status_forcelist=tuple(int(x) for x in HTTPStatus if x != 200),
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount("http://", adapter)
-
-    # We will also need to retry the connection if the health status is not UP.
-    ret_val = 1
-    for _ in range(5):
-        try:
-            response = session.get(f"{oidc_server}/health")
-            response.raise_for_status()
-        except Exception:
-            logger.exception("Error connecting to the OIDC client")
-            break
-        if response.json().get("status") == "UP":
-            logger.debug("OIDC server connection verified")
-            ret_val = 0
-            break
-        else:
-            logger.error(
-                "OIDC client not running, OIDC server response: {}", response.json()
-            )
-            retry.sleep()
-    return ret_val
 
 
 def generate_crontab_if_necessary(
@@ -203,31 +105,31 @@ def main():
 
     logger.debug("Waiting for database instance to become available.")
     try:
-        wait_for_database(db_uri, db_wait_timeout)
+        Database.wait_for_database(db_uri, db_wait_timeout)
     except ConnectionRefusedError:
         logger.error("Database {} not responding", db_uri)
         sys.exit(1)
 
     try:
-        oidc_server = server_config.get("authentication", "server_url")
-    except (NoOptionError, NoSectionError) as exc:
-        logger.warning("KeyCloak not configured, {}", exc)
+        oidc_server = OpenIDClient.wait_for_oidc_server(server_config, logger)
+    except OpenIDClient.NotConfigured as exc:
+        logger.warning("OpenID Connect client not configured, {}", exc)
+    except Exception as exc:
+        logger.error("Error connecting to OpenID Connect server, {}", exc)
+        sys.exit(1)
     else:
-        logger.debug("Waiting for OIDC server to become available.")
-        ret_val = keycloak_connection(oidc_server, logger)
-        if ret_val != 0:
-            sys.exit(ret_val)
         logger.info("Pbench server using OIDC server {}", oidc_server)
 
     # Multiple gunicorn workers will attempt to connect to the DB; rather than
     # attempt to synchronize them, detect a missing DB (from the database URI)
     # and create it here. It's safer to do this here, where we're
     # single-threaded.
-    if not database_exists(db_uri):
-        logger.info("Database {} doesn't exist", db_uri)
-        create_database(db_uri)
-        logger.info("Created database {}", db_uri)
-    Database.init_db(server_config, logger)
+    Database.create_if_missing(db_uri, logger)
+    try:
+        Database.init_db(server_config, logger)
+    except (NoOptionError, NoSectionError) as exc:
+        logger.error("Invalid database configuration: {}", exc)
+        sys.exit(1)
 
     ret_val = generate_crontab_if_necessary(
         crontab_dir, server_config.BINDIR, server_config.log_dir, logger
