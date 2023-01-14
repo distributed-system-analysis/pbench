df4be89f5735205c326a2e7a302f3812b7a50ec2
Peter Portante <peter.portante@redhat.com>
2023-01-14 09:06:32 -0500

    Refactor `PbenchServerConfig`
    This is a major refactoring of the `PbenchServerConfig` class and top
    level server module.
    
    The first change is to remove unused properties for `server_config`,
    `mail_recipients`, `_unittests`, and `_ref_datetime`.
    
    Then we use properties for all the attributes set in the constructor
    which require the use of instance methods.  Since the original behavior
    was for the constructor to raise `BadConfig` for missing required
    options, we create a module method, `get_pbench_server_config()` to
    provide the same behavior.
    
    We also take the opportunity to update and correct the doc strings for
    the whole module.
    
    As a consequence of removing the `_ref_datetime` property, we had to
    update `pbench-cull-unpacked-tarballs`, and as a result of removing
    `_unittests` we took the opportunity to remove the unused "mock" code
    in the `pbench.server.s3backup` module.

--- lib/pbench/cli/server/__init__.py
+++ lib/pbench/cli/server/__init__.py
@@ -1,9 +1,9 @@
-from pbench.server import PbenchServerConfig
+from pbench.server import get_pbench_server_config, PbenchServerConfig
 from pbench.server.database import init_db
 
 
 def config_setup(context: object) -> PbenchServerConfig:
-    config = PbenchServerConfig(context.config)
+    config = get_pbench_server_config(context.config)
     # We're going to need the DB to track dataset state, so setup DB access.
     init_db(config, None)
     return config
