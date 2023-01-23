b6effc325c892e40df1b2cfaad8a502d7a20f4cc
npalaska <npalaska@redhat.com>
2023-01-23 12:43:12 -0500

    Extend laod_keycloak.sh script to automate keycloak image build
    PBENCH-1015

--- server/pbenchinacan/load_keycloak.sh
+++ server/pbenchinacan/load_keycloak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# This scripts loads the keycloak server with following configuration
+# This scripts creates the custom keycloak server image with following configuration
 # 1. A realm with the name 'pbench'
 # 2. A client 'pbench-server' under the realm pbench
 # 3. A user 'admin' under the realm pbench
@@ -17,12 +17,53 @@
 # "http://localhost:8080/*" unless specified otherwise by 'KEYCLOAK_REDIRECT_URI'
 # env variable.
 
+KEYCLOAK_BASE_IMAGE=${KEYCLOAK_BASE_IMAGE:-"quay.io/keycloak/keycloak:latest"}
 KEYCLOAK_HOST_PORT=${KEYCLOAK_HOST_PORT:-"http://localhost:8090"}
+KEYCLOAK_HEALTH_URI="${KEYCLOAK_HOST_PORT}/health"
 KEYCLOAK_REDIRECT_URI=${KEYCLOAK_REDIRECT_URI:-"http://localhost:8080/*"}
 ADMIN_USERNAME=${ADMIN_USERNAME:-"admin"}
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-"admin"}
 REALM=${KEYCLOAK_REALM:-"pbench"}
 CLIENT=${KEYCLOAK_CLIENT:-"pbench-server"}
+KEYCLOAK_IMAGE_TAG=${KEYCLOAK_IMAGE_TAG:-"test3"}
+
+container_name="mykeycloak"
+podman run -d --rm --name ${container_name} -p 8090:8090 \
+-e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin \
+${KEYCLOAK_BASE_IMAGE} start-dev --health-enabled=true --http-port=8090
+
+end_in_epoch_secs=$(( $(date +"%s") + 120 ))
+
+until curl -s -o /dev/null ${KEYCLOAK_HEALTH_URI}; do
+  if [[ $(date +"%s") -ge ${end_in_epoch_secs} ]]; then
+    echo "Timed out connecting to Keycloak" >&2
+    exit 1
+  fi
+  echo "Waiting for the Keycloak server" >&2
+  sleep 2
+done
+
+echo "Keycloak health url is up" >&2
+
+status_code=$(curl -s -o /dev/null -w "%{http_code}" ${KEYCLOAK_HEALTH_URI})
+
+while [[ $(date +"%s") -le ${end_in_epoch_secs} ]]; do
+  if [[ "${status_code}" == "200" ]]; then
+    keycloak_status=$(curl -s ${KEYCLOAK_HEALTH_URI} | jq -r ".status")
+    if [[ ${keycloak_status} == "UP" ]]; then
+      echo "Keycloak Server is up" >&2
+      break
+    else
+      echo "Keycloak Server status is not UP" >&2
+    fi
+  else
+    echo "Keycloak health status code ${status_code}" >&2
+  fi
+  status_code=$(curl -s -o /dev/null -w "%{http_code}" ${KEYCLOAK_HEALTH_URI})
+  sleep 2
+done
+
+# Run the custom configuration
 
 echo
 echo "KEYCLOAK_HOST_PORT: ${KEYCLOAK_HOST_PORT}"
@@ -104,3 +145,12 @@ curl -i -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/users/${USER_ID}/ro
   -H "Authorization: Bearer ${ADMIN_TOKEN}" \
   -H "Content-Type: application/json" \
   -d '[{"id":"'${ROLE_ID}'","name":"ADMIN"}]'
+
+sleep 5
+
+podman pause ${container_name}
+podman commit ${container_name} images.paas.redhat.com/pbench/pbenchinacan-keycloak:${KEYCLOAK_IMAGE_TAG}
+podman unpause ${container_name}
+podman stop ${container_name}
+
+podman push images.paas.redhat.com/pbench/pbenchinacan-keycloak:${KEYCLOAK_IMAGE_TAG}
