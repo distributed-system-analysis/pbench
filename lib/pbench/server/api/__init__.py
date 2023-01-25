6db3531df795e54b5e3eed02a7a5776ec981b439
Webb Scales <7795764+webbnh@users.noreply.github.com>
2023-01-25 12:22:38 -0500

    Clarify code which installs the Flask application context teardown handler

--- lib/pbench/server/api/__init__.py
+++ lib/pbench/server/api/__init__.py
@@ -202,6 +202,10 @@ def get_server_config() -> PbenchServerConfig:
 def create_app(server_config):
     """Create Flask app with defined resource endpoints."""
 
+    def shutdown_session(exception=None):
+        """Called from app context teardown hook to end the database session"""
+        Database.db_session.remove()
+
     app = Flask(__name__.split(".")[0])
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
@@ -221,8 +225,6 @@ def create_app(server_config):
         app.logger.exception("Exception while initializing sqlalchemy database")
         sys.exit(1)
 
-    @app.teardown_appcontext
-    def shutdown_session(exception=None):
-        Database.db_session.remove()
+    app.teardown_appcontext(shutdown_session)
 
     return app
