2dc9b23ef3192ccc568cb8fb930c2dbeee8d109b
Nikhil Palaskar <npalaska@redhat.com>
2023-02-03 20:53:41 -0500

    Simplify keycloak build script (#3228)
    * Simplify Keycloak build script
    
    - Remove the need for 2 redundant loops from the script.
    - Remove the health URI check when configuring OIDC 
      Keycloak server.
    - Quite down the Keycloak output.

--- server/pbenchinacan/load_keycloak.sh
+++ server/pbenchinacan/load_keycloak.sh
@@ -24,59 +24,34 @@ ADMIN_PASSWORD=${ADMIN_PASSWORD:-"admin"}
 REALM=${KEYCLOAK_REALM:-"pbench"}
 CLIENT=${KEYCLOAK_CLIENT:-"pbench-server"}
 
-keycloak_health_uri="${KEYCLOAK_HOST_PORT}/health"
-end_in_epoch_secs=$(( $(date +%s) + 120 ))
-
-until curl -s -o /dev/null ${keycloak_health_uri}; do
-  if [[ $(date +%s) -ge ${end_in_epoch_secs} ]]; then
-    echo "Timed out connecting to Keycloak" >&2
-    exit 1
-  fi
-  echo "Waiting for the Keycloak server" >&2
-  sleep 2
-done
-
-echo "Keycloak health url is up" >&2
-
-while [[ $(date +%s) -le ${end_in_epoch_secs} ]]; do
-  status_code=$(curl -s -o /dev/null -w "%{http_code}" ${keycloak_health_uri})
-  if [[ "${status_code}" == "200" ]]; then
-    keycloak_status=$(curl -s ${keycloak_health_uri} | jq -r ".status")
-    if [[ ${keycloak_status} == "UP" ]]; then
-      echo "Keycloak Server is up" >&2
-      break
-    else
-      echo "Keycloak Server status is not UP" >&2
-    fi
-  else
-    echo "Keycloak health status code ${status_code}" >&2
-  fi
-  sleep 2
-done
+end_in_epoch_secs=$(date --date "2 minutes" +%s)
 
 # Run the custom configuration
 
-echo
-echo "KEYCLOAK_HOST_PORT: ${KEYCLOAK_HOST_PORT}"
+ADMIN_TOKEN=""
+while true; do
+  ADMIN_TOKEN=$(curl -s -f -X POST "${KEYCLOAK_HOST_PORT}/realms/master/protocol/openid-connect/token" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d "username=${ADMIN_USERNAME}" \
+    -d "password=${ADMIN_PASSWORD}" \
+    -d 'grant_type=password' \
+    -d 'client_id=admin-cli' | jq -r '.access_token')
+  if [[ -n "${ADMIN_TOKEN}" ]]; then
+    break
+  elif [[ $(date +%s) -ge ${end_in_epoch_secs} ]]; then
+    echo "Timed out connecting to Keycloak" >&2
+    exit 1
+  else
+    echo "Waiting for the Keycloak server" >&2
+  fi
+  sleep 2
+done
 
 echo
-echo "Getting admin access token"
-echo "--------------------------"
-
-ADMIN_TOKEN=$(curl -s -f -X POST "${KEYCLOAK_HOST_PORT}/realms/master/protocol/openid-connect/token" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "username=${ADMIN_USERNAME}" \
-  -d "password=${ADMIN_PASSWORD}" \
-  -d 'grant_type=password' \
-  -d 'client_id=admin-cli' | jq -r '.access_token')
-
-echo "ADMIN_TOKEN=${ADMIN_TOKEN}"
+echo "Keycloak connection successful on : ${KEYCLOAK_HOST_PORT}"
 echo
 
-echo "Creating ${REALM} realm"
-echo "--------------"
-
-status_code=$(curl -f -i -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PORT}/admin/realms" \
+status_code=$(curl -f -s -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PORT}/admin/realms" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" \
   -H "Content-Type: application/json" \
   -d '{"realm": "'${REALM}'", "enabled": true}')
@@ -84,10 +59,9 @@ status_code=$(curl -f -i -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST
 if [[ "${status_code}" != "201" ]]; then
   echo "Realm creation failed with ${status_code}"
   exit 1
+else
+  echo "Created ${REALM} realm"
 fi
-
-echo "Creating ${CLIENT} client"
-echo "---------------"
 
 CLIENT_CONF=$(curl -si -f -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/clients" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" \
@@ -96,22 +70,22 @@ CLIENT_CONF=$(curl -si -f -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/c
 
 
 CLIENT_ID=$(grep -o -e 'http://[^[:space:]]*' <<< ${CLIENT_CONF} | sed -e 's|.*/||')
-echo "client_id=${CLIENT_ID}"
-echo
-
-echo "Getting client secret"
-echo "---------------------"
+if [[ -z "${CLIENT_ID}" ]]; then
+  echo "${CLIENT} id is empty"
+  exit 1
+else
+  echo "Created ${CLIENT} client"
+fi
 
 PBENCH_CLIENT_SECRET=$(curl -s -f -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/clients/${CLIENT_ID}/client-secret" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" | jq -r '.value')
 
-echo "PBENCH_CLIENT_SECRET=${PBENCH_CLIENT_SECRET}"
-echo
+if [[ -z "${PBENCH_CLIENT_SECRET}" ]]; then
+  echo "${CLIENT} secret is empty"
+  exit 1
+fi
 
-echo "Creating an 'ADMIN' role under ${CLIENT} client of the ${REALM} realm"
-echo "--------------------"
-
-status_code=$(curl -i -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/clients/${CLIENT_ID}/roles" \
+status_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/clients/${CLIENT_ID}/roles" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" \
   -H "Content-Type: application/json" \
   -d '{"name": "ADMIN"}')
@@ -119,16 +93,17 @@ status_code=$(curl -i -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PO
 if [[ "${status_code}" != "201" ]]; then
   echo "ADMIN role creation failed with ${status_code}"
   exit 1
+else
+  echo "Created an 'ADMIN' role under ${CLIENT} client of the ${REALM} realm"
 fi
 
 ROLE_ID=$(curl -s -f "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/clients/${CLIENT_ID}/roles" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" | jq -r '.[0].id')
 
-echo "ROLE_ID=${ROLE_ID}"
-echo
-
-echo "Creating an 'admin' user inside ${REALM} realm"
-echo "-------------"
+if [[ -z "${ROLE_ID}" ]]; then
+  echo "ADMIN role id is empty"
+  exit 1
+fi
 
 USER=$(curl -si -f -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/users" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" \
@@ -136,13 +111,15 @@ USER=$(curl -si -f -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/users" \
   -d '{"username": "admin", "enabled": true, "credentials": [{"type": "password", "value": "123", "temporary": false}]}')
 
 USER_ID=$(grep -o -e 'http://[^[:space:]]*' <<< ${USER} | sed -e 's|.*/||')
-echo "USER_ID=${USER_ID}"
-echo
 
-echo "Assigning 'ADMIN' client role to the user 'admin' created above"
-echo "---------------------------"
+if [[ -z "${USER_ID}" ]]; then
+  echo "User id is empty"
+  exit 1
+else
+  echo "Created an 'admin' user inside ${REALM} realm"
+fi
 
-status_code=$(curl -i -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/users/${USER_ID}/role-mappings/clients/${CLIENT_ID}" \
+status_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PORT}/admin/realms/${REALM}/users/${USER_ID}/role-mappings/clients/${CLIENT_ID}" \
   -H "Authorization: Bearer ${ADMIN_TOKEN}" \
   -H "Content-Type: application/json" \
   -d '[{"id":"'${ROLE_ID}'","name":"ADMIN"}]')
@@ -150,6 +127,8 @@ status_code=$(curl -i -o /dev/null -w "%{http_code}" -X POST "${KEYCLOAK_HOST_PO
 if [[ "${status_code}" != "204" ]]; then
   echo "Assigning 'ADMIN' client role to the user 'admin' failed with ${status_code}"
   exit 1
+else
+  echo "Assigned an 'ADMIN' client role to the user 'admin' created above"
 fi
 
 # Verify that the user id has a role assigned to it
