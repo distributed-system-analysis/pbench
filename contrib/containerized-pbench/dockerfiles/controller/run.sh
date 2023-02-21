e563bd62dcc29f338ca634bc09f88a1b9397f81d
Nick Dokos <ndokos@redhat.com>
2023-02-21 17:11:45 -0500

    Get rid of obsolete `pbench_results_redirector' setting

--- contrib/containerized-pbench/dockerfiles/controller/run.sh
+++ contrib/containerized-pbench/dockerfiles/controller/run.sh
@@ -5,7 +5,6 @@ mkdir -p /var/lib/pbench-agent/tools-default
 cp /root/.ssh/id_rsa /opt/pbench-agent/id_rsa
 
 # Set pbench-server in the config
-sed -i "/^pbench_results_redirector/c pbench_results_redirector = ${pbench_server}" /opt/pbench-agent/config/pbench-agent.cfg
 sed -i "/^pbench_web_server/c pbench_web_server = ${pbench_server}"  /opt/pbench-agent/config/pbench-agent.cfg
 
 # Clear tools
@@ -26,7 +25,7 @@ fi
 
 if [[ "${clear_results}" == "true" ]] || [[ "${clear_results}" == "True" ]]; then
        pbench-clear-results
-fi       
+fi
 
 # Run the benchmark
 ${benchmark}
