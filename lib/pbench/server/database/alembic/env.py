c2b0928fc10ed6dc61799c91dfb5bebebc1011a1
npalaska <npalaska@redhat.com>
2023-02-26 23:52:58 -0500

    new db migration file

--- lib/pbench/server/database/alembic/env.py
+++ lib/pbench/server/database/alembic/env.py
@@ -56,7 +56,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata, compare_type=True
+        )
 
         with context.begin_transaction():
             context.run_migrations()
