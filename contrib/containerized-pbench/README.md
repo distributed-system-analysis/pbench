8f926516eb7386d78cbae5ff661975e27c49384b
Peter Portante <peter.portante@redhat.com>
2023-03-04 13:27:29 -0500

    Apply review feedback

--- contrib/containerized-pbench/README.md
+++ contrib/containerized-pbench/README.md
@@ -2,7 +2,7 @@
 1. Create a workload script (`example-workload.sh` can be used to start)
    This script performs the invocation of a Pbench Agent workload driver, e.g.
    `pbench-user-benchmark`, or `pbench-fio`, or `pbench-uperf`.
-2. Setup the local exeuction environment to be aware of the target Pbench Server
+2. Setup the local execution environment to be aware of the target Pbench Server
    `./setup.sh <pbench server host name>:<port number>`
 3. Execute the workload
    Typically this script performs the necessary setup required for the
--- contrib/containerized-pbench/setup.sh
+++ contrib/containerized-pbench/setup.sh
@@ -2,16 +2,16 @@
 
 if [[ -z "${1}" ]]; then
     echo "Please provide a target Pbench Server (<host name>:<port number>) to move the results to as the first argument." >&2
-    exit 1
+    exit 2
 fi
-cat > ./pbench-agent.cfg.tmp << EOF
-[DEFAULT]
-pbench_install_dir = /opt/pbench-agent
-pbench_web_server = ${1}
-[config]
-path = %(pbench_install_dir)s/config
-files = pbench-agent-default.cfg
-EOF
+cat > ./pbench-agent.cfg.tmp <<- EOF
+	[DEFAULT]
+	pbench_install_dir = /opt/pbench-agent
+	pbench_web_server = ${1}
+	[config]
+	path = %(pbench_install_dir)s/config
+	files = pbench-agent-default.cfg
+	EOF
 
 # Create a directory to use as the `/var/lib/pbench-agent` directory mounted
 # in the container.
