4cf519e2e8cdb36df9d2f24a26ec4e25ba2bc8cb
Peter Portante <peter.portante@redhat.com>
2023-03-08 07:41:05 -0500

    Remove agent code from `b0.69`, bump to `v0.69.11`
    This also fixes the `pyproject` related warnings when running tests
    without the `-r` option to `tox`.

--- agent/bench-scripts/test-bin/fio-histo-log-pctiles.py
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "$0 $*" >> $_testlog
-
-echo "ts, samples, min, median, p90, p95, p99, p99.5, max"
-echo "1000, 0, 1, 2, 3, 4, 5, 6, 7"
--- agent/util-scripts/configs/pbench-register-tool-set/test-11.10/pbench-agent.cfg.frag
+++
@@ -1,3 +0,0 @@
-[pbench/tools]
-default-tool-set = mpstat, vmstat, iostat, perf
-other-tool-set = pidstat, sar
