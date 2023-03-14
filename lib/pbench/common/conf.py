e6f20137d0a82cb2995b04b609e80d5da9b553aa
Peter Portante <peter.portante@redhat.com>
2023-03-14 00:56:44 -0400

    Separate `pbench-config` and `pbench-server-config`

--- lib/pbench/common/conf.py
+++ lib/pbench/common/conf.py
@@ -1,31 +1,19 @@
-import os
-import sys
-
 from pbench.common import configtools
 
 
-def get_pbench_config():
-    """Determine if we are using agent or server."""
-    agent_cfg = os.environ.get("_PBENCH_AGENT_CONFIG")
-    server_cfg = os.environ.get("_PBENCH_SERVER_CONFIG")
-    if agent_cfg:
-        return "_PBENCH_AGENT_CONFIG"
-    elif server_cfg:
-        return "_PBENCH_SERVER_CONFIG"
-    else:
-        return sys.exit(1)
+def common_main(prog: str, env: str):
+    """Common entry point for agent and server pbench-config commands.
 
+    Args:
+        prog : invocation program name to use in Usage message
+        env : environment variable for configuration file
 
-def main():
-    prog = os.path.basename(sys.argv[0])
+    Returns:
+        Pass through of return code from `configtools.main`
+    """
     opts, args = configtools.parse_args(
         configtools.options,
         usage=f"Usage: {prog} [options] <item>|-a <section> [<section> ...]",
     )
-    conf, files = configtools.init(opts, get_pbench_config())
-    status = configtools.main(conf, args, opts, files)
-    sys.exit(status)
-
-
-if __name__ == "__main__":
-    main()
+    conf, files = configtools.init(opts, env)
+    return configtools.main(conf, args, opts, files)
