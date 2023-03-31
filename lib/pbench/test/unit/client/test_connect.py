05265d25143a543ded2b8f40cdaa3dc7e63afb62
David Butenhof <dbutenho@redhat.com>
2023-03-31 10:35:33 -0400

    Like pulling weed roots ...

--- lib/pbench/test/unit/client/test_connect.py
+++ lib/pbench/test/unit/client/test_connect.py
@@ -30,7 +30,7 @@ class TestConnect:
                 url,
                 json={
                     "identification": "string",
-                    "api": {},
+
                     "uri": {},
                     "openid": openid_dict,
                 },
@@ -54,7 +54,6 @@ class TestConnect:
         # Check that the fake endpoints we returned are captured
         endpoints = pbench.endpoints
         assert endpoints
-        assert endpoints["api"] == {}
         assert endpoints["identification"] == "string"
         assert endpoints["uri"] == {}
         assert endpoints["openid"] == openid_dict
