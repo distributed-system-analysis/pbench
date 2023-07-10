094a6e3dccc0305a97ea2f66445725dc7110c6d5
Nick Dokos <ndokos@redhat.com>
2023-07-10 16:09:12 -0400

    Partial backport of #3469 (agent side only) to b0.72
    Replace `requests.exceptions.JSONDecodeError' by generic `Exception'.
    
    See #3469 for some details. The upstream issue is
    
       https://github.com/psf/requests/issues/5794
    
    and the PR is
    
       https://github.com/psf/requests/pull/5856
    
    but we are running older versions of `requests' in various places.

--- lib/pbench/cli/agent/commands/results/move.py
+++ lib/pbench/cli/agent/commands/results/move.py
@@ -5,7 +5,6 @@ import tempfile
 from typing import List
 
 import click
-import requests
 
 from pbench.agent.base import BaseCommand
 from pbench.agent.results import CopyResultTb, MakeResultTb
@@ -111,7 +110,7 @@ class MoveResults(BaseCommand):
                     if not res.ok:
                         try:
                             msg = res.json()["message"]
-                        except requests.exceptions.JSONDecodeError:
+                        except Exception:
                             msg = res.text if res.text else res.reason
                         raise CopyResultTb.FileUploadError(msg)
                 except Exception as exc:
