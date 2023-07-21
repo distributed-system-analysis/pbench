aab4a16b22921a44416b18eac85c2e08fec9aa96
David Butenhof <dbutenho@redhat.com>
2023-07-21 16:02:59 -0400

    Cleanup

--- agent/containers/images/container_build.sh
+++ agent/containers/images/container_build.sh
@@ -4,17 +4,33 @@
 # Pbench Agent containers for a platform specified by the PB_AGENT_DISTRO
 # environment variable, defaulting to fedora-38.
 #
-# This is similar to the commands used in the CI; however, the CI builds the
+# These are the same commands used in the CI; however, the CI builds the
 # RPMs in the common build.sh script while the Jenkins Pipeline.gy builds the
-# CI container on top of that. This encapsulates the "trickery" used to get a
-# functional result so it's easier to use interactively.
+# CI container on top of that. Here we encapsulate the steps used to get a
+# functional result to make this more convenient interactively.
 #
-# For efficiency, we don't use the various "cleanup" Make targets; however
-# instead of needing to remember and execute them separately, you can use
+# WORKSPACE_TMP is assumed to be the root of the work area, and by default will
+# be set to ${HOME}.
+#
+# If you want to clean the make targets for RPM and container builds, use:
+#
 #   agent/containers/images/container_build.sh --clean
-# to insert the cleanup targets when desirable.
 
-opts=$(getopt -q -o ch --longoptions "clean,help" -n "container_build" -- "${@}")
+export PB_AGENT_DISTRO=${PB_AGENT_DISTRO:-fedora-38}
+export WORKSPACE_TMP=${WORKSPACE_TMP:-${HOME}}
+
+function usage {
+    printf "Build a Pbench Agent container for the distribution named by the\n"
+    printf "PB_AGENT_DISTRO environment variable, which defaults to '${PB_AGENT_DISTRO}'.\n"
+    printf "\nThe following options are available:\n"
+    printf "\n"
+    printf -- "\t-c|--clean\n"
+    printf "\t\tRemove old RPM and container image targets before building.\n"
+    printf -- "\t-h|--help\n"
+    printf "\t\tPrint this usage message and terminate.\n"
+}
+
+opts=$(getopt -q -o ch --longoptions "clean,help" -n "${0}" -- "${@}")
 if [[ ${?} -ne 0 ]]; then
     printf -- "%s %s\n\n\tunrecognized option specified\n\n" "${0}" "${*}" >&2
     usage >&2
@@ -22,14 +38,14 @@ if [[ ${?} -ne 0 ]]; then
 fi
 eval set -- "${opts}"
 rpm_clean=
-pod_clean=
+image_clean=
 while true; do
     arg=${1}
     shift
     case "${arg}" in
     -c|--clean)
         rpm_clean=distclean
-        pod_clean=clean
+        image_clean=clean
         ;;
     -h|--help)
         usage
@@ -46,8 +62,6 @@ while true; do
     esac
 done
 
-PB_AGENT_DISTRO=${PB_AGENT_DISTRO:-fedora-38}
-
-WORKSPACE_TMP=${HOME} make -C agent/rpm ${rpm_clean} ${PB_AGENT_DISTRO}-rpm
-CI=1 WORKSPACE_TMP=${HOME} make -C agent/containers/images \
-    CI_RPM_ROOT=${HOME} ${pod_clean} ${PB_AGENT_DISTRO}-everything
+make -C agent/rpm ${rpm_clean} ${PB_AGENT_DISTRO}-rpm
+make -C agent/containers/images CI=1 CI_RPM_ROOT=${WORKSPACE_TMP} \
+    ${image_clean} ${PB_AGENT_DISTRO}-everything
