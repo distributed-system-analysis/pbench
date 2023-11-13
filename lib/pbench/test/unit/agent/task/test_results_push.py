93155c201d8d61656366b7f2a8359017ab3bb8c9
Webb Scales <wscales@redhat.com>
2023-11-13 12:27:39 -0500

    pick lint from test_results_push.py

--- lib/pbench/test/unit/agent/task/test_results_push.py
+++ lib/pbench/test/unit/agent/task/test_results_push.py
@@ -25,7 +25,7 @@ class TestResultsPush:
     RELAY_SWITCH = "--relay"
     BRIEF_SWITCH = "--brief"
     SRVR_SWITCH = "--server"
-    RELAY_TEXT = "http://relay.example.com"
+    RELAY_TEXT = "https://relay.example.com"
     SRVR_TEXT = "https://pbench.test.example.com"
     URL = "https://pbench.example.com/api/v1"
 
@@ -144,7 +144,7 @@ class TestResultsPush:
         assert result.exit_code == 0
         pattern = (
             "RELAY log.tar.xz: " if not brief else ""
-        ) + r"http://relay.example.com/[a-z0-9]+\n"
+        ) + r"https://relay.example.com/[a-z0-9]+\n"
         assert re.match(pattern, result.stdout)
 
     @staticmethod
